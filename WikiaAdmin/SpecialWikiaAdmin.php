4087009ce827171c2af4da6911e1c5859d26bb07
Aran Dunkley <aran@organicdesign.co.nz>
2009-10-12 01:49:40 +0000

    wiki creation tested and working
    git-svn-id: svn+ssh://organicdesign.co.nz/extensions@50 2ad5491c-c162-49ae-9d3b-afa169216a3e

--- WikiaAdmin/SpecialWikiaAdmin.php
+++ WikiaAdmin/SpecialWikiaAdmin.php
@@ -12,7 +12,9 @@
 
 if ( !defined('MEDIAWIKI' ) ) die( 'Not an entry point.' );
 
-define( 'WIKIAADMIN_VERSION', '0.0.1, 2009-10-08' );
+define( 'WIKIAADMIN_VERSION', '0.0.2, 2009-10-12' );
+
+$wgWikiaAdminDomains = array();
 
 $wgExtensionFunctions[] = 'wfSetupWikiaAdmin';
 
@@ -31,100 +33,147 @@ require_once( "$IP/includes/SpecialPage.php" );
  */
 class SpecialWikiaAdmin extends SpecialPage {
 
+	var $error = '';
+	var $result = '';
+
 	function SpecialWikiaAdmin() {
 		SpecialPage::SpecialPage( 'WikiaAdmin', 'sysop', true, false, false, false );
-	}
-
-	/**
-	 * Return passed size as Bytes, KB, MB, GB
-	 */
-	function friendlySize( $size ) {
-		$suffix = 'Bytes';
-		if ( $size >> 30 )     { $size >>= 30; $suffix = 'GB'; }
-		elseif ( $size >> 20 ) { $size >>= 20; $suffix = 'MB'; }
-		elseif ( $size >> 10 ) { $size >>= 10; $suffix = 'KB'; }
-		return "$size $suffix";
 	}
 
 	/**
 	 * Override SpecialPage::execute()
 	 */
 	function execute() {
-		global $wgOut, $wgRequest, $domains, $settings, $wgWikiaAdminDomains;
+		global $wgOut, $wgRequest;
 		$this->setHeaders();
+
+		# Retrieve any posted data or set defaults
+		$this->newid    = $wgRequest->getText( 'wpNewid' );
+		$this->curid    = $wgRequest->getText( 'wpCurid' );
+		$this->sitename = $wgRequest->getText( 'wpSitename' );
+		$this->domains  = $wgRequest->getText( 'wpDomains' );
+		$this->codebase = $wgRequest->getText( 'wpCodebase' );
+		$this->user     = $wgRequest->getText( 'wpUser', 'WikiSysop' );
+		$this->pass     = $wgRequest->getText( 'wpPass' );
+		$this->pass2    = $wgRequest->getText( 'wpPass2' );
+
+		# Process request if form submitted
+		if ( $wgRequest->getText( 'wpSubmit' ) ) $this->processForm();
+
+		# Render any errors or results set during processing
+		if ( !empty( $this->error ) ) $wgOut->addHtml( $this->error );
+		if ( !empty( $this->result ) ) $wgOut->addHtml( $this->result );
+
+		# Render the form
+		$this->renderForm();
+
+	}
+
+	/**
+	 * Render the special page form and populate with posted data or defaults
+	 */
+	function renderForm() {
+		global $wgOut, $wgWikiaAdminDomains;
+		$url = Title::newFromText( 'WikiaAdmin', NS_SPECIAL )->getLocalUrl();
+		$wgOut->AddHtml( "<form action=\"$url\" method=\"POST\" enctype=\"multipart/form-data\">" );
 		$wgOut->AddHtml( "<table cellpadding=\"4\" cellspacing=\"0\">" );
-
-		# Retrieve any posted data
-		$newid    = $wgRequest->getText( 'newid' );
-		$curid    = $wgRequest->getText( 'curid' );
-		$sitename = $wgRequest->getText( 'sitename' );
-		$domains  = $wgRequest->getText( 'domains' );
-		$codebase = $wgRequest->getText( 'codebase' );
-		$user     = $wgRequest->getText( 'user', 'WikiSysop' );
-		$pass     = $wgRequest->getText( 'pass' );
-		$pass2    = $wgRequest->getText( 'pass2' );
-
-		# Process request if form posted
-		if ( $wgRequest->getText( 'submit' ) ) {
-		}
 
 		# ID for a new wiki
 		$wgOut->AddHtml( "<tr><td>Enter ID for new wiki:</td> " );
-		$wgOut->AddHtml( "<td><input name=\"newid\" value=\"$newid\" /></td><td><i>(lowercase a-z, used for FS location and for DB prefix)</i></td></tr>" );
+		$wgOut->AddHtml( "<td><input name=\"wpNewid\" value=\"{$this->newid}\" /></td><td><i>(lowercase a-z, used for FS location and for DB prefix)</i></td></tr>" );
 
 		# or an ID of an existing one to modify
 		$wgOut->AddHtml( "<tr><td>Or modify an existing wiki:</td> " );
 		$options = "<option />";
 		foreach ( glob( '/var/www/wikis/*' ) as $wiki ) {
 			if ( ereg( '([^/]+)$', $wiki, $m ) ) {
-				$selected = $curid == $m[1] ? ' selected' : '';
+				$selected = $this->curid == $m[1] ? ' selected' : '';
 				$options .= "<option$selected>$m[1]</option>";
 			}
 		}
-		$wgOut->AddHtml( "<td><select name=\"curid\">$options</select></td></tr>" );
+		$wgOut->AddHtml( "<td><select name=\"wpCurid\">$options</select></td></tr>" );
 
 		$wgOut->AddHtml( "<tr><td colspan=\"3\"><hr /></td></tr>" );
 
 		# Site name
 		$wgOut->AddHtml( "<tr><td>Full site name:</td> " );
-		$wgOut->AddHtml( "<td><input name=\"sitename\" value=\"$sitename\" /></td></tr>" );
+		$wgOut->AddHtml( "<td><input name=\"wpSitename\" value=\"{$this->sitename}\" /></td></tr>" );
 
 		# Domain selection
 		$wgOut->AddHtml( "<tr><td valign=\"top\">Domains:</td>" );
-		$wgOut->AddHtml( "<td><textarea name=\"domains\">$domains</textarea></td>" );
+		$wgOut->AddHtml( "<td><textarea name=\"wpDomains\">{$this->domains}</textarea></td>" );
 		$wgOut->AddHtml( "<td valign=\"top\"><i>(naked domains automatically<br />work for www and wiki as well)</i></td></tr>" );
 
 		# Codebase selection
 		$wgOut->AddHtml( "<tr><td>MediaWiki version:</td> " );
 		$options = "";
 		foreach ( glob( '/var/www/empty*.sql' ) as $cb ) {
-			if ( ereg( '([0-9.]+).*\.sql', $cb, $m ) ) $options .= "<option>$m[1]</option>";
+			if ( ereg( '([0-9.]+).*\.sql', $cb, $m ) ) {
+				$selected = $this->codebase == $m[1] ? ' selected' : '';
+				$options .= "<option>$m[1]</option>";
+			}
 		}
-		$wgOut->AddHtml( "<td><select name=\"codebase\">$options</select></td></tr>" );
+		$wgOut->AddHtml( "<td><select name=\"wpCodebase\">$options</select></td></tr>" );
 
 		$wgOut->AddHtml( "<tr><td colspan=\"3\"><hr /></td></tr>" );
 
 		# Sysop details
 		$wgOut->AddHtml( "<tr><td>Sysop user:</td> " );
-		$wgOut->AddHtml( "<td><input name=\"sysopuser\" value=\"$user\" /></td></tr>" );
+		$wgOut->AddHtml( "<td><input name=\"wpUser\" value=\"{$this->user}\" /></td></tr>" );
 		$wgOut->AddHtml( "<tr><td>Sysop password:</td> " );
-		$wgOut->AddHtml( "<td><input type=\"password\" name=\"sysoppass\" /></td></tr>" );
+		$wgOut->AddHtml( "<td><input type=\"password\" name=\"wpPass\" /></td></tr>" );
 		$wgOut->AddHtml( "<tr><td>Confirm password:</td> " );
-		$wgOut->AddHtml( "<td><input type=\"password\" name=\"confirmpass\" /></td></tr>" );
-
-		# Get list of domain symlinks by inode
-		$dlist = array();
-		foreach ( glob( "/var/www/domains/*" ) as $link ) {
-			$stat  = stat( $link );
-			$inode = $stat[1];
-			if ( isset( $dlist[$inode] ) && is_array( $dlist[$inode] ) ) $dlist[$inode][] = basename( $link );
-			else $dlist[$inode] = array( basename( $link ) );
-		}
+		$wgOut->AddHtml( "<td><input type=\"password\" name=\"wpPass2\" /></td></tr>" );
 
 		$wgOut->AddHtml( "<tr><td colspan=\"3\"><hr /></td></tr>" );
 		$wgOut->AddHtml( "<tr><td /><td align=\"right\"><input type=\"submit\" name=\"wpSubmit\" value=\"Submit\" /></td></tr>" );
 
-		$wgOut->AddHtml( "</table>" );
+		$wgOut->AddHtml( "</table></form>" );
+	}
+
+	/**
+	 * Create or update a wiki in accord with submitted data
+	 */
+	function processForm() {
+		$dir = "/var/www/wikis/{$this->newid}";
+		$mw = "/var/www/mediawiki-{$this->codebase}";
+
+		# Validation (should use friendly JS instead)
+		if ( empty( $this->pass ) )                           return $this->error = 'Please fill in the password fields!';
+		if ( $this->pass !== $this->pass2 )                   return $this->error = 'The password fields do not match!';
+		if ( empty( $this->newid ) && empty( $this->curid ) ) return $this->error = 'No ID specified!';
+		if ( ereg( '[^a-zA-Z0-9_]', $this->newid ) )          return $this->error = 'Invalid wiki ID!';
+		if ( is_dir( $dir ) )                                 return $this->error = 'A wiki with that ID already exists!';
+		if ( empty( $this->domains ) )                        return $this->error = 'No domain specified!';
+		if ( empty( $this->sitename ) )                       return $this->error = 'No site name specified!';
+		if ( empty( $this->pass ) )                           return $this->error = 'Please fill in the password fields!';
+		if ( empty( $this->user ) || ereg( '[^a-zA-Z0-9_]', $this->user ) ) return $this->error = 'Invalid user name!';
+
+		if ( !empty( $this->newid ) ) {
+			$id = $this->newid;
+
+			# Create the wiki dir and codebase symlink
+			if ( !is_dir( $mw ) ) return $this->error = "MediaWiki codebasee '$mw' not found!";
+			shell_exec( "mkdir $dir" );
+			shell_exec( "mkdir -m 777 $dir/files" );
+			shell_exec( "ln -s $mw $dir/wiki" );
+
+			# Create LocalSettings
+			$ls = "<?php\n";
+			$ls .= "\$wgSitename          = '{$this->sitename}';\n";
+			$ls .= "\$wgShortName         = '$id';\n";
+			$ls .= "\$wgDBname            = 'wikia';\n";
+			$ls .= "\$wgDBprefix          = '{$id}_';\n";
+			file_put_contents( "$dir/LocalSettings.php", $ls );
+			
+			# Add the database template to the "wikia" DB
+			shell_exec( "/var/www/tools/add-db /var/www/empty-{$this->codebase}.sql wikia.{$id}_" );
+
+			# Add the domain symlinks
+			foreach( split( "\n", $this->domains ) as $domain ) {
+				shell_exec( "ln -s $dir /var/www/domains/$domain" );
+			}
+		}
 
 	}
 
