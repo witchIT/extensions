fd4b9b1811f8290c7ad0c33842c496cb0fca1e6c
Aran Dunkley <aran@organicdesign.co.nz>
2009-11-08 07:34:33 +0000

    Start internationalisation for OD wikia
    git-svn-id: svn+ssh://organicdesign.co.nz/extensions@74 2ad5491c-c162-49ae-9d3b-afa169216a3e

---
+++ wikia.i18n.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Internationalisation for OrganicDesign wiki shared messages
+ *
+ * @author Nad
+ * @file
+ * @ingroup Extensions
+ */
+
+$messages = array();
+
+/** English
+ * @author Nad
+ */
+$messages['en'] = array(
+	'od-nospec' => 'There is no specification yet, you can create one after submitting this form',
+);
+
+
