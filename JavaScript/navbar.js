bd0239df72407e83fea085d2801f1585ebb05f19
Aran Dunkley <aran@organicdesign.co.nz>
2009-11-09 00:17:41 +0000

    W3C cookie functions needed by navbar
    git-svn-id: svn+ssh://organicdesign.co.nz/extensions@77 2ad5491c-c162-49ae-9d3b-afa169216a3e

--- JavaScript/navbar.js
+++ JavaScript/navbar.js
@@ -1,3 +1,27 @@
+// for backwards compatibility
+var addLoadEvent = addOnloadHook;
+
+// Cookie set/get functions from W3C
+function setCookie(c_name, value, expiredays) {
+  var exdate = new Date();
+  exdate.setDate(exdate.getDate() + expiredays);
+  document.cookie = c_name+ "=" +escape(value) +
+  ((expiredays == null) ? "" : ";expires=" + exdate.toGMTString());
+}
+
+function getCookie(c_name) {
+  if (document.cookie.length > 0) {
+    c_start = document.cookie.indexOf(c_name + "=");
+    if (c_start != -1) { 
+      c_start = c_start + c_name.length + 1; 
+      c_end = document.cookie.indexOf(";", c_start);
+      if (c_end == -1) c_end = document.cookie.length;
+      return unescape(document.cookie.substring(c_start, c_end));
+    } 
+  }
+  return "";
+}
+
 // hasClass()
  // Description: Uses regular expressions and caching for better performance.
  // Maintainers: w:User:Mike Dillon, w:User:R. Koot, w:User:SG
