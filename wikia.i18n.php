8e997e3ab07e2099005df2123e5e151143d89e36
Jack Henderson <jack@jack.co.nz>
2009-11-12 03:18:57 +0000

    Committed
    git-svn-id: svn+ssh://organicdesign.co.nz/extensions@93 2ad5491c-c162-49ae-9d3b-afa169216a3e

--- wikia.i18n.php
+++ wikia.i18n.php
@@ -13,9 +13,11 @@ $messages = array();
  * @author Nad
  */
 $messages['en'] = array(
+	'od-nospec' => 'There is no specification yet, you can create one after submitting this form.',
 	'od-nospec-form' => 'There is no specification yet, you can create one after submitting this form. This should lead to a complete set of Key Issues (Milestones).',
 	'od-nospec-template' => 'By clicking on the Specifications link below you can create a complete set of Key Issues (milestones) and their Due Dates.',
 	'od-nofaq' => 'When you have completed this form, you can then write the Position Description, FAQ, Best Practice and Contract articles for this Role.',
+	'od-multiselect-click' => 'Ctrl-click to multi-select.',
 );
 
 
