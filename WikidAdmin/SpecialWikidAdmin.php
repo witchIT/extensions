7923b3f4cd7ebb0e3b9407afde5c7056cc347c36
Aran Dunkley <aran@organicdesign.co.nz>
2009-11-26 08:06:36 +0000

    Depends on EventPipe extension, die if not present
    git-svn-id: svn+ssh://organicdesign.co.nz/extensions@123 2ad5491c-c162-49ae-9d3b-afa169216a3e

--- WikidAdmin/SpecialWikidAdmin.php
+++ WikidAdmin/SpecialWikidAdmin.php
@@ -8,10 +8,10 @@
  * @copyright © 2009 Aran Dunkley
  * @licence GNU General Public Licence 2.0 or later
  */
+if ( !defined('MEDIAWIKI' ) )            die( 'Not an entry point.' );
+if ( !defined( 'EVENTPIPE_VERSION' ) ) die( 'This extension depends on the EventPipe extension' );
 
-if ( !defined('MEDIAWIKI' ) ) die( 'Not an entry point.' );
-
-define( 'WIKIDADMIN_VERSION', '1.1.2, 2009-11-23' );
+define( 'WIKIDADMIN_VERSION', '1.1.3, 2009-11-26' );
 
 $wgExtensionFunctions[] = 'wfSetupWikidAdmin';
 $wgAjaxExportList[] = 'wfWikidAdminRenderWork';
