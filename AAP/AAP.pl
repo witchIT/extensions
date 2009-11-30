3d357be1f7bca29c7d3b4e8cd7074c7726a3a329
Aran Dunkley <aran@organicdesign.co.nz>
2009-11-30 01:17:44 +0000

    create an index of the byte offsets for each line in the input file
    git-svn-id: svn+ssh://organicdesign.co.nz/extensions@130 2ad5491c-c162-49ae-9d3b-afa169216a3e

--- AAP/AAP.pl
+++ AAP/AAP.pl
@@ -12,60 +12,35 @@ sub initAAPImport {
 	my $file = $$::job{'file'};
 
 	# Index the byte offsets of each line in the source file
-	my @index = ();
-	open INPUT, '<', $file
-	
-	$offset = tell($data_file);
-	
-
-
-	# Preprocess the workload to check for errors
-	my $line = 1;
-	my $errors = 0;
-	$$::job{'status'} = "Preprocessing \"$file\"";
+	my @index = ( 0 );
 	if ( open INPUT, '<', $file ) {
-		$$::job{'length'} = 1 + $#{$$::job{'work'}};
-	}
+		push @index, tell INPUT while <INPUT>;
+    }
+    
+	# Couldn't open file
+    else {
+		workLogError( "Couldn't open input file \"$file\", job aborted!" );
+		workStopJob();
+	}	
+
+	$$::job{'index'} = \@index;
+	$$::job{'length'} = 1 + $#index;
 
 	1;
 }
 
-sub mainDcsImport {
-	my $wiki  = $$::job{'wiki'};
-	my $title = $$::job{'work'}[$$::job{'wptr'}];
-	my $text  = readFile( '/tmp/' . $$job{'id'} . '-' . encode_base64( $title ) );
-	my $file  = $$::job{'file'};
-	$$::job{'status'} = "Processing \"$title\"";
+sub mainAAPImport {
+	my $wiki   = $$::job{'wiki'};
+	my $file   = $$::job{'file'};
+	my $ptr    = $$::job{'wptr'};
+	my $offset = $$::job{'index'}[$ptr];
+	my $length = $$::job{'index'}[$ptr + 1] - $offset;
+	$$::job{'status'} = "Processing record $ptr";
 	my $cur = wikiRawPage( $wiki, $title );
 	$$::job{'revisions'}++ if wikiEdit( $wiki, $title, $text, "Content imported from \"$file\"" ) && $cur ne $text;
 	1;
 }
 
-sub stopDcsImport {
+sub stopAAPImport {
 	1;
 }
-
-
-#---------------------------------------------------------------------------------------------------------#
-# DEBUGGING
-
-# Include this for stand alone testing
-if ( 0 ) {
-	use DBI;
-	require( 'wiki.pl' );
-	$wiki = 'http://114.localhost/wiki/index.php';
-	wikiLogin( $::wiki, 'Nad', '*******' );
-	$::dbname = 'svn';
-	$::dbuser = 'root';
-	$::dbpass = '*******';
-	$::dbpre = '';
-	$::db = DBI->connect( "DBI:mysql:$::dbname", $::dbuser, $::dbpass );
-}
-
-1;
-
-
-
-
-
-
