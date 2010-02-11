cc0eb4124b32eb71e16cc547734ea52ddd3aa378
Aran Dunkley <aran@organicdesign.co.nz>
2010-02-11 06:56:00 +0000

    new extension for simplifying discussion behaviour
    git-svn-id: svn+ssh://organicdesign.co.nz/extensions@168 2ad5491c-c162-49ae-9d3b-afa169216a3e

---
+++ Comments/Comments.php
@@ -0,0 +1,44 @@
+<?php
+/**
+ * Comments extension
+ * - Changes discussion page behaviour to a more forum-like format below the article
+ *
+ * See http://www.organicdesign.co.nz/Extension:Comments for development notes and disucssion
+ *
+ * Started: 2010-02-11
+ * 
+ * @package MediaWiki
+ * @subpackage Extensions
+ * @author Aran Dunkley [http://www.organicdesign.co.nz/nad User:Nad]
+ * @copyright © 2010 Aran Dunkley
+ * @licence GNU General Public Licence 2.0 or later
+ */
+if ( !defined( 'MEDIAWIKI' ) ) die( 'Not an entry point.' );
+
+define( 'COMMENTS_VERSION', '0.0.1, 2010-02-11' );
+
+$wgExtensionFunctions[]        = 'wfSetupComments';
+
+$wgExtensionCredits['other'][] = array(
+	'path'        => __FILE__,
+	'name'	      => 'Comments',
+	'author'      => '[http://www.organicdesign.co.nz/nad User:Nad]',
+	'description' => 'Changes discussion page behaviour to a more forum-like format below the article',
+	'url'	      => 'http://www.organicdesign.co.nz/Extension:Comments',
+	'version'     => COMMENTS_VERSION
+);
+
+class Comments {
+
+	function Comments() {
+		global $wgHooks, $wgParser, $wgPdfBookMagic;
+		$wgHooks['UnknownAction'][] = $this;
+
+	}
+
+}
+
+function wfSetupComments() {
+	global $wgComments;
+	$wgComments = new Comments();
+}
