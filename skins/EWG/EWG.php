d73cb385d95e0cbf37b5d9d01702e2871f641343
Jack Henderson <jack@jack.co.nz>
2010-02-18 23:56:42 +0000

    new ewg skin
    git-svn-id: svn+ssh://organicdesign.co.nz/extensions@183 2ad5491c-c162-49ae-9d3b-afa169216a3e

--- skins/EWG/EWG.php
+++ skins/EWG/EWG.php
@@ -1,8 +1,6 @@
 <?php
 /**
- * KG 
- *
- * Based on Monobook nouveau
+ * EWG Skin based on MonoBook
  *
  * Translated from gwicke's previous TAL template version to remove
  * dependency on PHPTAL.
@@ -12,8 +10,7 @@
  * @ingroup Skins
  */
 
-if( !defined( 'MEDIAWIKI' ) )
-	die( -1 );
+if( !defined( 'MEDIAWIKI' ) ) die( -1 );
 
 /**
  * Inherit main code from SkinTemplate, set the CSS and template filter.
@@ -21,7 +18,6 @@ if( !defined( 'MEDIAWIKI' ) )
  * @ingroup Skins
  */
 class SkinEWG extends SkinTemplate {
-	/** Using ewg. */
 	function initPage( &$out ) {
 		SkinTemplate::initPage( $out );
 		$this->skinname  = 'ewg';
@@ -64,14 +60,24 @@ class EWGTemplate extends QuickTemplate {
 		<meta http-equiv="Content-Type" content="<?php $this->text('mimetype') ?>; charset=<?php $this->text('charset') ?>" />
 		<?php $this->html('headlinks') ?>
 		<title><?php $this->text('pagetitle') ?></title>
-		<?php $this->html('csslinks') ?>
 		<style type="text/css" media="screen, projection">/*<![CDATA[*/
 			@import "<?php $this->text('stylepath') ?>/common/shared.css?<?php echo $GLOBALS['wgStyleVersion'] ?>";
 			@import "<?php $this->text('stylepath') ?>/<?php $this->text('stylename') ?>/main.css?<?php echo $GLOBALS['wgStyleVersion'] ?>";
 		/*]]>*/</style>
+		<style type="text/css">@import "<?php $this->text('stylepath') ?>/<?php $this->text('stylename') ?>/ewg.css";</style>
 		<link rel="stylesheet" type="text/css" <?php if(empty($this->data['printable']) ) { ?>media="print"<?php } ?> href="<?php $this->text('printcss') ?>?<?php echo $GLOBALS['wgStyleVersion'] ?>" />
+		<?php if( in_array( 'IE50', $skin->cssfiles ) ) { ?><!--[if lt IE 5.5000]><style type="text/css">@import "<?php $this->text('stylepath') ?>/<?php $this->text('stylename') ?>/IE50Fixes.css?<?php echo $GLOBALS['wgStyleVersion'] ?>";</style><![endif]-->
+		<?php } if( in_array( 'IE55', $skin->cssfiles ) ) { ?><!--[if IE 5.5000]><style type="text/css">@import "<?php $this->text('stylepath') ?>/<?php $this->text('stylename') ?>/IE55Fixes.css?<?php echo $GLOBALS['wgStyleVersion'] ?>";</style><![endif]-->
+		<?php } if( in_array( 'IE60', $skin->cssfiles ) ) { ?><!--[if IE 6]><style type="text/css">@import "<?php $this->text('stylepath') ?>/<?php $this->text('stylename') ?>/IE60Fixes.css?<?php echo $GLOBALS['wgStyleVersion'] ?>";</style><![endif]-->
+		<?php } if( in_array( 'IE70', $skin->cssfiles ) ) { ?><!--[if IE 7]><style type="text/css">@import "<?php $this->text('stylepath') ?>/<?php $this->text('stylename') ?>/IE70Fixes.css?<?php echo $GLOBALS['wgStyleVersion'] ?>";</style><![endif]-->
+		<?php } ?><!--[if lt IE 7]><?php if( in_array( 'IE', $skin->cssfiles ) ) { ?><script type="<?php $this->text('jsmimetype') ?>" src="<?php $this->text('stylepath') ?>/common/IEFixes.js?<?php echo $GLOBALS['wgStyleVersion'] ?>"></script>
+		<?php } ?><meta http-equiv="imagetoolbar" content="no" /><![endif]-->
+		
 		<?php print Skin::makeGlobalVariablesScript( $this->data ); ?>
-                <script type="<?php $this->text('jsmimetype') ?>" src="<?php $this->text('stylepath' ) ?>/common/wikibits.js?<?php echo $GLOBALS['wgStyleVersion'] ?>"><!-- wikibits js --></script>
+                
+		<script type="<?php $this->text('jsmimetype') ?>" src="<?php $this->text('stylepath' ) ?>/common/wikibits.js?<?php echo $GLOBALS['wgStyleVersion'] ?>"><!-- wikibits js --></script>
+		<script type="<?php $this->text('jsmimetype') ?>" src="<?php $this->text('stylepath' ) ?>/<?php $this->text('stylename') ?>/findinpage.js?<?php echo $GLOBALS['wgStyleVersion'] ?>"><!-- find in page js --></script>
+		<script type="<?php $this->text('jsmimetype') ?>" src="<?php $this->text('stylepath' ) ?>/<?php $this->text('stylename') ?>/ewg.js ?>"></script>
 		<!-- Head Scripts -->
 <?php $this->html('headscripts') ?>
 <?php	if($this->data['jsvarurl']) { ?>
@@ -90,163 +96,114 @@ class EWGTemplate extends QuickTemplate {
 		<script type="<?php $this->text('jsmimetype') ?>"><?php $this->html('userjsprev') ?></script>
 <?php	}
 		if($this->data['trackbackhtml']) print $this->data['trackbackhtml']; ?>
+
 	</head>
 <body<?php if($this->data['body_ondblclick']) { ?> ondblclick="<?php $this->text('body_ondblclick') ?>"<?php } ?>
 <?php if($this->data['body_onload']) { ?> onload="<?php $this->text('body_onload') ?>"<?php } ?>
  class="mediawiki <?php $this->text('nsclass') ?> <?php $this->text('dir') ?> <?php $this->text('pageclass') ?>">
-<div id="globalWrapper">
-	<div align="center">
-	<br />
-	<table width="95%" cellspacing="0" cellpadding="0">
-		<tr>
-			<td id="border-l" valign="top">
-				<img src="<?php $this->text('stylepath') ?>/<?php $this->text('stylename') ?>/border_tl.jpg" />
-			</td>
-			<td id="bkg" align="left">
-				<table width="100%" cellspacing="0" cellpadding="0">
-					<tr>
-					<td colspan="3" id="kgpersonal">
-						<div class="portlet" id="p-personal">
-							<div id="verytoph" align="left">
-								<ul>
-					<?php 			foreach($this->data['personal_urls'] as $key => $item) { ?>
-									<li id="pt-<?php echo Sanitizer::escapeId($key) ?>"<?php
-										if ($item['active']) { ?> class="active"<?php } ?>><a href="<?php
-									echo htmlspecialchars($item['href']) ?>"<?php echo $skin->tooltipAndAccesskey('pt-'.$key) ?><?php
-									if(!empty($item['class'])) { ?> class="<?php
-									echo htmlspecialchars($item['class']) ?>"<?php } ?>><?php
-									echo htmlspecialchars($item['text']) ?></a></li>
-					<?php			} ?>
-								</ul>
-							</div>
-							</div>
-					</td>
-					</tr>
-					<tr>
-						<td id="kgthec">
-							<div id="kgthel">
-							&nbsp;
-							</div>
-							<div id="kgther">
-							&nbsp;
-							</div>
-						</td>
-					</tr>
-				</table>
-				<table width="100%" cellspacing="0" cellpadding="0">
-					 <tr>
-						<td id="menubar-l">&nbsp;</td>
-						<td id="menubar-c">&nbsp;</td>
-						<td id="menubar-r" valign="top" align="right">
+	<div id="globalWrapper">
 							<div id="p-cactions" class="portlet">
-							<h5><?php $this->msg('views') ?></h5>
-								<div class="pBody">
-									<ul id="kgactul">
-							<?php		foreach($this->data['content_actions'] as $key => $tab) {
-											echo '
-										 <li id="ca-' . Sanitizer::escapeId($key).'"';
-											if( $tab['class'] ) {
-												echo ' class="'.htmlspecialchars($tab['class']).'"';
-											}
-											echo'><a href="'.htmlspecialchars($tab['href']).'"';
-											# We don't want to give the watch tab an accesskey if the
-											# page is being edited, because that conflicts with the
-											# accesskey on the watch checkbox.  We also don't want to
-											# give the edit tab an accesskey, because that's fairly su-
-											# perfluous and conflicts with an accesskey (Ctrl-E) often
-											# used for editing in Safari.
-											if( in_array( $action, array( 'edit', 'submit' ) )
-											&& in_array( $key, array( 'edit', 'watch', 'unwatch' ))) {
-												echo $skin->tooltip( "ca-$key" );
-											} else {
-												echo $skin->tooltipAndAccesskey( "ca-$key" );
-											}
-											echo '>'.htmlspecialchars($tab['text']).'</a></li>';
-										} ?>
-									</ul>
-								</div>
-
-							</div>
-						</td>
-					</tr>
-				</table>
-				<table width="100%" cellspacing="0" cellpadding="0">
-					<tr>
-						<td>
-							<table width="100%" cellspacing="0" cellpadding="0">
-								<tr>
-									<td id="sub-top"></td>
-								</tr>
-								<tr>
-									<td id="lsub" valign="top">
-										<div id="column-one">						
+						<h5><?php $this->msg('views') ?></h5>
+						<div class="pBody">
+							<ul>
+					<?php		foreach($this->data['content_actions'] as $key => $tab) {
+									echo '
+								 <li id="ca-' . Sanitizer::escapeId($key).'"';
+									if( $tab['class'] ) {
+										echo ' class="'.htmlspecialchars($tab['class']).'"';
+									}
+									echo'><a href="'.htmlspecialchars($tab['href']).'"';
+									# We don't want to give the watch tab an accesskey if the
+									# page is being edited, because that conflicts with the
+									# accesskey on the watch checkbox.  We also don't want to
+									# give the edit tab an accesskey, because that's fairly su-
+									# perfluous and conflicts with an accesskey (Ctrl-E) often
+									# used for editing in Safari.
+									if( in_array( $action, array( 'edit', 'submit' ) )
+									&& in_array( $key, array( 'edit', 'watch', 'unwatch' ))) {
+										echo $skin->tooltip( "ca-$key" );
+									} else {
+										echo $skin->tooltipAndAccesskey( "ca-$key" );
+									}
+									echo '>'.htmlspecialchars($tab['text']).'</a></li>';
+								} ?>
+							</ul>
+						</div>
+					</div>
+					<div class="portlet" id="p-personal">
+						<h5><?php $this->msg('personaltools') ?></h5>
+						<div class="pBody">
+							<ul>
+				<?php 			foreach($this->data['personal_urls'] as $key => $item) { ?>
+								<li id="pt-<?php echo Sanitizer::escapeId($key) ?>"<?php
+									if ($item['active']) { ?> class="active"<?php } ?>><a href="<?php
+								echo htmlspecialchars($item['href']) ?>"<?php echo $skin->tooltipAndAccesskey('pt-'.$key) ?><?php
+								if(!empty($item['class'])) { ?> class="<?php
+								echo htmlspecialchars($item['class']) ?>"<?php } ?>><?php
+								echo htmlspecialchars($item['text']) ?></a></li>
+				<?php			} ?>
+							</ul>
+						</div>
+					</div>
+					<div class="portlet" id="p-logo">
+						<a style="background-image: url(<?php $this->text('logopath') ?>);" <?php
+							?>href="<?php echo htmlspecialchars($this->data['nav_urls']['mainpage']['href'])?>"<?php
+							echo $skin->tooltipAndAccesskey('n-mainpage') ?>></a>
+					</div>
+		<table id="column-content" width="!00%" cellpadding="0" cellspacing="0">
+			<tr>
+				<td colspan="2">
+					<div id="top-div" />
+				</td>
+			<tr>
+				 <td id="column-one">
+			<script type="<?php $this->text('jsmimetype') ?>"> if (window.isMSIE55) fixalpha(); </script>
+		
 <?php
 # MediaWiki:Sidebar
-$this->searchBox();
 global $wgUser,$wgTitle,$wgParser;
 $side = new Article(Title::newFromText('Sidebar',NS_MEDIAWIKI));
 if (is_object($wgParser)) { $psr = $wgParser; $opt = $wgParser->mOptions; }
 else { $psr = new Parser; $opt = NULL; }
 if (!is_object($opt)) $opt = ParserOptions::newFromUser($wgUser);
 echo $psr->parse($side->fetchContent(),$wgTitle,$opt,true,true)->getText();
+?><br /><?php
+$this->searchBox();
 ?>
-										</div>
-									</td>
-									<td id="tdmain" valign="top">
-											<div id="leaf">&nbsp;</div>
-											<div id="margindiv">
-												<div id="bodyContent">
-
-													<a name="top" id="top"></a>
-													<?php if($this->data['sitenotice']) { ?><div id="siteNotice"><?php $this->html('sitenotice') ?></div><?php } ?>
-													<h1 class="firstHeading"><?php $this->data['displaytitle']!=""?$this->html('title'):$this->text('title') ?></h1>
-													<div><h3 id="siteSub"><?php $this->msg('tagline') ?></h3>
-													<div id="contentSub"><?php $this->html('subtitle') ?></div>
-													<?php if($this->data['undelete']) { ?><div id="contentSub2"><?php     $this->html('undelete') ?></div><?php } ?>
-													<?php if($this->data['newtalk'] ) { ?><div class="usermessage"><?php $this->html('newtalk')  ?></div><?php } ?>
-													<?php if($this->data['showjumplinks']) { ?><div id="jump-to-nav"><?php $this->msg('jumpto') ?> <a href="#column-one"><?php $this->msg('jumptonavigation') ?></a>, <a href="#searchInput"><?php $this->msg('jumptosearch') ?></a></div><?php } ?>
-<!-- start content -->
-<?php $this->html('bodytext') ?>
-<?php if($this->data['catlinks']) { $this->html('catlinks'); } ?>
-<!-- end content -->
-
-												</div>
-											</div>
-											<div class="visualClear"></div>
-										</div>
-									</td>
-								</tr>
-							</table>
-						</td>
-					</tr>
-					<tr>
-						<td>
-							<table width="100%" cellspacing="0" cellpadding="0">
-								<tr>
-									<td id="logob-l">&nbsp;</td>
-									<td id="logob-c" colspan="2">&nbsp;</td>
-								</tr>
-							</table>
-						</td>
-					</tr>
-				</table>
-			</td>
-			<td>
-			<td id="border-r" valign="top">
-				<img src="<?php $this->text('stylepath') ?>/<?php $this->text('stylename') ?>/border_tr.jpg" />
-			</td>
-		</tr>
-		<tr>
-		<td id="kgbl">&nbsp;</td>
-		<td id="kgb" colspan="2">&nbsp;</td>
-		<td id="kgbr">&nbsp;</td>
-		</tr>
-	</table>
-</div>
-	
-	<!-- end of the left (by default at least) column -->
-			<div class="visualClear"></div>
-			
+				</td><!-- end of the left (by default at least) column -->
+				<td id="content" width="100%">
+					<a name="top" id="top"></a>
+					<?php if($this->data['sitenotice']) { ?><div id="siteNotice"><?php $this->html('sitenotice') ?></div><?php } ?>
+					<h1 class="firstHeading"><?php $this->data['displaytitle']!=""?$this->html('title'):$this->text('title') ?></h1>
+					<div id="bodyContent">
+						<h3 id="siteSub"><?php $this->msg('tagline') ?></h3>
+						<div id="contentSub"><?php $this->html('subtitle') ?></div>
+						<?php if($this->data['undelete']) { ?><div id="contentSub2"><?php     $this->html('undelete') ?></div><?php } ?>
+						<?php if($this->data['newtalk'] ) { ?><div class="usermessage"><?php $this->html('newtalk')  ?></div><?php } ?>
+						<?php if($this->data['showjumplinks']) { ?><div id="jump-to-nav"><?php $this->msg('jumpto') ?> <a href="#column-one"><?php $this->msg('jumptonavigation') ?></a>, <a href="#searchInput"><?php $this->msg('jumptosearch') ?></a></div><?php } ?>
+						<!-- start content -->
+						<?php $this->html('bodytext') ?>
+						<?php if($this->data['catlinks']) { $this->html('catlinks'); } ?>
+						<!-- end content -->
+						<div class="visualClear"></div>
+						</div>
+					</div>
+				</td>
+			</tr>
+		</table>
+		
+		<div class="visualClear"></div>
+		<div id="footer">
+<?php
+global $wgUser, $wgTitle, $wgParser;
+if ( is_object( $wgParser ) ) $psr =& $wgParser; else $psr = new Parser;
+$opt = ParserOptions::newFromUser( $wgUser );
+$nav = new Article( Title::newFromText( 'Footer', NS_MEDIAWIKI ) );
+$out = $psr->parse( $nav->fetchContent( 0, false, false ), $wgTitle, $opt, true, true );
+echo $out->getText();
+?>
+			</div>
+	</div>
 <?php $this->html('bottomscripts'); /* JS call to runBodyOnloadHook */ ?>
 <?php $this->html('reporttime') ?>
 <?php if ( $this->data['debug'] ): ?>
@@ -266,12 +223,14 @@ echo $psr->parse($side->fetchContent(),$wgTitle,$opt,true,true)->getText();
 	<div id="p-search" class="portlet">
 		<h5><label for="searchInput"><?php $this->msg('search') ?></label></h5>
 		<div id="searchBody" class="pBody">
-			<form action="<?php $this->text('searchaction') ?>" id="searchform"><div>
+			<form name="searchform" action="<?php $this->text('searchaction') ?>" id="searchform"><div>
 				<input id="searchInput" name="search" type="text"<?php echo $this->skin->tooltipAndAccesskey('search');
 					if( isset( $this->data['search'] ) ) {
 						?> value="<?php $this->text('search') ?>"<?php } ?> />
-				<input type='submit' name="go" class="searchButton" id="searchGoButton"	value="<?php $this->msg('searcharticle') ?>"<?php echo $this->skin->tooltipAndAccesskey( 'search-go' ); ?> />&nbsp;
-				<input type='submit' name="fulltext" class="searchButton" id="mw-searchButton" value="<?php $this->msg('searchbutton') ?>"<?php echo $this->skin->tooltipAndAccesskey( 'search-fulltext' ); ?> />
+				<input type='submit' name="fulltext" class="searchButton" id="mw-searchButton" value="Go"<?php echo $this->skin->tooltipAndAccesskey( 'search-fulltext' ); ?> />
+				<input type='button' class="searchButton" value="Search" onClick="document.searchform.findthis.value=document.searchform.search.value;findinpage(document.searchform, frametosearch);"
+				/>
+				<input type='hidden' name='findthis' />
 			</div></form>
 		</div>
 	</div>
