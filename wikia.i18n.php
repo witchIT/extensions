13ff114735a3ccefe54d638918eb594055921a11
Aran Dunkley <aran@organicdesign.co.nz>
2010-03-06 23:56:24 +0000

    change over to bullet lists
    git-svn-id: svn+ssh://organicdesign.co.nz/extensions@416 2ad5491c-c162-49ae-9d3b-afa169216a3e

--- wikia.i18n.php
+++ wikia.i18n.php
@@ -26,51 +26,137 @@ $messages['en'] = array(
 	'od-contingency' => 'Includes: Contingencies (how to deal with events when they do not go according to plan).', 
 	'od-description-clear' => 'Make the description clear, assuming no prior context, even if raising it to yourself',
 	'od-datepicker-use' => 'Use Datepicker to select Date',
-	'od-minform-record-list' => '<option>Activity</option><option>Task</option><option>Project</option><option>Document</option><option>Procedure</option><option>Date</option>',
+	'od-minform-record-list' => '*Person
+*Role
+*Document
+*Procedure
+*Project
+*Task
+*Activity',
 	'od-calendar-use' => 'Use Datepicker, then Go to your Calendar',
 	'od-calendar-form' => 'Use Datepicker, then Submit or Search for Date',
 	'od-calendar-another' => 'Use Datepicker, then Go to another Date',
 	'od-textbox-alias' => 'Other addresses that point to this address. Write every entry on a new line.',
 	'od-textbox-forward' => 'Other addresses that emails are forwarded to. Write every entry on a new line.',
-	'od-person-title-list' => '<option>Mr</option><option>Mrs</option><option>Miss</option><option>Ms</option><option>Dr</option>',
+	'od-person-title-list' => '*Mr
+*Mrs
+*Miss
+*Ms
+*Dr',
 	'od-person-title-list-info' => 'This is the list of different titles that a person in the organisation can have. To change the options available in this list click [{{fullurl:MediaWiki:od-person-title-list|action=edit}} here]',
-	'od-person-location-list' => '<option>Head Office</option><option>Orini Factory</option><option>Auckland Sales Office</option>',
+	'od-person-location-list' => '*Head Office
+*Orini Factory
+*Auckland Sales Office',
 	'od-person-location-list-info' => 'This is the list of locations that people in the organisation can work from. To change the options available in this list click [{{fullurl:MediaWiki:od-person-location-list|action=edit}} here]',
 	'od-person-administration-info' => '{{warning|This section is only viewable by system administrators because it defines peoples roles and access rights.}}',
 	'od-person-external-info' => 'External contributors can access nothing by default, they must be granted specific access to articles to be able to view them.',
-	'od-procedure-status-list' => '<option>Planned</option><option>Work in Progress</option><option>Final Draft</option><option>Completed</option> <option>Signed Off</option><option>Invoiced</option><option>Paid</option><option>Cancelled</option>',
+	'od-procedure-status-list' => '*Planned
+*Work in Progress
+*Final Draft
+*Completed
+*Signed Off
+*Invoiced
+*Paid
+*Cancelled',
 	'od-procedure-status-list-info' => 'The list of current states that a procedure in the organisation can be in. To change the options available in this list click [{{fullurl:MediaWiki:od-procedure-status-list|action=edit}} here]',
-	'od-procedure-priority-list' => '<option>1 - Urgent</option> <option>2 - High</option><option>3 - Standard</option><option>4 - Low</option>',
+	'od-procedure-priority-list' => '*1 - Urgent
+*2 - High
+*3 - Standard
+*4 - Low',
 	'od-procedure-priority-list-info' => 'The list of different priorities that a procedure in the organisation can have. To change the options available in this list click [{{fullurl:MediaWiki:od-procedure-priority-list|action=edit}} here]',
-	'od-procedure-version-list' => '<option>New</option><option>1.0</option><option>2.0</option><option>3.0</option>',
+	'od-procedure-version-list' => '*New
+*1.0
+*2.0
+*3.0',
 	'od-procedure-version-list-info' => 'The list of different versions that a procedure in the organisation can be. To change the options available in this list click [{{fullurl:MediaWiki:od-procedure-version-list|action=edit}} here]',
-	'od-project-type-list' => '<option>Specification</option><option>Development</option><option>Consultancy</option><option>Documentation</option><option>Content</option><option>Systems Administration</option>',
+	'od-project-type-list' => '*Specification
+*Development
+*Consultancy
+*Documentation
+*Content
+*Systems Administration',
 	'od-project-type-list-info' => 'The list of different types that a project in the organisation can be. To change the options available in this list click [{{fullurl:MediaWiki:od-project-type-list|action=edit}} here]',
-	'od-project-status-list' => '<option>Planned</option><option>Work in Progress</option><option>Final Draft</option><option>Completed</option><option>Signed Off</option><option>Invoiced</option><option>Paid</option><option>Cancelled</option>',
+	'od-project-status-list' => '*Planned
+*Work in Progress
+*Final Draft
+*Completed
+*Signed Off
+*Invoiced
+*Paid
+*Cancelled',
 	'od-project-status-list-info' => 'The list of current states that a project in the organisation can be in. To change the options available in this list click [{{fullurl:MediaWiki:od-project-status-list|action=edit}} here]',
-	'od-task-budgettype-list' => '<option>Hours</option><option>NZD</option><option>USD</option><option>EUR</option>',
+	'od-task-budgettype-list' => '*Hours
+*NZD
+*USD
+*EUR',
 	'od-task-budgettype-list-info' => 'The list of currencies for the task\'s budget. To change the options available in this list click [{{fullurl:MediaWiki:od-task-budgettype-list|action=edit}} here]',
-	'od-task-type-list' => '<option>Request</option><option>Inquiry</option><option>Problem</option><option>Task</option><option>Regular Task</option><option>Ongoing Task</option><option>Procedure</option><option>Milestone</option>',
+	'od-task-type-list' => '*Request
+*Inquiry
+*Problem
+*Task
+*Regular Task
+*Ongoing Task
+*Procedure
+*Milestone',
 	'od-task-type-list-info' => 'The list of different types that a task in the organisation can be. To change the options available in this list click [{{fullurl:MediaWiki:od-task-type-list|action=edit}} here]',
-	'od-task-status-list' => '<option>Planned</option><option>Assigned</option><option>In Progress</option><option>Resolved</option> <option>Invoiced</option> <option>Paid</option><option>Postponed</option><option>Cancelled</option>',
+	'od-task-status-list' => '*Planned
+*Assigned
+*In Progress
+*Resolved
+*Invoiced
+*Paid
+*Postponed
+*Cancelled',
 	'od-task-status-list-info' => 'The list of current states that a task in the organisation can be in. To change the options available in this list click [{{fullurl:MediaWiki:od-task-status-list|action=edit}} here]',
-	'od-task-priority-list' => '<option>1 - Urgent</option><option>2 - High</option><option>3 - Medium</option><option>4 - Low</option>',
+	'od-task-priority-list' => '*1 - Urgent
+*2 - High
+*3 - Medium
+*4 - Low',
 	'od-task-priority-list-info' => 'The list of different priorities that a task in the organisation can have. To change the options available in this list click [{{fullurl:MediaWiki:od-task-priority-list|action=edit}} here]',
-	'od-activity-currency-list' => '<option>NZD</option><option>EUR</option><option>USD</option><option>CHF</option>',
+	'od-activity-currency-list' => '*NZD
+*EUR
+*USD
+*CHF',
 	'od-activity-currency-list-info' => 'The list of currencies for the activity\'s budget. To change the options available in this list click [{{fullurl:MediaWiki:od-activity-currency-list|action=edit}} here]',
-	'od-activity-type-list' => '<option>Work</option><option>Procedure</option><option>Meeting</option><option>Weekly Meeting</option><option>Discussion</option><option>Travel</option>',
+	'od-activity-type-list' => '*Work
+*Procedure
+*Meeting
+*Weekly Meeting
+*Discussion
+*Travel',
 	'od-activity-type-list-info' => 'The list of different types that an activity in the organisation can be. To change the options available in this list click [{{fullurl:MediaWiki:od-activity-type-list|action=edit}} here]',
-	'od-activity-status-list' => '<option>Planned</option><option>Work in Progress</option><option>Final Draft</option><option>Completed</option><option>Signed Off</option><option>Invoiced</option><option>Paid</option><option>Cancelled</option>',
+	'od-activity-status-list' => '*Planned
+*Work in Progress
+*Final Draft
+*Completed
+*Signed Off
+*Invoiced
+*Paid
+*Cancelled',
 	'od-activity-status-list-info' => 'The list of current states that an activity in the organisation can be in. To change the options available in this list click [{{fullurl:MediaWiki:od-activity-status-list|action=edit}} here]',
-	'od-role-group-list' => '<option>Adminstrator</option><option>Moderator</option><option>Editor</option><option>Developer</option><option>User</option>',
-	'od-role-group-list-info' => 'The list of groups that a role in the organisation can be in. To change the options available in this list click [{{fullurl:MediaWiki:od-role-group-list|action=edit}} here]',
-	'od-role-smb-list' => '<option>earthwise-management</option><option>earthwise-staff</option><option>kiwigreen-management</option><option>kiwigreen-staff</option><option>shared-management</option>',
+	'od-role-smb-list' => '*earthwise-management
+*earthwise-staff
+*kiwigreen-management
+*kiwigreen-staff
+*shared-management',
 	'od-role-smb-list-info' => 'The list of shares that a role in the organisation can have. To change the options available in this list click [{{fullurl:MediaWiki:od-role-smb-list|action=edit}} here]',
-	'od-document-type-list' => '<option>Specification</option><option>Development</option><option>Consultancy</option><option>Memo</option><option>Letter</option><option>Prospectus</option>',
+	'od-document-type-list' => '*Specification
+*Development
+*Consultancy
+*Memo
+*Letter
+*Prospectus',
 	'od-document-type-list-info' => 'The list of different types that a document in the organisation can be. To change the options available in this list click [{{fullurl:MediaWiki:od-document-type-list|action=edit}} here]',
-	'od-document-status-list' => '<option>Work in Progress</option><option>Completed</option><option>Signed Off</option>',
+	'od-document-status-list' => '*Work in Progress
+*Completed
+*Signed Off',
 	'od-document-status-list-info' => 'The list of current states that a document in the organisation can be in. To change the options available in this list click [{{fullurl:MediaWiki:od-document-status-list|action=edit}} here]',
-	'od-document-category-list' => '<option>Directors</option><option>Management</option><option>Sales</option><option>Factory</option><option>R&D</option><option>Customer Services</option>',
+	'od-document-category-list' => '*Directors
+*Management
+*Sales
+*Factory
+*R&D
+*Customer Services',
 	'od-document-category-list-info' => 'The list of categories that a document in the organisation can be assigned to. To change the options available in this list click [{{fullurl:MediaWiki:od-document-category-list|action=edit}} here]',
 	'sidebar' => "{{#tree:id=side-bar-tree|openlevels=1|root=<big>''' {{ns:4}}    '''</big>|
 *[[{{CURRENTPERSON}}|My Home Page]]
@@ -102,5 +188,6 @@ $messages['en'] = array(
 **[[Special:Specialpages|Special pages]]
 **[{{fullurl:Special:Allpages|namespace=8}} Custom messages]
 **[{{fullurl:MediaWiki:Sidebar|action=edit}} Edit Sidebar]
-}}"
+}}",
+	'footer' => '[[OD:Wiki organisation|About Wiki Organisation]]'
 );
