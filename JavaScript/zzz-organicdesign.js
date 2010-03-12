abfd64060cc47c096a087e2bb967f2d7472bb0f4
Aran Dunkley <aran@organicdesign.co.nz>
2010-03-12 04:34:44 +0000

    and visa versa, populate record-name from ra-title on load
    git-svn-id: svn+ssh://organicdesign.co.nz/extensions@456 2ad5491c-c162-49ae-9d3b-afa169216a3e

--- JavaScript/zzz-organicdesign.js
+++ JavaScript/zzz-organicdesign.js
@@ -29,6 +29,7 @@ function odOnLoadHook() {
 			$('#record-name').hide();
 		} else {
 			$('#record-name input').addClass(':required');
+			$('#record-name input').val($('ra-title').val());
 			$('form.recordadmin').attr('onSubmit','$("#ra-title").val($("#record-name input").val())');
 		}
 	}
