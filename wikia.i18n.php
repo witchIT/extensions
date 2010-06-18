9d282c8d1c581ad905b90c7a712e92a355d1036c
Aran Dunkley <aran@organicdesign.co.nz>
2010-06-18 22:27:51 +0000

    use od-sidebar now
    git-svn-id: svn+ssh://organicdesign.co.nz/extensions@503 2ad5491c-c162-49ae-9d3b-afa169216a3e

--- wikia.i18n.php
+++ wikia.i18n.php
@@ -179,7 +179,7 @@ $messages['en'] = array(
 *Customer Services',
 	'od-document-category-list-info' => 'The list of categories that a document in the organisation can be assigned to. To change the options available in this list click [{{fullurl:MediaWiki:od-document-category-list|action=edit}} here]',
 	'od-document-edit-info' => 'To edit this document, click the "edit" link above as usual and then add the content below the existing properties infomration.',
-	'sidebar' => "{{#ifgroup:user|{{#tree:id=sidebartree|root=<big>'''{{ns:4}}'''</big>|
+	'od-sidebar' => "{{#ifgroup:user|{{#tree:id=sidebartree|root=<big>'''{{ns:4}}'''</big>|
 *[[{{CURRENTPERSON}}|My Home Page]]
 *[[Main Page]]
 *[[Special:Recentchanges|Recent changes]]
