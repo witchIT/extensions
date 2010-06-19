7aeb42964efb7107c74d718457cda59d5b07c5b3
Aran Dunkley <aran@organicdesign.co.nz>
2010-06-19 09:16:54 +0000

    add more settings etc required to make a wiki org from a standalone wiki config
    git-svn-id: svn+ssh://organicdesign.co.nz/extensions@504 2ad5491c-c162-49ae-9d3b-afa169216a3e

--- LocalSettings.sample.php
+++ LocalSettings.sample.php
@@ -1,25 +1,56 @@
 <?php
-# Sample LocalSettings.php file to be used with MediaWiki's set up as OD wiki organisations
-
+# This is a sample of settings to be appended to a LocalSettings.php file
+# for a wiki which is not part of an Organic Design wikia installation.
+#
+# These settings prepare a wiki for the Wiki Organisation extensions
+#
 $wgSitename          = "Sample";
 $wgShortName         = "foo";
 $wgDBname            = "foo";
 $wgDBprefix          = "foo_";
 
-define( 'NS_FORM', 2000 );
-$wgExtraNamespaces[NS_FORM]   = 'Form';
-$wgExtraNamespaces[NS_FORM+1] = 'Form_talk';
+
+
+# Namespaces
+define( 'NS_FORM', 106  );
+$wgExtraNamespaces[NS_FORM]        = 'Form';
+$wgExtraNamespaces[NS_FORM + 1]    = 'Form_talk';
+
+# General settings
+$wgUseDatabaseMessages    = true;
+$wgSecurityUseDBHook      = true;
+$wgVerifyMimeType         = false;
+$wgSVGConverter           = 'rsvg';
+$wgEmergencyContact       = false;
+$wgRawHtml                = true;
+$wgUseSiteCss             = true;
+$wgUseSiteJs              = true;
+$wgUseWikiaCss            = true;
+
+# File upload settings
+$wgEnableUploads          = true;
+$wgAllowCopyUploads       = true;
+$wgFileExtensions         = array(
+        'jpeg', 'jpg', 'png', 'gif', 'svg', 'swf',
+        'pdf', 'xls', 'ods', 'odt', 'doc', 'docx', 'mm',
+        'zip', '7z', 'gz', 'tgz', 't7z',
+        'avi', 'divx', 'mpeg', 'mpg', 'ogv', 'ogm', 'mp3', 'mp4', 'flv',
+        'torrent'
+);
+$wgGroupPermissions['sysop']['upload_by_url'] = true;
+
+# Add Organic Design i18n messages
+$wgExtensionMessagesFiles['OD'] = "$IP/extensions/wikia.i18n.php";
 
 # Don't include extension code if running from shell for maintenance
 if ( !$wgCommandLineMode ) {
 
+	# Add Organic Design CSS
+	if ( $wgUseWikiaCss ) $wgHooks['BeforePageDisplay'][] = 'odAddWikiaCss';
+
 	# Force skin to DCS
 	$wgDefaultSkin = 'foo';
 	$wgExtensionFunctions[] = 'wfForceSkin';
-	function wfForceSkin() {
-		global $wgUser;
-		$wgUser->setOption( 'skin', 'foo' );
-	}
 
 	# Force timezone default to NZ
 	$wgLocaltimezone = "Pacific/Auckland";
@@ -39,7 +70,8 @@ if ( !$wgCommandLineMode ) {
 	) );
 
 	# Example of adding codes for google analytics
-	$wgGoogleTrackingCodes[]  = 'UA-1234567-1';
+	$wgGoogleTrackingCodes = array( 'UA-1234567-1' );
+	$wgExtensionFunctions[] = 'wfGoogleAnalytics';
 
 	# General extensions
 	include( 'extensions/Nuke/SpecialNuke.php' );
@@ -80,16 +112,6 @@ if ( !$wgCommandLineMode ) {
 
 	# Allow articles in Category:Public to be publically accessible
 	$wgHooks['UserGetRights'][] = 'wfPublicCat';
-	function wfPublicCat() {
-		global $wgTitle, $wgWhitelistRead;
-		$id   = $wgTitle->getArticleID();
-		$dbr  = wfGetDB( DB_SLAVE );
-		$cat  = $dbr->addQuotes( 'Public' );
-		$cl   = $dbr->tableName( 'categorylinks' );
-		if ( $dbr->selectRow( $cl, '0', "cl_from = $id AND cl_to = $cat" ) )
-			$wgWhitelistRead[] = $wgTitle->getText();
-		return true;
-	}
 
 	# Bot jobs
 	include( '/var/www/tools/jobs/ImportCSV.php' );
@@ -101,4 +123,34 @@ if ( !$wgCommandLineMode ) {
 	$wgTreeViewImages['folderOpen'] = "Folder_opn_mist.gif";
 }
 
+function odAddWikiaCss( &$out, $skin = false ) {
+	global $wgScriptPath;
+	$out->addScript("<link rel=\"stylesheet\" type=\"text/css\" href=\"$wgScriptPath/extensions/wikia.css\" />");
+	return true;
+}
 
+function wfForceSkin() {
+	global $wgUser;
+	$wgUser->setOption( 'skin', 'foo' );
+}
+
+function wfPublicCat() {
+	global $wgTitle, $wgWhitelistRead;
+	$id   = $wgTitle->getArticleID();
+	$dbr  = wfGetDB( DB_SLAVE );
+	$cat  = $dbr->addQuotes( 'Public' );
+	$cl   = $dbr->tableName( 'categorylinks' );
+	if ( $dbr->selectRow( $cl, '0', "cl_from = $id AND cl_to = $cat" ) )
+		$wgWhitelistRead[] = $wgTitle->getText();
+	return true;
+}
+
+function wfGoogleAnalytics() {
+	global $wgOut, $wgGoogleTrackingCodes;
+	foreach ( $wgGoogleTrackingCodes as $code ) $wgOut->addScript( '<script type="text/javascript">
+		var gaJsHost = (("https:" == document.location.protocol) ? "https://ssl." : "http://www.");
+		document.write(unescape("%3Cscript src=\'" + gaJsHost + "google-analytics.com/ga.js\' type=\'text/javascript\'%3E%3C/script%3E"));
+		</script><script type="text/javascript">
+		var pageTracker = _gat._getTracker("' . $code . '");
+		pageTracker._trackPageview();</script>' );
+}
