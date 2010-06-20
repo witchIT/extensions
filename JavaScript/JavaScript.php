ce4c552a3d2664a6c0e3cb00ad2a85850a64dbce
Aran Dunkley <aran@organicdesign.co.nz>
2010-06-20 06:14:40 +0000

    ereg deprecated, change to preg
    git-svn-id: svn+ssh://organicdesign.co.nz/extensions@506 2ad5491c-c162-49ae-9d3b-afa169216a3e

--- JavaScript/JavaScript.php
+++ JavaScript/JavaScript.php
@@ -10,13 +10,13 @@
  */
 if ( !defined( 'MEDIAWIKI' ) ) die( 'Not an entry point.' );
 
-define( 'JAVASCRIPT_VERSION', '1.0.1, 2009-11-11' );
+define( 'JAVASCRIPT_VERSION', '1.0.2, 2010-06-20' );
 
 $wgExtensionCredits['other'][] = array(
-	'name'        => 'JavaScript',
-	'author'      => '[http://www.organicdesign.co.nz/nad User:Nad]',
-	'description' => 'Includes all *.js files in the directory containing this script',
-	'url'         => 'http://www.organicdesign.co.nz/Extension:JavaScript',
+	'name'        => "JavaScript",
+	'author'      => "[http://www.organicdesign.co.nz/nad User:Nad]",
+	'description' => "Includes all *.js files in the directory containing this script",
+	'url'         => "http://www.organicdesign.co.nz/Extension:JavaScript",
 	'version'     => JAVASCRIPT_VERSION
 );
 
@@ -25,7 +25,7 @@ $wgHooks['BeforePageDisplay'][] = 'wfJavaScriptAddScripts';
 function wfJavaScriptAddScripts( &$out, $skin = false ) {
 	global $wgJsMimeType, $wgScriptPath;
 	foreach ( glob( dirname( __FILE__ ) . "/*.js" ) as $file ) {
-		$file = ereg_replace( "^.*/extensions/", "$wgScriptPath/extensions/", $file );
+		$file = preg_replace( "|^.*/extensions/|", "$wgScriptPath/extensions/", $file );
 		$out->addScript( "<script src='$file' type='$wgJsMimeType'></script>" );
 	}
 	return true;
