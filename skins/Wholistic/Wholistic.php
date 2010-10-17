bceb5f1831d953b89369535264916d3764111316
Aran Dunkley <aran@organicdesign.co.nz>
2010-10-17 21:25:54 +0000

    add bex skin
    git-svn-id: svn+ssh://organicdesign.co.nz/extensions@596 2ad5491c-c162-49ae-9d3b-afa169216a3e

---
+++ skins/Wholistic/Wholistic.php
@@ -0,0 +1,185 @@
+<?php
+/**
+ * Wholistic nouveau
+ *
+ * Translated from gwicke's previous TAL template version to remove
+ * dependency on PHPTAL.
+ *
+ * @todo document
+ * @file
+ * @ingroup Skins
+ */
+
+if( !defined( 'MEDIAWIKI' ) )
+	die( -1 );
+
+/**
+ * Inherit main code from SkinTemplate, set the CSS and template filter.
+ * @todo document
+ * @ingroup Skins
+ */
+class SkinWholistic extends SkinTemplate {
+
+	var $skinname = 'wholistic', $stylename = 'wholistic',
+		$template = 'WholisticTemplate', $useHeadElement = true;
+
+	function setupSkinUserCss( OutputPage $out ) {
+		global $wgHandheldStyle;
+
+		parent::setupSkinUserCss( $out );
+
+		// Append to the default screen common & print styles...
+		$out->addStyle( 'wholistic/main.css', 'screen' );
+		if( $wgHandheldStyle ) {
+			// Currently in testing... try 'chick/main.css'
+			$out->addStyle( $wgHandheldStyle, 'handheld' );
+		}
+
+		$out->addStyle( 'wholistic/IE50Fixes.css', 'screen', 'lt IE 5.5000' );
+		$out->addStyle( 'wholistic/IE55Fixes.css', 'screen', 'IE 5.5000' );
+		$out->addStyle( 'wholistic/IE60Fixes.css', 'screen', 'IE 6' );
+		$out->addStyle( 'wholistic/IE70Fixes.css', 'screen', 'IE 7' );
+
+		$out->addStyle( 'wholistic/rtl.css', 'screen', '', 'rtl' );
+
+	}
+}
+
+/**
+ * @todo document
+ * @ingroup Skins
+ */
+class WholisticTemplate extends QuickTemplate {
+	var $skin;
+	/**
+	 * Template filter callback for Wholistic skin.
+	 * Takes an associative array of data set from a SkinTemplate-based
+	 * class, and a wrapper for MediaWiki's localization database, and
+	 * outputs a formatted page.
+	 *
+	 * @access private
+	 */
+	function execute() {
+		global $wgRequest;
+
+		$this->skin = $skin = $this->data['skin'];
+		$action = $wgRequest->getText( 'action' );
+
+		// Suppress warnings to prevent notices about missing indexes in $this->data
+		wfSuppressWarnings();
+
+		$this->html( 'headelement' );
+?><div id="globalWrapper"><table width="100%"><tr><td align="center">
+<div id="content" <?php $this->html("specialpageattributes") ?>>
+
+<?php
+global $wgUser, $wgTitle, $wgParser;
+$article = new Article( Title::newFromText( 'Wholistic Panel', NS_TEMPLATE ) );
+$text = $article->fetchContent();
+if ( is_object( $wgParser ) ) { $psr = $wgParser; $opt = $wgParser->mOptions; }
+else { $psr = new Parser; $opt = NULL; }
+if ( !is_object( $opt ) ) $opt = ParserOptions::newFromUser( $wgUser );
+echo $psr->parse( $text, $wgTitle, $opt, true, true )->getText();
+?>
+
+	<a id="top"></a>
+	<?php if($this->data['sitenotice']) { ?><div id="siteNotice"><?php $this->html('sitenotice') ?></div><?php } ?>
+
+	<h1 id="firstHeading" class="firstHeading"><?php $this->html('title') ?></h1>
+	<div id="bodyContent">
+		<h3 id="siteSub"><?php $this->msg('tagline') ?></h3>
+		<div id="contentSub"<?php $this->html('userlangattributes') ?>><?php $this->html('subtitle') ?></div>
+<?php if($this->data['undelete']) { ?>
+		<div id="contentSub2"><?php $this->html('undelete') ?></div>
+<?php } ?><?php if($this->data['newtalk'] ) { ?>
+		<div class="usermessage"><?php $this->html('newtalk')  ?></div>
+<?php } ?><?php if($this->data['showjumplinks']) { ?>
+		<div id="jump-to-nav"><?php $this->msg('jumpto') ?> <a href="#column-one"><?php $this->msg('jumptonavigation') ?></a>, <a href="#searchInput"><?php $this->msg('jumptosearch') ?></a></div>
+<?php } ?>
+		<!-- start content -->
+<?php $this->html('bodytext') ?>
+		<!-- end content -->
+		<?php if($this->data['dataAfterContent']) { $this->html ('dataAfterContent'); } ?>
+		<div class="visualClear"></div>
+	</div>
+</div>
+</td></tr></table></div>
+<div id="column-one"<?php $this->html('userlangattributes')  ?>>
+	<div id="p-cactions" class="portlet">
+		<h5><?php $this->msg('views') ?></h5>
+		<div class="pBody">
+			<ul><?php
+				foreach($this->data['content_actions'] as $key => $tab) {
+					echo '
+				 <li id="' . Sanitizer::escapeId( "ca-$key" ) . '"';
+					if( $tab['class'] ) {
+						echo ' class="'.htmlspecialchars($tab['class']).'"';
+					}
+					echo '><a href="'.htmlspecialchars($tab['href']).'"';
+					# We don't want to give the watch tab an accesskey if the
+					# page is being edited, because that conflicts with the
+					# accesskey on the watch checkbox.  We also don't want to
+					# give the edit tab an accesskey, because that's fairly su-
+					# perfluous and conflicts with an accesskey (Ctrl-E) often
+					# used for editing in Safari.
+				 	if( in_array( $action, array( 'edit', 'submit' ) )
+				 	&& in_array( $key, array( 'edit', 'watch', 'unwatch' ))) {
+				 		echo $skin->tooltip( "ca-$key" );
+				 	} else {
+				 		echo $skin->tooltipAndAccesskey( "ca-$key" );
+				 	}
+				 	echo '>'.htmlspecialchars($tab['text']).'</a></li>';
+				} ?>
+
+			</ul>
+		</div>
+	</div>
+	<div class="portlet" id="p-personal">
+		<h5><?php $this->msg('personaltools') ?></h5>
+		<div class="pBody">
+			<ul<?php $this->html('userlangattributes') ?>>
+<?php 			foreach($this->data['personal_urls'] as $key => $item) { ?>
+				<li id="<?php echo Sanitizer::escapeId( "pt-$key" ) ?>"<?php
+					if ($item['active']) { ?> class="active"<?php } ?>><a href="<?php
+				echo htmlspecialchars($item['href']) ?>"<?php echo $skin->tooltipAndAccesskey('pt-'.$key) ?><?php
+				if(!empty($item['class'])) { ?> class="<?php
+				echo htmlspecialchars($item['class']) ?>"<?php } ?>><?php
+				echo htmlspecialchars($item['text']) ?></a></li>
+<?php			} ?>
+			</ul>
+		</div>
+	</div>
+
+<?php
+global $wgUser, $wgTitle, $wgParser;
+$title = 'od-sidebar'; # note the lcfirst is important here since it's also a msg key
+$article = new Article( Title::newFromText( $title, NS_MEDIAWIKI ) );
+$text = $article->fetchContent();
+if ( empty( $text ) ) $text = wfMsg( $title );
+if ( is_object( $wgParser ) ) { $psr = $wgParser; $opt = $wgParser->mOptions; }
+else { $psr = new Parser; $opt = NULL; }
+if ( !is_object( $opt ) ) $opt = ParserOptions::newFromUser( $wgUser );
+echo $psr->parse( $text, $wgTitle, $opt, true, true )->getText();
+?>
+
+	<script type="<?php $this->text('jsmimetype') ?>"> if (window.isMSIE55) fixalpha(); </script>
+
+</div><!-- end of the left (by default at least) column -->
+<div class="visualClear"></div>
+
+<?php $this->html('bottomscripts'); /* JS call to runBodyOnloadHook */ ?>
+<?php $this->html('reporttime') ?>
+<?php if ( $this->data['debug'] ): ?>
+<!-- Debug output:
+<?php $this->text( 'debug' ); ?>
+
+-->
+<?php endif; ?>
+</body></html>
+<?php
+	wfRestoreWarnings();
+	} // end of execute() method
+
+} // end of class
+
+
