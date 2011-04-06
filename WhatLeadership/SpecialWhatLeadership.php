2a1fe851e392aa77649c82ca4aa1571e1ca169ef
Aran Dunkley <aran@organicdesign.co.nz>
2011-04-06 10:54:40 +0000

    update
    git-svn-id: svn+ssh://organicdesign.co.nz/extensions@653 2ad5491c-c162-49ae-9d3b-afa169216a3e

--- WhatLeadership/SpecialWhatLeadership.php
+++ WhatLeadership/SpecialWhatLeadership.php
@@ -33,26 +33,52 @@ require_once( "$IP/includes/SpecialPage.php" );
 class WhatLeadership extends SpecialPage {
 
 	function __construct() {
+		global $wgHooks;
 		SpecialPage::SpecialPage( 'WhatLeadership', 'sysop', true, false, false, false );
+		$wgHooks['UnknownAction'][] = $this;
 	}
 
 	function execute( $param ) {
-		global $wgParser, $wgOut;
+		global $wgParser, $wgOut, $wgLang;
 		$wgParser->disableCache();
 		$this->setHeaders();
-
-
+		$wgOut->addHTML( "<table class='sortable changes'>" );
+		$wgOut->addHTML( "<tr><th>Book</th><th>Last updated</th><th>Last checked</th></tr>" );
+		foreach( $this->books() as $book => $time ) {
+			if( $time == 0 ) $time = "<i>Never updated</i>";
+			else {
+				#preg_match( "|^(\d\d\d\d)(\d\d)(\d\d)(\d\d)(\d\d)|", $time, $m );
+				#$time = "$m[1]-$m[2]-$m[3], $m[4]:$m[5]";
+				$time = $wgLang->timeanddate($time, true );
+			}
+			$url = Title::newFromText( $book )->getLocalURL();
+			$wgOut->addHTML( "<tr><td><a href='$url'>$book</a></td><td>$time</td></tr>" );
+		}
+		$wgOut->addHTML( "</table>" );
 	}
+
+	/*
+	 * Return the data for the last updated book
+	 */
+	function onUnknownAction( $action, $article ) {
+		global $wgOut;
+		if( $action == 'oldestbook' ) {
+			$wgOut->disable();
+			$book = $this->books( 1 );
+			print_r($book);
+		}
+		return true;
+	}
+
 
 	/*
 	 * Get a list of books
 	 */
 	function books( $limit = false, $rev = false ) {
 		global $wgWhatLeadershipTemplate;
-		$limit = $limit ? " LIMIT $count" : "";
-		$desc = $rev ? " DESC" : "";
 		$dbr = wfGetDB( DB_SLAVE );
 		$books = array();
+		$titles = array();
 
 		# Get page id list of all books (articles using the Book template)
 		$table = $dbr->tableName( 'templatelinks' );
@@ -63,19 +89,20 @@ class WhatLeadership extends SpecialPage {
 
 		# Get the data age of each book
 		$table = $dbr->tableName( 'revision' );
-		$cond = "rev_comment LIKE '%whatleadership.pl' ORDER BY rev_timestamp DESC LIMIT 1";
+		$cond = "rev_comment LIKE '%whatleadership.pl' ORDER BY rev_timestamp DESC";
 		foreach( $books as $page => $data ) {
-			if( $row = $dbr->selectRow( $table, 'tl_from', "rev_page = $page AND $cond" ) )
-				$books[Title::newFromID( $page )->getPrefixedText()] = $row[0];
+			$title = Title::newFromID( $page )->getPrefixedText();
+			if( $row = $dbr->selectRow( $table, 'rev_timestamp', "rev_page = $page AND $cond" ) ) $titles[$title] = $row->rev_timestamp;
+			else $titles[$title] = 0;
 		}
 
-		# Sort books into newest first
-		arsort( $books );
+		# Sort books;
+		$rev ? arsort( $titles ) : asort( $titles );
 
 		# Chop if limit supplied
-		if( $limit > 0 && $limit < count( $books ) ) $books = array_splice( $books, 0, $limit );
+		if( $limit > 0 && $limit < count( $titles ) ) $titles = array_slice( $titles, 0, $limit, true );
 
-		return $books;
+		return $titles;
 	}
 
 }
