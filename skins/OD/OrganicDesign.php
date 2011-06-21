e382336f00a1e8d395630cd4733c5e3f1c9ad232
Aran Dunkley <aran@organicdesign.co.nz>
2011-06-21 23:30:29 +0000

    readonly, not disable
    git-svn-id: svn+ssh://organicdesign.co.nz/extensions@683 2ad5491c-c162-49ae-9d3b-afa169216a3e

--- skins/OD/OrganicDesign.php
+++ skins/OD/OrganicDesign.php
@@ -149,7 +149,7 @@ if ( $wgOrganicDesignDonations ) {?>
 		<br />
 		<h5>We accept <a href="/Bitcoin">Bitcoins</a>!</h5>
 		<div class="pBody" style="white-space:nowrap;vertical-align:top;background:url(/files/a/a0/Bitcoin-icon.png) no-repeat 5px 2px;">
-			<input style="width:135px;margin-left:23px" disabled="1" value="1PVoayGWHbShwM6m5r2qACURBMMfTmR5jT" onmouseover="this.select()" />
+			<input style="width:135px;margin-left:23px" readonly="1" value="1PVoayGWHbShwM6m5r2qACURBMMfTmR5jT" onmouseover="this.select()" />
 		</div>
 	</div>
 <?php }?>
