1788590ea599db7ffb32d6ffeaedf3a430cb4a42
Aran Dunkley <aran@organicdesign.co.nz>
2011-08-02 22:44:07 +0000

    update for 117
    git-svn-id: svn+ssh://organicdesign.co.nz/extensions@713 2ad5491c-c162-49ae-9d3b-afa169216a3e

--- JavaScript/organicdesign.js
+++ JavaScript/organicdesign.js
@@ -3,21 +3,25 @@
  */
 
 // OD functions to run after page load
-window.onload = function() {
+( function( $, mw ) {
 
-	// A fix for table.js to allow it to handle dates which include HH:MM time
-	window.Sort.date.formats[0] = {
-		re : /(\d{2,4})-(\d{1,2})-(\d{1,2})\D*((\d\d):(\d\d))?/,
-		f  : function(x) {
-			var d = new Date(Sort.date.fixYear(x[1]), +x[2], +x[3]);
-			if (x[5] > 0) d.setHours(x[5]);
-			if (x[6] > 0) d.setMinutes(x[6]);
-			return d.getTime();
-		}
+// A fix for table.js to allow it to handle dates which include HH:MM time
+/*
+* 	window.Sort.date.formats[0] = {
+	re : /(\d{2,4})-(\d{1,2})-(\d{1,2})\D*((\d\d):(\d\d))?/,
+	f  : function(x) {
+		var d = new Date(Sort.date.fixYear(x[1]), +x[2], +x[3]);
+		if (x[5] > 0) d.setHours(x[5]);
+		if (x[6] > 0) d.setMinutes(x[6]);
+		return d.getTime();
 	}
+}
+*/
 
 	// Make vanadium validation not work for RecordAdmin searches
 	$('#ra-find').attr('onClick','Vanadium={}');
+
+	$('a').css('background-color','yellow');
 
 	// Improve RA record name inputs
 	// - normal record-id is always hidden (css)
@@ -33,5 +37,5 @@ window.onload = function() {
 			$('form.recordadmin').attr('onSubmit', submit + '$("#ra-title").val($("#record-name input").val() );');
 		}
 	}
-}
 
+} )( jQuery, mediaWiki );
