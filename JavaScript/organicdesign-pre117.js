b4a576176e3f7eff3eadad24d2dc5aeb612275a0
Aran Dunkley <aran@organicdesign.co.nz>
2011-08-03 04:11:02 +0000

    pdate here
    git-svn-id: svn+ssh://organicdesign.co.nz/extensions@724 2ad5491c-c162-49ae-9d3b-afa169216a3e

---
+++ JavaScript/organicdesign-pre117.js
@@ -0,0 +1,43 @@
+/*
+ * Various patches and JS additions needed by wikis in the OD wikia
+ */
+
+odOnloadHook = function() {
+	
+// A fix for table.js to allow it to handle dates which include HH:MM time
+/*
+* 	window.Sort.date.formats[0] = {
+	re : /(\d{2,4})-(\d{1,2})-(\d{1,2})\D*((\d\d):(\d\d))?/,
+	f  : function(x) {
+		var d = new Date(Sort.date.fixYear(x[1]), +x[2], +x[3]);
+		if (x[5] > 0) d.setHours(x[5]);
+		if (x[6] > 0) d.setMinutes(x[6]);
+		return d.getTime();
+	}
+}
+*/
+
+	// Make vanadium validation not work for RecordAdmin searches
+	$('#ra-find').attr('onClick','Vanadium={}');
+
+	$('#pt-userpage').css('text-decoration','underline');
+
+	// Improve RA record name inputs
+	// - normal record-id is always hidden (css)
+	// - if a record-name row exists, then it should be visible and mandatory only if record-id also exists
+	if ($('#record-name')) {
+		if ($('#ra-record').val()) {
+			$('#record-name input').removeClass(':required');
+			$('#record-name').hide();
+		} else {
+			$('#record-name input').addClass(':required').val($('#ra-title').val());
+			var submit = $('form.recordadmin').attr('onSubmit');
+			if( submit ) submit = submit + ';'; else submit = '';
+			$('form.recordadmin').attr('onSubmit', submit + '$("#ra-title").val($("#record-name input").val() );');
+		}
+	}
+
+};
+
+addOnloadHook( odOnloadHook );
+
