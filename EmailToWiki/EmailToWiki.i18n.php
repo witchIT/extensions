509456e19e5ac8788fd5c9ce64bc4bba2619a97b
Aran Dunkley <aran@organicdesign.co.nz>
2011-11-25 17:09:59 +0000

    oops forgot i18n
    git-svn-id: svn+ssh://organicdesign.co.nz/extensions@787 2ad5491c-c162-49ae-9d3b-afa169216a3e

---
+++ EmailToWiki/EmailToWiki.i18n.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Internationalisation for EmailToWiki extension
+ *
+ * @author Aran Dunkley
+ * @file
+ * @ingroup Extensions
+ */
+
+$messages = array();
+
+/** English
+ * @author Dunkley
+ */
+$messages['en'] = array(
+	'emailtowiki_uploadcomment' => "File attachment for [[$1]] uploaded by EmailToWiki",
+	'emailtowiki_uploadtext' => "This file is an attachment in the [[$1]] email",
+	'emailtowiki_attachsection' => "File attachments",
+	'emailtowiki_articlecomment' => "Email imported by EmailToWiki",
+);
