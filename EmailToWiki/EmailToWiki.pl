8ddec3e4178da731549dd7580d54f8997caa0a33
Aran Dunkley <aran@organicdesign.co.nz>
2011-12-04 23:57:41 +0000

    use Message-ID not Delivered-To as double-line test
    git-svn-id: svn+ssh://organicdesign.co.nz/extensions@798 2ad5491c-c162-49ae-9d3b-afa169216a3e

--- EmailToWiki/EmailToWiki.pl
+++ EmailToWiki/EmailToWiki.pl
@@ -30,7 +30,7 @@ use Email::MIME;
 use HTTP::Request;
 use LWP::UserAgent;
 use strict;
-$::ver   =  '2.1.1 (2011-12-04)';
+$::ver   =  '2.1.2 (2011-12-04)';
 
 # Determine log file, tmp file and program directory
 $0 =~ /^(.+)\..+?$/;
@@ -109,11 +109,8 @@ exit( 0 );
 sub processEmail {
 	my $email = shift;
 
-	# Remove the CR's if running fatware
-	$email =~ s/\r//g;
-
 	# Test if lines are doubled up and fix if so
-	$email =~ s/\n\n/\n/g if $email =~ /Delivered-To: \S+\n\n/s;
+	$email =~ s/\n\n/\n/g if $email =~ /Message-ID: \S+\n\n/s;
 
 	# Extract the useful header portion of the message
 	my $id      = $1 if $email =~ /^message-id:\s*<(.+?)>\s*$/mi;
