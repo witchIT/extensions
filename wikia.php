9918aa736c6da3de5e1adac2f9cd51813fbb126d
Aran Dunkley <aran@organicdesign.co.nz>
2012-01-05 22:27:27 +0000

    $wgServer setting failing, have to include in LocalSettings for 1.18+
    git-svn-id: svn+ssh://organicdesign.co.nz/extensions@829 2ad5491c-c162-49ae-9d3b-afa169216a3e

--- wikia.php
+++ wikia.php
@@ -114,9 +114,6 @@ else {
 	$domain = $_SERVER['SERVER_NAME'];
 }
 
-$proto = ( array_key_exists( 'HTTPS', $_SERVER ) && $_SERVER['HTTPS'] == 'On' ) ? 'https' : 'http';
-$wgServer = "$proto://$domain";
-
 # Add google analytics code
 $wgExtensionFunctions[] = 'wfGoogleAnalytics';
 $wgGoogleTrackingCodes = array();
