e5db7e0af7e39f24337c9a1384c4f69a42fc62ab
Aran Dunkley <aran@organicdesign.co.nz>
2012-01-08 20:30:52 +0000

    getStructure started
    git-svn-id: svn+ssh://organicdesign.co.nz/extensions@831 2ad5491c-c162-49ae-9d3b-afa169216a3e

--- BookNavigation/BookNavigation.php
+++ BookNavigation/BookNavigation.php
@@ -13,6 +13,7 @@ define( 'BOOKNAVIGATION_VERSION', "0.0.1, 2012-01-07" );
 
 $wgBookNavigationStructureArticle = 'MediaWiki:BookStructure';
 $wgBookNavigationPrevNextMagic = 'PrevNext';
+$wgBookNavigationTreeMagic = 'BookTree';
 
 $dir = dirname( __FILE__ );
 //$wgExtensionMessagesFiles['BookNavigation'] = "$dir/BookNavigation.i18n.php";
@@ -33,19 +34,28 @@ $wgExtensionCredits['other'][] = array(
 class BookNavigation {
 
 	function __construct() {
-		global $wgHooks, $wgParser, $wgBookNavigationPrevNextMagic;
+		global $wgHooks, $wgParser, $wgBookNavigationPrevNextMagic, $wgBookNavigationBookTreeMagic;
 
-		// Create a parser-function to render login & account creations forms
+		// Create parser-functions
 		$wgParser->setFunctionHook( $wgBookNavigationPrevNextMagic, array( $this, 'expandPrevNext' ), SFH_NO_HASH );
+		$wgParser->setFunctionHook( $wgBookNavigationBookTreeMagic, array( $this, 'expandBookTree' ), SFH_NO_HASH );
 		
 		// TODO: add hook to put tree into sidebar
+		$this->getStructure();
 
 	}
 
 	/**
-	 * Expand the PrevNextMagic parser function
+	 * Expand the PrevNext parser function
 	 */
 	function expandPrevNext( &$parser ) {
+		return array( '< prev | next >', 'isHTML' => true, 'noparse' => true);
+	}
+
+	/**
+	 * Expand the BookTree parser function
+	 */
+	function expandBookTree( &$parser ) {
 		return array( '< prev | next >', 'isHTML' => true, 'noparse' => true);
 	}
 
@@ -57,6 +67,37 @@ class BookNavigation {
 		$tree = '';
 
 		return $tree;
+	}
+
+	/**
+	 * Render breadcrumbs for current title in structure
+	 */
+	function renderBreadcrumbs() {
+	}
+
+	/**
+	 * Get an array of the article structure from the structure article content
+	 */
+	function getStructure() {
+		global $wgBookNavigationStructureArticle;
+
+		// Get the structure-article content
+		$title = Title::newFromText( $wgBookNavigationStructureArticle );
+		$article = new Article( $title );
+		$content = $article->getContent();
+
+		// Extract all the chapters and their following structure data
+		$structure = array();
+		foreach( preg_split( "|^=|m", $content ) as $chapter ) {
+			if( preg_match( "|^=\s*(.+?)\s*==$(.*)|sm", $chapter, $m ) ) {
+				preg_match_all( "|^(\*+\s*.+?)\s*$|m", $m[2], $n );
+				$structure[$m[1]] = $n[1];
+			}
+		}
+
+print_r( $structure );
+		
+		return $structure;
 	}
 }
 
@@ -75,7 +116,8 @@ function wfSetupBookNavigation() {
  * Set up magic words for parser-functions
  */
 function wfBookNavigationLanguageGetMagic( &$langMagic, $langCode = 0 ) {
-	global $wgBookNavigationPrevNextMagic;
+	global $wgBookNavigationPrevNextMagic, $wgBookNavigationBookTreeMagic;
 	$langMagic[$wgBookNavigationPrevNextMagic] = array( $langCode, $wgBookNavigationPrevNextMagic );
+	$langMagic[$wgBookNavigationBookTreeMagic] = array( $langCode, $wgBookNavigationBookTreeMagic );
 	return true;
 }
