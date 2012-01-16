b1270234d8f70541b19fd78f10f1565ff6f82786
Aran Dunkley <aran@organicdesign.co.nz>
2012-01-16 14:43:22 +0000

    changed data format and added action for retrieving individual trails info
    git-svn-id: svn+ssh://organicdesign.co.nz/extensions@840 2ad5491c-c162-49ae-9d3b-afa169216a3e

--- TrailWikiMaps/TrailWikiMaps.php
+++ TrailWikiMaps/TrailWikiMaps.php
@@ -35,75 +35,146 @@ $wgExtensionCredits['parserhook'][] = array(
 
 class TrailWikiMaps {
 
+	var $opts = array(
+		'"type":"TERRAIN"',
+		'"zoom":8'
+	);
+
 	function __construct() {
-		global $wgOut, $wgHooks, $wgParser, $wgScriptPath, $wgJsMimeType, $wgTrailWikiMagic;
+		global $wgOut, $wgHooks, $wgParser, $wgResourceModules, $wgTrailWikiMagic;
 
 		$wgHooks['UnknownAction'][] = $this;
 
 		$wgParser->setFunctionHook( $wgTrailWikiMagic, array( $this,'expandAjaxMap' ) );
+		$wgParser->setFunctionHook( 'ajaxmapinternal', array( $this,'expandAjaxMapInternal' ) );
 
 		$wgResourceModules['ext.trailwikimaps'] = array(
 			'scripts' => array( 'trailwikimaps.js' ),
 			'styles' => array(),
-			'dependencies' => array( 'ext.maps.googlemaps3' ),
 			'localBasePath' => dirname( __FILE__ ),
 			'remoteExtPath' => basename( dirname( __FILE__ ) ),
 		);
-		$wgOut->addModules( 'ext.trailwikimaps' );
-
 	}
 
 	/**
 	 * Return the trail data in JSON format when the ajaxmap action is requested
 	 */
 	function onUnknownAction( $action, $article ) {
-		global $wgOut;
+		global $wgOut, $wgJsMimeType;
 
 		// Update the information for the specified ISBN (or oldest book in the wiki if none supplied)
 		// - if the book doesn't exist and the "create" query-string item is set, then create the book article
-		if( $action == 'ajaxmap' ) {
+		if( $action == 'traillocations' ) {
 			$wgOut->disable();
 			header( 'Content-Type: application/json' );
+			$comma = '';
 			print "{\n";
-			$dbr    = &wfGetDB(DB_SLAVE);
-			$tmpl   = $dbr->addQuotes( Title::newFromText( 'Infobox Trail' )->getDBkey() );
-			$table  = $dbr->tableName( 'templatelinks' );
-			$res    = $dbr->select( $table, 'tl_from', "tl_namespace = 10 AND tl_title = $tmpl LIMIT 5" );
-			$comma  = '';
-			while( $row = $dbr->fetchRow( $res ) ) {
-				$data = self::getTrailData( $row[0] );
-				print $comma . $data;
+			foreach( self::getTrailLocations() as $pos => $trails ) {
+				print "$comma\"$pos\":[\"" . implode( '","', $trails ) . "\"]\n";
 				$comma = ',';
 			}
-			$dbr->freeResult( $res );
 			print "}";
 		}
+
+		if( $action == 'trailinfo' ) {
+			$wgOut->disable();
+			global $wgTitle;
+			$data = self::getTrailInfo( $wgTitle );
+
+			// Render the info
+			$info = "<b>Difficulty: </b><i>Unknown</i><br />";
+			$info .= "<b>Distance: </b>" . $data['Distance'] . " Miles<br />";
+			$info .= "<b>Trail Type: </b><i>Unknown</i><br />";
+			$info .= "<b>Trail Uses: </b><i>Unknown</i><br />";
+
+			// Get a thumbnail image if the image field is set
+			$img = '';
+			if( !empty( $data['Image Name'] ) ) {
+				if( $img = wfLocalFile( $data['Image Name'] ) ) $img = $img->transform( array( 'width' => 140 ) )->toHtml();
+			}
+
+			// Return the data in a table
+			print "<table><tr><td>$info</td><th>$img</th></tr></table>";
+		}
+
 		return true;
 	}
 
 
 	/**
 	 * Expand #ajaxmap parser-functions
+	 * - pretends to render a map from Extension:Maps to load the necessary resources
+	 * - adds our internal parser-function to remove the dummy map and to define our map options in script
 	 */
 	public function expandAjaxMap() {
-		$options = func_get_args();
-		array_shift( $options );
-		return 'ajaxmap';
+		global $wgOut, $wgJsMimeType;
+		foreach( func_get_args() as $opt ) {
+			if( !is_object( $opt ) && preg_match( "/^(\w+?)\s*=\s*(.*)$/s", $opt, $m ) ) {
+				$v = is_numeric( $m[2] ) ? $m[2] : '"' . str_replace( '"', '', $m[2] ) . '"';
+				$this->opts[] = "\"$m[1]\":$v";
+			}
+		}
+		return array(
+			'<div id="ajaxmap">{{#display_map:0,0}}{{#ajaxmapinternal:}}</div>',
+			'found'   => true,
+			'nowiki'  => false,
+			'noparse' => false,
+			'noargs'  => false,
+			'isHTML'  => false
+		);
+	}
+
+	public function expandAjaxMapInternal() {
+		global $wgOut, $wgJsMimeType;
+		$wgOut->addModules( 'ext.trailwikimaps' );
+		$script = "window.ajaxmap_opt = {" . implode( ',', $this->opts ) . "};";
+		$script .= "document.getElementById('ajaxmap').innerHTML = '';";
+		return array(
+			"<script type=\"$wgJsMimeType\">$script</script>",
+			'found'   => true,
+			'nowiki'  => false,
+			'noparse' => true,
+			'noargs'  => false,
+			'isHTML'  => true
+		);
 	}
 
 	/**
-	 * Return array of args from the trail infobox
+	 * Return array of args from the trail infobox for passed trail
 	 */
-	static function getTrailData( $id ) {
-		$title = Title::newFromId( $id );
-		$trail = $title->getText();
+	static function getTrailInfo( $title ) {
 		$article = new Article( $title );
 		preg_match( "|\{\{Infobox Trail(.+?)^\}\}|sm", $article->fetchContent(), $m );
 		$template = preg_replace( "/(?<=\S)( +\| )/s", "\n$1", $m[1] ); // fix malformed template syntax
 		preg_match_all( "|^\s*\|\s*(.+?)\s*= *(.*?) *(?=^\s*[\|\}])|sm", $template, $m );
 		$data = array();
-		foreach( $m[1] as $i => $k ) $data[] = '"' . trim( $k ) . '":"' . trim( $m[2][$i] ) . '"';
-		return "\"$trail\":\n{\n" . implode( ',', $data ) . "\n}\n";
+		foreach( $m[1] as $i => $k ) $data[trim( $k )] = trim( $m[2][$i] );
+		return $data;
+	}
+
+	/**
+	 * Build a list of trails at each location
+	 */
+	static function getTrailLocations() {
+		$dbr   = &wfGetDB( DB_SLAVE );
+		$tmpl  = $dbr->addQuotes( Title::newFromText( 'Infobox Trail' )->getDBkey() );
+		$table = $dbr->tableName( 'templatelinks' );
+		$res   = $dbr->select( $table, 'tl_from', "tl_namespace = 10 AND tl_title = $tmpl" );
+		$list  = array();
+		while( $row = $dbr->fetchRow( $res ) ) {
+			$title = Title::newFromId( $row[0] );
+			$trail = $title->getText();
+			$data = self::getTrailInfo( $title );
+			if( array_key_exists( 'Latitude', $data ) && array_key_exists( 'Longitude', $data ) ) {
+				if( is_numeric( $data['Latitude'] ) && is_numeric( $data['Longitude'] ) ) {
+					$pos = $data['Latitude'] . ',' . $data['Longitude'];
+					if( !array_key_exists( $pos, $list ) ) $list[$pos] = array( $trail );
+					else $list[$pos][] = $trail;
+				}
+			}
+		}
+		$dbr->freeResult( $res );
+		return $list;
 	}
 
 }
@@ -116,5 +187,6 @@ function wfSetupTrailWikiMaps() {
 function wfTrailWikiMapsLanguageGetMagic( &$magicWords, $langCode = 0 ) {
 	global $wgTrailWikiMagic;
 	$magicWords[$wgTrailWikiMagic] = array( $langCode, $wgTrailWikiMagic );
+	$magicWords['ajaxmapinternal'] = array( $langCode, 'ajaxmapinternal' );
 	return true;
 }
--- TrailWikiMaps/trailwikimaps.js
+++ TrailWikiMaps/trailwikimaps.js
@@ -1,1 +1,196 @@
-alert( 'trailwikimaps.js loaded' );
+/**
+ * JavaScript for TrailWikiMaps MediaWiki extension
+ * - creates a map with markers positions retrieved via AJAX and custom info overlays with content retrieved via AJAX
+ */
+
+function InfoBox( marker ) {
+	google.maps.OverlayView.call( this );
+	this.latlng_ = marker.position;
+	this.map_ = marker.map;
+	this.titles_ = marker.titles;
+	var me = this;
+	this.boundsChangedListener_ = google.maps.event.addListener( this.map_, "bounds_changed", function() {
+		return me.panMap.apply( me );
+	});
+	this.setMap( this.map_ );
+}
+
+InfoBox.prototype = new google.maps.OverlayView();
+
+InfoBox.prototype.remove = function() {
+	if( this.div_ ) {
+		this.div_.parentNode.removeChild( this.div_ );
+		this.div_ = null;
+	}
+};
+
+InfoBox.prototype.draw = function() {
+	this.createElement();
+	if( !this.div_ ) return;
+	var pixPosition = this.getProjection().fromLatLngToDivPixel( this.latlng_ );
+	if( !pixPosition ) return;
+	this.width_ = $( this.div_ ).width();
+	this.height_ = $( this.div_ ).height();
+	this.offsetVertical_ = -this.height_;
+	this.offsetHorizontal_ = 0;
+	this.div_.style.left = ( pixPosition.x + this.offsetHorizontal_ ) + "px";
+	this.div_.style.top = ( pixPosition.y + this.offsetVertical_ ) + "px";
+	this.div_.style.display = 'block';
+};
+
+InfoBox.prototype.createElement = function() {
+	var panes = this.getPanes();
+	var div = this.div_;
+	if( !div ) {
+		div = this.div_ = document.createElement( 'div' );
+		div.className = 'ajaxmap-info';
+		var contentDiv = document.createElement( 'div' );
+		contentDiv.className = 'ajaxmap-info-content';
+		this.loadContent( this.titles_, contentDiv );
+		var topDiv = document.createElement( 'div' );
+		topDiv.className = 'ajaxmap-info-top';
+		titlep = document.createElement( 'div' );
+		title = document.createElement( 'a' );
+		title.innerHTML = this.titles_[0];
+		title.href = mw.util.wikiScript() + '?' + $.param({ title: this.titles_[0] });
+		var closeImg = document.createElement( 'img' );
+		closeImg.src = "/files/b/bd/Close.png";
+		topDiv.appendChild( closeImg );
+		topDiv.appendChild( titlep );
+		titlep.appendChild( title );
+
+		function removeInfoBox( ib ) {
+			return function() {
+				ib.setMap( null );
+			};
+		}
+
+		google.maps.event.addDomListener( closeImg, 'click', removeInfoBox( this ) );
+
+		div.appendChild( topDiv );
+		div.appendChild( contentDiv );
+		div.style.display = 'none';
+		panes.floatPane.appendChild( div );
+		this.panMap();
+	} else if( div.parentNode != panes.floatPane ) {
+		div.parentNode.removeChild( div );
+		panes.floatPane.appendChild( div );
+	}
+}
+
+InfoBox.prototype.panMap = function() {
+	var map = this.map_;
+	var bounds = map.getBounds();
+	if( !bounds ) return;
+
+	// The position of the infowindow
+	var position = this.latlng_;
+	var iwWidth = this.width_;
+	var iwHeight = this.height_;
+	var iwOffsetX = this.offsetHorizontal_;
+	var iwOffsetY = this.offsetVertical_;
+
+	// Padding on the infowindow
+	var padX = 0; //40;
+	var padY = 0; //40;
+
+	// The degrees per pixel
+	var mapDiv = map.getDiv();
+	var mapWidth = mapDiv.offsetWidth;
+	var mapHeight = mapDiv.offsetHeight;
+	var boundsSpan = bounds.toSpan();
+	var longSpan = boundsSpan.lng();
+	var latSpan = boundsSpan.lat();
+	var degPixelX = longSpan / mapWidth;
+	var degPixelY = latSpan / mapHeight;
+
+	// The bounds of the map
+	var mapWestLng = bounds.getSouthWest().lng();
+	var mapEastLng = bounds.getNorthEast().lng();
+	var mapNorthLat = bounds.getNorthEast().lat();
+	var mapSouthLat = bounds.getSouthWest().lat();
+
+	// The bounds of the infowindow
+	var iwWestLng = position.lng() + ( iwOffsetX - padX ) * degPixelX;
+	var iwEastLng = position.lng() + ( iwOffsetX + iwWidth + padX ) * degPixelX;
+	var iwNorthLat = position.lat() - ( iwOffsetY - padY ) * degPixelY;
+	var iwSouthLat = position.lat() - ( iwOffsetY + iwHeight + padY ) * degPixelY;
+
+	// calculate center shift
+	var shiftLng =
+	  ( iwWestLng < mapWestLng ? mapWestLng - iwWestLng : 0 ) +
+	  ( iwEastLng > mapEastLng ? mapEastLng - iwEastLng : 0 );
+	var shiftLat =
+	  ( iwNorthLat > mapNorthLat ? mapNorthLat - iwNorthLat : 0 ) +
+	  ( iwSouthLat < mapSouthLat ? mapSouthLat - iwSouthLat : 0 );
+
+	// The center of the map
+	var center = map.getCenter();
+
+	// The new map center
+	var centerX = center.lng() - shiftLng;
+	var centerY = center.lat() - shiftLat;
+
+	// center the map to the new shifted center
+	map.setCenter( new google.maps.LatLng( centerY, centerX ) );
+
+	// Remove the listener after panning is complete.
+	google.maps.event.removeListener( this.boundsChangedListener_ );
+	this.boundsChangedListener_ = null;
+};
+
+// Load content for passed titles and add to target element
+InfoBox.prototype.loadContent = function( titles, target ) {
+	for( i in titles ) {
+		$.ajax({
+			type: "GET",
+			url: mw.util.wikiScript(),
+			data: { title: titles[i], action: 'trailinfo' },
+			dataType: 'html',
+			success: function( html ) {
+				if( target.innerHTML ) {
+					hr = document.createElement( 'hr' );
+					target.appendChild( hr );
+					h = document.createElement( 'a' );
+					h.innerHTML = titles[i];
+					h.href = mw.util.wikiScript() + '?' + $.param({ title: titles[i] });
+					target.appendChild( h );
+				}
+				target.innerHTML = target.innerHTML + html;
+			}
+		});
+	}
+};
+
+$( function() {
+
+	// Format some of the options
+    window.ajaxmap_opt.center = new google.maps.LatLng( window.ajaxmap_opt.lat, window.ajaxmap_opt.lon );
+    window.ajaxmap_opt.mapTypeId = google.maps.MapTypeId[window.ajaxmap_opt.type.toUpperCase()];
+    //window.ajaxmap_opt.sensor = 'true';
+
+	// Create the map and set canvas size
+	var canvas = document.getElementById('ajaxmap');	
+    var map = new google.maps.Map( canvas, window.ajaxmap_opt );
+	canvas.style.width = window.ajaxmap_opt['width'] + 'px';
+	canvas.style.height = window.ajaxmap_opt['height'] + 'px';
+
+	// Retrieve location info and create markers
+	$.ajax({
+		type: "GET",
+		url: mw.util.wikiScript(),
+		data: { action: 'traillocations' },
+		dataType: 'json',
+		success: function( data ) {
+			for( i in data ) {
+				var pos = i.split( ',' );
+				var marker = new google.maps.Marker({
+					position: new google.maps.LatLng( pos[0], pos[1] ),
+					map: map,
+					titles: data[i]
+				});
+				google.maps.event.addListener( marker, 'click', function() { new InfoBox( this ); });
+			}
+		}
+	});
+});
