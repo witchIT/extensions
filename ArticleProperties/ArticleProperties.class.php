53a905a4ce546f384a9e4a3be74aaf9cbccd8095
Aran Dunkley <aran@organicdesign.co.nz>
2012-05-24 17:29:37 +0000

    make ArticleProperties initialise the object of the classname
    git-svn-id: svn+ssh://organicdesign.co.nz/extensions@1058 2ad5491c-c162-49ae-9d3b-afa169216a3e

--- ArticleProperties/ArticleProperties.class.php
+++ ArticleProperties/ArticleProperties.class.php
@@ -7,7 +7,7 @@ class ArticleProperties extends Article {
 	public static $prefix = '';
 
 	// If set this includes member properties that should be available from the JavaScript side
-	public var $js = false;
+	public $js = false;
 
 	// Some methods can benefit from caching their results
 	private static  $cache = array();
