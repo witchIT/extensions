a79fb3c4e527818e5db06dd075a6a3fb49a6c75c
Aran Dunkley <aran@organicdesign.co.nz>
2012-06-27 02:15:14 +0000

    pretty much done now, few minor bugs
    git-svn-id: svn+ssh://organicdesign.co.nz/extensions@1145 2ad5491c-c162-49ae-9d3b-afa169216a3e

--- AjaxComments/AjaxComments.php
+++ AjaxComments/AjaxComments.php
@@ -11,6 +11,11 @@
 if( !defined( 'MEDIAWIKI' ) ) die( 'Not an entry point.' );
 
 define( 'AJAXCOMMENTS_VERSION','0.0.1, 2012-06-26' );
+define( 'AJAXCOMMENTS_USER', 1 );
+define( 'AJAXCOMMENTS_DATE', 2 );
+define( 'AJAXCOMMENTS_TEXT', 3 );
+define( 'AJAXCOMMENTS_PARENT', 4 );
+define( 'AJAXCOMMENTS_REPLIES', 5 );
 
 $wgExtensionFunctions[] = 'wfSetupAjaxComments';
 $wgExtensionCredits['other'][] = array(
@@ -29,14 +34,17 @@ class AjaxComments {
 
 	var $comments = array();
 	var $changed = false;
+	var $talk = false;
 
 	function __construct() {
+		global $wgHooks, $wgOut, $wgResourceModules;
+
+		$wgHooks['UnknownAction'][] = $this;
 
 		// Set up JavaScript and CSS resources
 		$wgResourceModules['ext.ajaxcomments'] = array(
 			'scripts'       => array( 'ajaxcomments.js' ),
 			'styles'        => array( 'ajaxcomments.css' ),
-			'dependencies'  => array( 'mediawiki.util' ),
 			'localBasePath' => dirname( __FILE__ ),
 			'remoteExtPath' => basename( dirname( __FILE__ ) ),
 		);
@@ -48,22 +56,24 @@ class AjaxComments {
 	 * - we're bypassing the Ajax handler because we need the title and parser to be established
 	 */
 	function onUnknownAction( $action, $article ) {
-		global $wgOut;
-
-			// Returns the list of trails in each location in JSON format
-			if( $action == 'ajaxcomments' ) {
-				$wgOut->disable();
-				$id = $wgRequest->getText( 'id', false );
-				$text = $wgRequest->getText( 'text', false );
+		if( $action == 'ajaxcomments' ) {
+			global $wgOut, $wgRequest;
+			$wgOut->disable();
+			$id = $wgRequest->getText( 'id', false );
+			$text = $wgRequest->getText( 'text', false );
+			$talk = $article->getTitle()->getTalkPage();
+			if( is_object( $talk ) ) {
+				$this->talk = $talk;
+				$article = new Article( $talk );
 
 				// Get the talk page content
-				if( $talk = $article->getTitle()->getTalkPage() && $talk->exists() ) {
-					$article = new Article( $talk );
-					$content = $talk->fetchContent();
-					$this->comments = self::textToData( $content );
-				}
+				if( $talk->exists() ) $this->comments = self::textToData( $article->fetchContent() );
+//print '<pre>';
+//print_r($this->comments);
+//print '</pre>';
 
-				switch( $command = $wgRequest->getText( 'command' ) ) {
+				// Perform the command on the talk content
+				switch( $command = $wgRequest->getText( 'cmd' ) ) {
 
 					case 'add':
 						print $this->add( $text );
@@ -77,31 +87,91 @@ class AjaxComments {
 						print $this->edit( $id, $text );
 					break;
 
-					case 'delete':
+					case 'del':
 						print $this->delete( $id );
+						print count( $this->comments ) > 0 ? '' : "<i id=\"ajaxcomments-none\">" . wfMsg( 'ajaxcomments-none' ) . "</i>";
 					break;
 
-					default: print $this->renderComments();
-			}
+					default:
+						print "<h2>" . wfMsg( 'ajaxcomments-heading' ) . "</h2>\n";
+						print $this->renderComments();
+				}
 
-			// If any comment data has been changed write it back to the talk article
-			if( $this->changed ) {
-				$article->doEdit( $self::dataToText( $this->comments ), wfMsg( "ajaxcomments-$command-summary" ), EDIT_UPDATE );
+				// If any comment data has been changed write it back to the talk article
+				if( $this->changed ) {
+					$flag = $talk->exists() ? EDIT_UPDATE : EDIT_NEW;
+					$article->doEdit( self::dataToText( $this->comments ), wfMsg( "ajaxcomments-$command-summary" ), $flag );
+				}
 			}
+		}
 
 		return true;
 	}
 
+	/**
+	 * Add a new comment to the data structure
+	 */
 	function add( $text ) {
+		global $wgUser;
+		$id = uniqid();
+		$this->comments[$id] = array(
+			AJAXCOMMENTS_PARENT => false,
+			AJAXCOMMENTS_USER => $wgUser->getName(),
+			AJAXCOMMENTS_DATE => time(),
+			AJAXCOMMENTS_TEXT => $text,
+			AJAXCOMMENTS_REPLIES => array()
+		);
+		$this->changed = true;
+		return $this->renderComment( $id );
 	}
 
-	function reply( $id, $text ) {
-	}
-
+	/**
+	 * Edit an existing comment in the data structure
+	 */
 	function edit( $id, $text ) {
+		$this->comments[$id][AJAXCOMMENTS_TEXT] = $text;
+		$this->changed = true;
+		return $this->renderComment( $id );
 	}
 
+	/**
+	 * Add a new comment as a reply to an existing comment in the data structure
+	 */
+	function reply( $parent, $text ) {
+		global $wgUser;
+		$id = uniqid();
+		$this->comments[$parent][AJAXCOMMENTS_REPLIES][] = $id;
+		$this->comments[$id] = array(
+			AJAXCOMMENTS_PARENT => $parent,
+			AJAXCOMMENTS_USER => $wgUser->getName(),
+			AJAXCOMMENTS_DATE => time(),
+			AJAXCOMMENTS_TEXT => $text,
+			AJAXCOMMENTS_REPLIES => array()
+		);
+		$this->changed = true;
+		return $this->renderComment( $id );
+	}
+
+	/**
+	 * Delete a comment amd all its replies from the data structure
+	 */
 	function delete( $id ) {
+		if( array_key_exists( $id, $this->comments ) ) {
+
+			// Call delete for all the replies of this comment
+			foreach( $this->comments[$id][AJAXCOMMENTS_REPLIES] as $child ) $this->delete( $child );
+
+			// Remove this item from the parents replies list (unless root level)
+			if( $parent = $this->comments[$id][AJAXCOMMENTS_PARENT] ) {
+				$i = array_search( $id, $this->comments[$parent][AJAXCOMMENTS_REPLIES] );
+				if( $i !== false ) unset( $this->comments[$parent][AJAXCOMMENTS_REPLIES][$i] );
+			}
+
+			// Remove this comment from the data
+			unset( $this->comments[$id] );
+
+			$this->changed = true;
+		}
 	}
 
 	/**
@@ -109,9 +179,13 @@ class AjaxComments {
 	 */
 	function renderComments() {
 		$html = '';
-		foreach( $this->comments as $comment ) {
-			$html .= $this->renderComment( $comment );
+		foreach( $this->comments as $id => $comment ) {
+			if( $comment[AJAXCOMMENTS_PARENT] === false ) $html .= $this->renderComment( $id );
 		}
+		if( $html == '' ) $html = "<i id=\"ajaxcomments-none\">" . wfMsg( 'ajaxcomments-none' ) . "</i>";
+		$html .= "<ul class=\"ajaxcomment-links\">" .
+			"<li id=\"ajaxcomment-add\"><a href=\"javascript:ajaxcomment_add()\">" . wfMsg( 'ajaxcomments-add' ) . "</a></li>\n" .
+			"</ul>\n";
 		return $html;
 	}
 
@@ -120,30 +194,37 @@ class AjaxComments {
 	 * - this is recursive - it will render any replies which could in turn contain replies etc
 	 * - renders edit/delete link if ysop, or noreplies and current user is owner
 	 */
-	function renderComment( $comment ) {
-		if( array_key_exists( 'replies', $comment ) ) $replies = $this->renderComment( $comment['replies'] );
-		return "<div class=\"ajaxcomment\" id=\"" . $comment['id'] . "\">\n" .
-			"<div class=\"ajaxcomment-sig\">" . wfMsg( 'ajaxcomments-sig', $comment['user'], wfTimestamp( TS_MW, $comment['time'] ) ) . "</div>\n" .
-			"<div class=\"ajaxcomment-text\">" . $wgParser->parse( $comment['text'], $talk, new ParserOptions(), true, true )->getText() . "</div>\n" .
+	function renderComment( $id ) {
+		global $wgParser;
+		$c = $this->comments[$id];
+		$r = '';
+		foreach( $c[AJAXCOMMENTS_REPLIES] as $child ) $r .= $this->renderComment( $child );
+		return "<div class=\"ajaxcomment\" id=\"ajaxcomments-$id\">\n" .
+			"<div class=\"ajaxcomment-sig\">" .
+				wfMsg( 'ajaxcomments-sig', $c[AJAXCOMMENTS_USER], wfTimestamp( TS_DB, $c[AJAXCOMMENTS_DATE] ) ) .
+			"</div>\n" .
+			"<div class=\"ajaxcomment-text\">" .
+				$wgParser->parse( $c[AJAXCOMMENTS_TEXT], $this->talk, new ParserOptions(), true, true )->getText() .
+			"</div>\n" .
 			"<ul class=\"ajaxcomment-links\">" .
-				"<li id=\"ajaxcomment-reply\"><a href=\"javascript:ajaxcomment_reply(this)\">" .wfMsg( 'ajaxcomments-reply' ) . "</a></li>\n" .
-				"<li id=\"ajaxcomment-edit\"><a href=\"javascript:ajaxcomment_edit(this)\">" .wfMsg( 'ajaxcomments-edit' ) . "</a></li>\n" .
-				"<li id=\"ajaxcomment-del\"><a href=\"javascript:ajaxcomment_del(this)\">" .wfMsg( 'ajaxcomments-del' ) . "</a></li>\n" .
-			"</ul>$replies</div>\n";
+				"<li id=\"ajaxcomment-reply\"><a href=\"javascript:ajaxcomment_reply('$id')\">" . wfMsg( 'ajaxcomments-reply' ) . "</a></li>\n" .
+				"<li id=\"ajaxcomment-edit\"><a href=\"javascript:ajaxcomment_edit('$id')\">" . wfMsg( 'ajaxcomments-edit' ) . "</a></li>\n" .
+				"<li id=\"ajaxcomment-del\"><a href=\"javascript:ajaxcomment_del('$id')\">" . wfMsg( 'ajaxcomments-del' ) . "</a></li>\n" .
+			"</ul>$r</div>\n";
 	}
 
 	/**
 	 * Return the passed talk text as a data structure of comments
 	 */
 	static function textToData( $text ) {
-		return unserialize( $text );
+		return $text ? unserialize( $text ) : array();
 	}
 
 	/**
 	 * Return the passed data structure of comments as text for a talk page
 	 */
-	static function textToData( $data ) {
-		return serialize( $data );
+	static function dataToText( $data ) {
+		return count( $data ) ? serialize( $data ) : '';
 	}
 
 }
--- AjaxComments/ajaxcomments.js
+++ AjaxComments/ajaxcomments.js
@@ -1,15 +1,147 @@
 $(document).ready( function() {
 
-	// If there's a discussion area on this page, populate it with the comments from the server
-	if( $('#ajaxcomments').length > 0 ) {
-		$('#ajaxcomments').html('<div class=\"ajaxcomments-loader\"></div>');
+	//$('#ca-talk').hide();
+
+	// If there's a discussion tab, normal view action and not on talk page, render the discussion below the article
+	if( $('#ca-talk').length > 0 && mw.config.get('wgAction') == 'view' && !mw.config.get('wgNamespaceNumber')&1 ) {
+		$('.printfooter').after('<div id="ajaxcomments"><div class="ajaxcomments-loader"></div></div>');
 		$.ajax({
 			type: 'GET',
 			url: mw.util.wikiScript(),
 			data: { action: 'ajaxcomments', title: mw.config.get('wgTitle') },
 			dataType: 'html',
-			success: function(html) { $('#ajaxcomments').html(html); }
+			success: function(html) {
+				$('#ajaxcomments').html(html);
+			}
 		});
 	}
+
+});
+
+/**
+ * An add link has been clicked
+ */
+window.ajaxcomment_add = function() {
+	ajaxcomment_textinput($('#ajaxcomment-add').parent(), 'add');
+	$('#ajaxcomments-none').remove();
+};
+
+/**
+ * An edit link has been clicked
+ */
+window.ajaxcomment_edit = function(id) {
+	var e = $('#ajaxcomments-' + id + ' .ajaxcomment-text').first();
+	ajaxcomment_textinput(e, 'edit');
+	$('textarea', e.parent()).first().val(e.html());
+	e.hide();
+};
+
+/**
+ * A reply link has been clicked
+ */
+window.ajaxcomment_reply = function(id) {
+	ajaxcomment_textinput($('#ajaxcomments-' + id + ' .ajaxcomment-links').first(), 'reply');
+};
+
+/**
+ * An delete link has been clicked
+ */
+window.ajaxcomment_del = function(id) {
+	var target = $('#ajaxcomments-' + id);
+	if(confirm('Are you sure you want to remove this comment?')) {
+		target.html('<div class="ajaxcomments-loader"></div>');
+		$.ajax({
+			type: 'GET',
+			url: mw.util.wikiScript(),
+			data: {
+				action: 'ajaxcomments',
+				title: mw.config.get('wgTitle'),
+				cmd: 'del',
+				id: id,
+			},
+			context: target,
+			dataType: 'html',
+			success: function(html) {
+				this.replaceWith(html);
+			}
+		});
+	}
+};
+
+/**
+ * Open a comment input box at the passed element location
+ */
+window.ajaxcomment_textinput = function(e, cmd) {
+	ajaxcomment_cancel();
+	var html = '<div id="ajaxcomment-input"><textarea></textarea><br />';
+	html += '<input type="button" onclick="ajaxcomment_submit(this,\'' + cmd + '\')" value="Post" />';
+	html += '<input type="button" onclick="ajaxcomment_cancel()" value="Cancel" />';
+	html += '</div>';
+	e.after(html);
+};
+
+/**
+ * Remove any current comment input box
+ */
+window.ajaxcomment_cancel = function() {
+	$('#ajaxcomment-input').remove();
+	$('.ajaxcomment-text').show();
+};
+
+/**
+ * Submit a comment command to the server
+ * - e is the button element that was clicked
+ * - cmd will be add, reply or edit
+ */
+window.ajaxcomment_submit = function(e, cmd) {
+	e = $(e);
+	var target;
+	var id = 0;
+	var text = '';
+
+	// If it's an add, create the target at the end
+	if( cmd == 'add' ) {
+		$('#ajaxcomment-add').parent().before('<div id="ajaxcomments-new"></div>');
+		target = $('#ajaxcomments-new');
+		text = $('#ajaxcomment-input textarea').val();
+	}
+
+	// If it's a reply, create the target within the current comment
+	if( cmd == 'reply' ) {
+		e.parent().before('<div id="ajaxcomments-new"></div>');
+		target = $('#ajaxcomments-new');
+		text = $('#ajaxcomment-input textarea').val();
+		id = target.parent().attr('id').substr(13);
+	}
+
+	// If it's an edit, create the target as the current comment
+	if( cmd == 'edit' ) {
+		text = $('#ajaxcomment-input textarea').val();
+		target = e.parent().parent();
+		target.html('<div id="ajaxcomments-new"></div>');
+		id = target.attr('id').substr(13);
+	}
+
+	// Put a loader into the target
+	target.html('<div class="ajaxcomments-loader"></div>');
+
+	// Send the command and replace the loader with the new post
+	$.ajax({
+		type: 'GET',
+		url: mw.util.wikiScript(),
+		data: {
+			action: 'ajaxcomments',
+			title: mw.config.get('wgTitle'),
+			cmd: cmd,
+			id: id,
+			text: text
+		},
+		context: target,
+		dataType: 'html',
+		success: function(html) {
+			this.replaceWith(html);
+			ajaxcomment_cancel();
+		}
+	});
 
 };
