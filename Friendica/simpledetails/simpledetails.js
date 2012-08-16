0ccc3aaf9f9df77f9e252acc072c9e64e1fe6385
Aran Dunkley <aran@organicdesign.co.nz>
2012-08-16 21:48:27 +0000

    remove most of the textareas
    git-svn-id: svn+ssh://organicdesign.co.nz/extensions@1207 2ad5491c-c162-49ae-9d3b-afa169216a3e

--- Friendica/simpledetails/simpledetails.js
+++ Friendica/simpledetails/simpledetails.js
@@ -7,7 +7,7 @@ $(document).ready( function() {
 	for( i = 0; i < elems.length; i++ ) {
 		var e = $(elems[i]);
 		var id = e.attr('id');
-		if( id == 'profile-edit-marital-wrapper' || id = 'dislikes-jot-end' || id = 'music-jot-wrapper' || 'profile-edit-submit-end' ) j++;
+		if( id == 'profile-edit-marital-wrapper' || id == 'dislikes-jot-end' || id == 'music-jot-wrapper' || id == 'profile-edit-submit-end' ) j++;
 		if( j == 1 || j == 3 ) e.hide();
 	}
 });
