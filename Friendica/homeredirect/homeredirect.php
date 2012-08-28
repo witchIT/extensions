77f1d72c33fc2c2d1a8b8b1400e919964b649355
Aran Dunkley <aran@organicdesign.co.nz>
2012-08-28 20:18:03 +0000

    new basic extension for replacing naked domain content for anon users
    git-svn-id: svn+ssh://organicdesign.co.nz/extensions@1240 2ad5491c-c162-49ae-9d3b-afa169216a3e

---
+++ Friendica/homeredirect/homeredirect.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Name: Home Redirect
+ * Description: Redirects homepage (naked domain) if user is not logged in
+ * Version: 1.0
+ * Author: Aran Dunkley <http://www.organicdesign.co.nz/nad>
+ *
+ */
+function homeredirect_install() {
+	register_hook('page_header', 'addon/homeredirect/homeredirect.php', 'homeredirect');
+}
+
+function homeredirect_uninstall() {
+	unregister_hook('page_header', 'addon/homeredirect/homeredirect.php', 'homeredirect');
+}
+function homeredirect(&$b) {
+	global $a;
+	if( $_SERVER['REQUEST_URI'] == '/' && !is_array( $a->user ) ) {
+		readfile( $_SERVER['DOCUMENT_ROOT']."/anonymous-home.html" );
+		exit;
+	}
+}
+
