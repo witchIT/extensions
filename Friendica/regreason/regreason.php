d95c76d740ca9fd4acdb17515804809077db1876
Aran Dunkley <aran@organicdesign.co.nz>
2012-09-08 13:45:38 +0000

    structure
    git-svn-id: svn+ssh://organicdesign.co.nz/extensions@1249 2ad5491c-c162-49ae-9d3b-afa169216a3e

--- Friendica/regreason/regreason.php
+++ Friendica/regreason/regreason.php
@@ -7,12 +7,31 @@
  *
  */
 function regreason_install() {
-	register_hook('app_menu', 'addon/regreason/regreason.php', 'regreason');
+	register_hook('register_form', 'addon/regreason/regreason.php', 'regreason_form');
+	register_hook('register_account', 'addon/regreason/regreason.php', 'regreason_register');
 }
 
-function simpledetails_uninstall() {
-	unregister_hook('app_menu', 'addon/regreason/regreason.php', 'regreason');
+function regreason_uninstall() {
+	unregister_hook('register_form', 'addon/regreason/regreason.php', 'regreason_form');
+	unregister_hook('register_account', 'addon/regreason/regreason.php', 'regreason_register');
 }
 
-function regreason( $menu ) {
+/**
+ * Add the new textbox to the form
+ */
+function register_form( $a, $form ) {
+}
+
+/**
+ * Send an email to admins with the textbox content
+ */
+function regreason_register( $a, $uid ) {
+
+	// Send email to admin
+	$res = mail($a->config['admin_email'], sprintf(t('Registration request at %s'), $a->config['sitename']),
+		$email_tpl,
+			'From: ' . t('Administrator') . '@' . $_SERVER['SERVER_NAME'] . "\n"
+			. 'Content-type: text/plain; charset=UTF-8' . "\n"
+			. 'Content-transfer-encoding: 8bit' );
+
 }
