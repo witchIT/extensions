c0b5f40bcf6f0fcb69743872b14650c46872f56e
Aran Dunkley <aran@organicdesign.co.nz>
2012-09-10 13:03:46 +0000

    ts 0 different than ts false
    git-svn-id: svn+ssh://organicdesign.co.nz/extensions@1251 2ad5491c-c162-49ae-9d3b-afa169216a3e

--- AjaxComments/AjaxComments.php
+++ AjaxComments/AjaxComments.php
@@ -114,7 +114,7 @@ class AjaxComments {
 			if( is_object( $talk ) ) {
 				$id = $wgRequest->getText( 'id', false );
 				$text = $wgRequest->getText( 'text', false );
-				$ts = $wgRequest->getText( 'ts', 0 );
+				$ts = $wgRequest->getText( 'ts', false );
 				$command = $wgRequest->getText( 'cmd' );
 				$this->talk = $talk;
 				$article = new Article( $talk );
@@ -129,7 +129,7 @@ class AjaxComments {
 				} else $latest = 0;
 
 				// If a timestamp is provided in the request, bail if nothings happened to the talk content since that time
-				if( $ts && ( $ts == $latest || $latest == 0 ) ) return '';
+				if( $ts !== false && ( $ts == $latest || $latest == 0 ) ) return '';
 
 				// Perform the command on the talk content
 				switch( $command ) {
