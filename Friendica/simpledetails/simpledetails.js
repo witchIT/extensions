4dde40d122c7e508707e223eaaaeb7d6ead69849
Aran Dunkley <aran@organicdesign.co.nz>
2012-09-14 12:19:53 +0000

    don't hide about box
    git-svn-id: svn+ssh://organicdesign.co.nz/extensions@1253 2ad5491c-c162-49ae-9d3b-afa169216a3e

--- Friendica/simpledetails/simpledetails.js
+++ Friendica/simpledetails/simpledetails.js
@@ -8,7 +8,7 @@ $(document).ready( function() {
 		var e = $(elems[i]);
 		var id = e.attr('id');
 		if( id == 'profile-edit-marital-wrapper' || id == 'dislikes-jot-end' || id == 'music-jot-wrapper' || id == 'profile-edit-submit-end' ) j++;
-		if( j == 1 || j == 3 ) e.hide();
+		if( ( j == 1 || j == 3 ) && e.attr('id') != 'about-jot-wrapper' ) e.hide();
 	}
 	gender = '<option selected="selected" value=""></option>';
 	gender += '<option value="Male">Male</option>';
