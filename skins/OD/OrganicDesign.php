27becc17bb5763990bc67cddbb8e833d7da76965
Aran Dunkley <aran@organicdesign.co.nz>
2012-09-26 14:34:35 +0000

    start upgrading skin to 1.19
    git-svn-id: svn+ssh://organicdesign.co.nz/extensions@1320 2ad5491c-c162-49ae-9d3b-afa169216a3e

--- skins/OD/OrganicDesign.php
+++ skins/OD/OrganicDesign.php
@@ -53,62 +53,10 @@ class OrganicDesignTemplate extends QuickTemplate {
 	 * @access private
 	 */
 	function execute() {
-		global $wgRequest, $wgVersion;
-		$this->skin = $skin = $this->data['skin'];
-		$action = $wgRequest->getText( 'action' );
-		$css = $this->data['csslinks'];
-		if ( version_compare( substr( $wgVersion, 0, 4 ), '1.16' ) >= 0 ) $css = str_replace( 'main.css', 'main116.css', $css );
-
 		// Suppress warnings to prevent notices about missing indexes in $this->data
 		wfSuppressWarnings();
-?><!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="<?php $this->text('xhtmldefaultnamespace') ?>" <?php
-	foreach($this->data['xhtmlnamespaces'] as $tag => $ns) {
-		?>xmlns:<?php echo "{$tag}=\"{$ns}\" ";
-	} ?>xml:lang="<?php $this->text('lang') ?>" lang="<?php $this->text('lang') ?>" dir="<?php $this->text('dir') ?>">
-	<head>
-		<meta http-equiv="Content-Type" content="<?php $this->text('mimetype') ?>; charset=<?php $this->text('charset') ?>" />
-		<meta name="ROBOTS" content="NOINDEX, NOFOLLOW">
-		<?php $this->html('headlinks') ?>
-		<title><?php $this->text('pagetitle') ?></title>
-		<?php print $css ?>
 
-		<!--[if lt IE 7]><script type="<?php $this->text('jsmimetype') ?>" src="<?php $this->text('stylepath') ?>/common/IEFixes.js?<?php echo $GLOBALS['wgStyleVersion'] ?>"></script>
-		<meta http-equiv="imagetoolbar" content="no" /><![endif]-->
-
-		<?php print Skin::makeGlobalVariablesScript( $this->data ); ?>
-
-		<script type="<?php $this->text('jsmimetype') ?>" src="<?php $this->text('stylepath' ) ?>/common/wikibits.js?<?php echo $GLOBALS['wgStyleVersion'] ?>"><!-- wikibits js --></script>
-		<!-- Head Scripts -->
-<?php $this->html('headscripts') ?>
-<?php	if($this->data['jsvarurl']) { ?>
-		<script type="<?php $this->text('jsmimetype') ?>" src="<?php $this->text('jsvarurl') ?>"><!-- site js --></script>
-<?php	} ?>
-<?php	if($this->data['pagecss']) { ?>
-		<style type="text/css"><?php $this->html('pagecss') ?></style>
-<?php	}
-		if($this->data['usercss']) { ?>
-		<style type="text/css"><?php $this->html('usercss') ?></style>
-<?php	}
-		if($this->data['userjs']) { ?>
-		<script type="<?php $this->text('jsmimetype') ?>" src="<?php $this->text('userjs' ) ?>"></script>
-<?php	}
-		if($this->data['userjsprev']) { ?>
-		<script type="<?php $this->text('jsmimetype') ?>"><?php $this->html('userjsprev') ?></script>
-<?php	}
-		if($this->data['trackbackhtml']) print $this->data['trackbackhtml']; ?>
-	</head>
-<body<?php if($this->data['body_ondblclick']) { ?> ondblclick="<?php $this->text('body_ondblclick') ?>"<?php } ?>
-<?php if($this->data['body_onload']) { ?> onload="<?php $this->text('body_onload') ?>"<?php } ?>
- class="mediawiki <?php $this->text('dir') ?> <?php $this->text('pageclass') ?> <?php $this->text('skinnameclass') ?>">
-<div id="fb-root"></div>
-<script>(function(d, s, id) {
-  var js, fjs = d.getElementsByTagName(s)[0];
-  if (d.getElementById(id)) {return;}
-  js = d.createElement(s); js.id = id;
-  js.src = "//connect.facebook.net/en_US/all.js#xfbml=1";
-  fjs.parentNode.insertBefore(js, fjs);
-}(document, 'script', 'facebook-jssdk'));</script>
+		$this->html( 'headelement' );?>
 	<table class="pageWrapper" cellpadding="0" cellspacing="0" width="100%"><tr><td align="center">
 	<table id="globalWrapper" cellpadding="0" cellspacing="0"><tr><td>
 	<table class="pageWrapper" cellpadding="0" cellspacing="0" width="100%"><tr><td id="column-one"><div id="c1-div">
@@ -117,21 +65,17 @@ class OrganicDesignTemplate extends QuickTemplate {
 	<div class="portlet" id="p-personal">
 		<h5><?php $this->msg('personaltools') ?></h5>
 		<div class="pBody">
-			<ul>
-<?php 			foreach($this->data['personal_urls'] as $key => $item) { ?>
-				<li id="pt-<?php echo Sanitizer::escapeId($key) ?>"<?php
-					if ($item['active']) { ?> class="active"<?php } ?>><a href="<?php
-				echo htmlspecialchars($item['href']) ?>"<?php echo $skin->tooltipAndAccesskey('pt-'.$key) ?><?php
-				if(!empty($item['class'])) { ?> class="<?php
-				echo htmlspecialchars($item['class']) ?>"<?php } ?>><?php
-				echo htmlspecialchars($item['text']) ?></a></li>
-<?php			} ?>
+			<ul<?php $this->html('userlangattributes') ?>>
+<?php		foreach($this->getPersonalTools() as $key => $item) { ?>
+				<?php echo $this->makeListItem($key, $item); ?>
+
+<?php		} ?>
 			</ul>
 		</div>
 	</div>
 
 <?php
-# Get avatar image
+// Get avatar image
 global $wgUser,$wgUploadDirectory,$wgUploadPath;
 if ($wgUser->isLoggedIn()) {
 	?><div id="p-avatar"><?php
@@ -150,7 +94,7 @@ if ($wgUser->isLoggedIn()) {
 ?>
 
 <?php
-# Donations
+// Donations
 global $wgOrganicDesignDonations;
 if ( $wgOrganicDesignDonations ) {?>
 	<div class="portlet" id="donations" >
@@ -190,7 +134,7 @@ if ( $wgOrganicDesignDonations ) {?>
 </div>
 
 <?php
-# MediaWiki:Sidebar
+// MediaWiki:Sidebar
 global $wgUser,$wgTitle,$wgParser;
 $title = 'od-sidebar';
 $article = new Article( Title::newFromText( $title, NS_MEDIAWIKI ) );
@@ -211,34 +155,28 @@ echo $psr->parse( $text, $wgTitle, $opt, true, true )->getText();
 		</tr>
 		<td id="shadow-l">
 		<td width="100%" id="content">
-			<div id="p-cactions" class="portlet">
-				<h5><?php $this->msg('views') ?></h5>
-				<div class="pBody">
-					<ul>
-			<?php			foreach($this->data['content_actions'] as $key => $tab) { ?>
-						 <li id="ca-<?php echo Sanitizer::escapeId($key) ?>"<?php
-								if($tab['class']) { ?> class="<?php echo htmlspecialchars($tab['class']) ?>"<?php }
-							 ?>><a href="<?php echo htmlspecialchars($tab['href']) ?>"<?php echo $skin->tooltipAndAccesskey('ca-'.$key) ?>><?php
-							 echo htmlspecialchars($tab['text']) ?></a></li>
-			<?php			 } ?>
-					</ul>
-				</div>
-			</div>
-			<a name="top" id="top"></a>
-			<?php if($this->data['sitenotice']) { ?><div id="siteNotice"><?php $this->html('sitenotice') ?></div><?php } ?>
-			<h1 class="firstHeading"><?php $this->html('title') ?></h1>
-			<div id="bodyContent">
-				<h3 id="siteSub"><?php $this->msg('tagline') ?></h3>
-				<div id="contentSub"<?php $this->html('userlangattributes') ?>><?php $this->html('subtitle') ?></div>
-				<?php if($this->data['undelete']) { ?><div id="contentSub2"><?php     $this->html('undelete') ?></div><?php } ?>
-				<?php if($this->data['newtalk'] ) { ?><div class="usermessage"><?php $this->html('newtalk')  ?></div><?php } ?>
-				<?php if($this->data['showjumplinks']) { ?><div id="jump-to-nav"><?php $this->msg('jumpto') ?> <a href="#column-one"><?php $this->msg('jumptonavigation') ?></a>, <a href="#searchInput"><?php $this->msg('jumptosearch') ?></a></div><?php } ?>
-				<!-- start content -->
-				<?php $this->html('bodytext') ?>
-				<?php if($this->data['catlinks']) { ?><?php       $this->html('catlinks') ?><?php } ?>
-				<!-- end content -->
-				<div class="visualClear"></div>
-			</div>
+			<?php $this->cactions(); ?>
+	<a id="top"></a>
+	<?php if($this->data['sitenotice']) { ?><div id="siteNotice"><?php $this->html('sitenotice') ?></div><?php } ?>
+
+	<h1 id="firstHeading" class="firstHeading"><span dir="auto"><?php $this->html('title') ?></span></h1>
+	<div id="bodyContent" class="mw-body">
+		<div id="siteSub"><?php $this->msg('tagline') ?></div>
+		<div id="contentSub"<?php $this->html('userlangattributes') ?>><?php $this->html('subtitle') ?></div>
+<?php if($this->data['undelete']) { ?>
+		<div id="contentSub2"><?php $this->html('undelete') ?></div>
+<?php } ?><?php if($this->data['newtalk'] ) { ?>
+		<div class="usermessage"><?php $this->html('newtalk')  ?></div>
+<?php } ?><?php if($this->data['showjumplinks']) { ?>
+		<div id="jump-to-nav" class="mw-jump"><?php $this->msg('jumpto') ?> <a href="#column-one"><?php $this->msg('jumptonavigation') ?></a>, <a href="#searchInput"><?php $this->msg('jumptosearch') ?></a></div>
+<?php } ?>
+		<!-- start content -->
+<?php $this->html('bodytext') ?>
+		<?php if($this->data['catlinks']) { $this->html('catlinks'); } ?>
+		<!-- end content -->
+		<?php if($this->data['dataAfterContent']) { $this->html ('dataAfterContent'); } ?>
+		<div class="visualClear"></div>
+	</div>
 		</td>
 			<td valign="top" id="shadow-r"><div id="logo-r"></div></td>
 		</tr>
