a3f19147ce657e99954d380d4c94dffb789c963b
Aran Dunkley <aran@organicdesign.co.nz>
2012-09-26 12:31:59 +0000

    updating to work with 119
    git-svn-id: svn+ssh://organicdesign.co.nz/extensions@1309 2ad5491c-c162-49ae-9d3b-afa169216a3e

--- ExtraMagic/ExtraMagic.php
+++ ExtraMagic/ExtraMagic.php
@@ -12,7 +12,7 @@
  */
 if( !defined( 'MEDIAWIKI' ) ) die('Not an entry point.' );
 
-define( 'EXTRAMAGIC_VERSION', '2.3.0, 2012-02-29' );
+define( 'EXTRAMAGIC_VERSION', '3.0.0, 2012-09-25' );
 
 $wgExtensionCredits['parserhook'][] = array(
 	'name'        => 'ExtraMagic',
@@ -35,165 +35,176 @@ $wgCustomVariables = array(
 	'THIS'
 );
 
-$wgExtensionFunctions[]                         = 'efSetupExtraMagic';
-$wgHooks['MagicWordMagicWords'][]               = 'efAddCustomVariable';
-$wgHooks['MagicWordwgVariableIDs'][]            = 'efAddCustomVariableID';
-$wgHooks['LanguageGetMagic'][]                  = 'efAddCustomVariableLang';
-$wgHooks['ParserGetVariableValueSwitch'][]      = 'efGetCustomVariable';
 
-/**
- * Called from $wgExtensionFunctions array when initialising extensions
- */
-function efSetupExtraMagic() {
-	global $wgParser;
-	$wgParser->setFunctionHook( 'REQUEST', 'efExtraMagicExpandRequest', SFH_NO_HASH );
-	$wgParser->setFunctionHook( 'COOKIE',  'efExtraMagicExpandCookie',  SFH_NO_HASH );
-	$wgParser->setFunctionHook( 'USERID',  'efExtraMagicExpandUserID',  SFH_NO_HASH );
-	$wgParser->setFunctionHook( 'AVATAR',  'efExtraMagicExpandAvatar',  SFH_NO_HASH );
-	$wgParser->setFunctionHook( 'IFGROUP', 'efExtraMagicExpandIfGroup' );
-}
+class ExtraMagic {
 
-/**
- * Register magic words
- */
-function efAddCustomVariable( &$magicWords ) {
-	global $wgCustomVariables;
-	foreach( $wgCustomVariables as $var ) $magicWords[] = "MAG_$var";
-	return true;
-}
-
-function efAddCustomVariableID( &$variables ) {
-	global $wgCustomVariables;
-	foreach( $wgCustomVariables as $var ) $variables[] = constant( "MAG_$var" );
-	return true;
-}
-
-function efAddCustomVariableLang( &$langMagic, $langCode = 0 ) {
-	global $wgCustomVariables;
-
-	// Magic words
-	foreach( $wgCustomVariables as $var ) {
-		$magic = "MAG_$var";
-		$langMagic[defined( $magic ) ? constant( $magic ) : $magic] = array( $langCode, $var );
+	function __construct() {
+		global $wgHooks, $wgExtensionFunctions;
+		$wgExtensionFunctions[] = array( $this, 'setup' );
+		$wgHooks['LanguageGetMagic'][] = $this;
+		$wgHooks['MagicWordwgVariableIDs'][] = $this;
+		//$wgHooks['ParserGetVariableValueVarCache'][] = $this;
+		$wgHooks['ParserGetVariableValueSwitch'][] = $this;
 	}
 
-	// Parser functions
-	$langMagic['REQUEST'] = array( $langCode, 'REQUEST' );
-	$langMagic['COOKIE']  = array( $langCode, 'COOKIE' );
-	$langMagic['USERID']  = array( $langCode, 'USERID' );
-	$langMagic['AVATAR']  = array( $langCode, 'AVATAR' );
-	$langMagic['IFGROUP'] = array( $langCode, 'IFGROUP' );
-
-	return true;
-}
-
-/**
- * Expand parser functions
- */
-function efExtraMagicExpandRequest( &$parser, $param, $default = '', $seperator = "\n" ) {
-	$parser->disableCache();
-	$val = array_key_exists( $param, $_REQUEST ) ? $_REQUEST[$param] : $default;
-	if( is_array( $val ) ) $val = implode( $seperator, $val );
-	return $val;
-}
-
-function efExtraMagicExpandCookie( &$parser, $param, $default = '' ) {
-	$parser->disableCache();
-	return array_key_exists( $param, $_COOKIE ) ? $_COOKIE[$param] : $default;
-}
-
-function efExtraMagicExpandUserID( &$parser, $param ) {
-	if( $param ) {
-		$col = strpos( $param, ' ' ) ? 'user_real_name' : 'user_name';
-		$dbr = wfGetDB( DB_SLAVE );
-		if( $row = $dbr->selectRow( 'user', array( 'user_id' ), array( $col => $param ) ) ) return $row->user_id;
-	} else {
-		global $wgUser;
-		return $wgUser->getID();
+	function setup() {
+		global $wgParser;
+		$wgParser->setFunctionHook( 'REQUEST', array( $this, 'expandRequest' ), SFH_NO_HASH );
+		$wgParser->setFunctionHook( 'COOKIE',  array( $this, 'expandCookie' ), SFH_NO_HASH );
+		$wgParser->setFunctionHook( 'USERID',  array( $this, 'expandUserID' ), SFH_NO_HASH );
+		$wgParser->setFunctionHook( 'AVATAR',  array( $this, 'expandAvatar' ), SFH_NO_HASH );
+		$wgParser->setFunctionHook( 'IFGROUP', array( $this, 'expandIfGroup' ) );
 	}
-	return '';
-}
 
-function efExtraMagicExpandAvatar( &$parser, $param ) {
-	if( $id = efExtraMagicExpandUserID( $parser, $param ) ) {
-		global $wgSitename, $wgUploadDirectory, $wgUploadPath;
-		$files = glob( "$wgUploadDirectory/avatar-$wgSitename-$id.*" );
-		if( count( $files ) > 0 ) {
-			return "$wgUploadPath/" . basename( $files[0] );
+	function onLanguageGetMagic( &$magicWords, $langCode = null ) {
+		global $wgCustomVariables;
+ 
+ 		// Magic words
+		foreach( $wgCustomVariables as $var ) $magicWords[strtolower( $var )] = array( 1, $var );
+
+		// Parser functions
+		$magicWords['request'] = array( 0, 'REQUEST' );
+		$magicWords['cookie']  = array( 0, 'COOKIE' );
+		$magicWords['userid']  = array( 0, 'USERID' );
+		$magicWords['avatar']  = array( 0, 'AVATAR' );
+		$magicWords['ifgroup'] = array( 0, 'IFGROUP' );
+
+		return true;
+	}
+
+	function onMagicWordwgVariableIDs( &$variableIDs ) {
+		global $wgCustomVariables;
+		foreach( $wgCustomVariables as $var ) $variableIDs[] = strtolower( $var );
+		return true;
+	}
+
+	function onParserGetVariableValueVarCache( &$parser, &$varCache ) {
+
+		$varCache['uuid'] = $v4String ? $v4String : 'NULL';
+		return true;
+	}
+
+
+	/**
+	 * Process variable values
+	 */
+	function onParserGetVariableValueSwitch( &$parser, &$cache, &$index, &$ret ) {
+		print "$index<br>";
+		return true;
+		switch( $index ) {
+
+			case MAG_CURRENTUSER:
+				global $wgUser;
+				$parser->disableCache();
+				$ret = $wgUser->mName;
+			break;
+
+			case MAG_CURRENTPERSON:
+				global $wgUser;
+				$parser->disableCache();
+				$ret = $wgUser->getRealName();
+			break;
+
+			case MAG_CURRENTLANG:
+				global $wgUser;
+				$parser->disableCache();
+			break;
+				$ret = $wgUser->getOption( 'language' );
+
+			case MAG_CURRENTSKIN:
+				global $wgUser;
+				$parser->disableCache();
+				$ret = $wgUser->getOption( 'skin' );
+			break;
+
+			case MAG_ARTICLEID:
+				global $wgTitle;
+				if ( is_object( $wgTitle ) ) {
+					$ret = $wgTitle->getArticleID();
+				} else $ret = 'No revision ID!';
+			break;
+
+			case MAG_IPADDRESS:
+				$parser->disableCache();
+				$ret = $_SERVER['REMOTE_ADDR'];
+			break;
+
+			case MAG_DOMAIN:
+				$parser->disableCache();
+				$ret = str_replace( 'www.', '', $_SERVER['SERVER_NAME'] );
+			break;
+
+			case MAG_NUMBERINGOFF:
+				global $wgUser;
+				$wgUser->setOption( 'numberheadings', false );
+				$ret = '';
+			break;
+
+			case MAG_GUID:
+				$parser->disableCache();
+				$ret = strftime( '%Y%m%d', time() ) . '-' . substr( strtoupper( uniqid('', true) ), -5 );
+			break;
+
+			case MAG_THIS:
+				global $wgThisTemplateName;
+				$ret = $wgThisTemplateName;
+
 		}
+		return true;
 	}
-	return '';
-}
 
-function efExtraMagicExpandIfGroup( &$parser, $groups, $then, $else = '' ) {
-	global $wgUser;
-	$intersection = array_intersect( array_map( 'strtolower', explode( ',', $groups ) ), $wgUser->getEffectiveGroups() );
-	return count( $intersection ) > 0 ? $then : $else;
-}
 
-/**
- * Process variable values
- */
-function efGetCustomVariable( &$parser, &$cache, &$index, &$ret ) {
-	switch( $index ) {
 
-		case MAG_CURRENTUSER:
-			global $wgUser;
-			$parser->disableCache();
-			$ret = $wgUser->mName;
-		break;
-
-		case MAG_CURRENTPERSON:
-			global $wgUser;
-			$parser->disableCache();
-			$ret = $wgUser->getRealName();
-		break;
-
-		case MAG_CURRENTLANG:
-			global $wgUser;
-			$parser->disableCache();
-		break;
-			$ret = $wgUser->getOption( 'language' );
-
-		case MAG_CURRENTSKIN:
-			global $wgUser;
-			$parser->disableCache();
-			$ret = $wgUser->getOption( 'skin' );
-		break;
-
-		case MAG_ARTICLEID:
-			global $wgTitle;
-			if ( is_object( $wgTitle ) ) {
-				$ret = $wgTitle->getArticleID();
-			} else $ret = 'No revision ID!';
-		break;
-
-		case MAG_IPADDRESS:
-			$parser->disableCache();
-			$ret = $_SERVER['REMOTE_ADDR'];
-		break;
-
-		case MAG_DOMAIN:
-			$parser->disableCache();
-			$ret = str_replace( 'www.', '', $_SERVER['SERVER_NAME'] );
-		break;
-
-		case MAG_NUMBERINGOFF:
-			global $wgUser;
-			$wgUser->setOption( 'numberheadings', false );
-			$ret = '';
-		break;
-
-		case MAG_GUID:
-			$parser->disableCache();
-			$ret = strftime( '%Y%m%d', time() ) . '-' . substr( strtoupper( uniqid('', true) ), -5 );
-		break;
-
-		case MAG_THIS:
-			global $wgThisTemplateName;
-			$ret = $wgThisTemplateName;
-
+	/**
+	 * Expand parser functions
+	 */
+	function expandRequest( &$parser, $param, $default = '', $seperator = "\n" ) {
+		$parser->disableCache();
+		$val = array_key_exists( $param, $_REQUEST ) ? $_REQUEST[$param] : $default;
+		if( is_array( $val ) ) $val = implode( $seperator, $val );
+		return $val;
 	}
-	return true;
-}
 
+	function expandCookie( &$parser, $param, $default = '' ) {
+		$parser->disableCache();
+		return array_key_exists( $param, $_COOKIE ) ? $_COOKIE[$param] : $default;
+	}
+
+	function expandUserID( &$parser, $param ) {
+		if( $param ) {
+			$col = strpos( $param, ' ' ) ? 'user_real_name' : 'user_name';
+			$dbr = wfGetDB( DB_SLAVE );
+			if( $row = $dbr->selectRow( 'user', array( 'user_id' ), array( $col => $param ) ) ) return $row->user_id;
+		} else {
+			global $wgUser;
+			return $wgUser->getID();
+		}
+		return '';
+	}
+
+	function expandAvatar( &$parser, $param ) {
+		if( $id = efExtraMagicExpandUserID( $parser, $param ) ) {
+			global $wgSitename, $wgUploadDirectory, $wgUploadPath;
+			$files = glob( "$wgUploadDirectory/avatar-$wgSitename-$id.*" );
+			if( count( $files ) > 0 ) {
+				return "$wgUploadPath/" . basename( $files[0] );
+			}
+		}
+		return '';
+	}
+
+	function expandIfGroup( &$parser, $groups, $then, $else = '' ) {
+		global $wgUser;
+		$intersection = array_intersect( array_map( 'strtolower', explode( ',', $groups ) ), $wgUser->getEffectiveGroups() );
+		return count( $intersection ) > 0 ? $then : $else;
+	}
+
+
+
+
+
+
+
+
+}
+new ExtraMagic();
