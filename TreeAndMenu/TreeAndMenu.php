372752150c4077a65d842f2ac71b72fe10dd0cb6
Aran Dunkley <aran@organicdesign.co.nz>
2012-09-26 20:15:22 +0000

    debug
    git-svn-id: svn+ssh://organicdesign.co.nz/extensions@1355 2ad5491c-c162-49ae-9d3b-afa169216a3e

--- TreeAndMenu/TreeAndMenu.php
+++ TreeAndMenu/TreeAndMenu.php
@@ -192,7 +192,7 @@ class TreeAndMenu {
 		if( !preg_match_all( "/\x7f\x7f1$u\x7f(.+?)\x7f/", $text, $subs ) ) $subs = array( 1 => array() );
 
 
-if( preg_match( "/\x7f/", $text ) ) print 'match<br>';
+if( preg_match( "/\x7f1$u/", $text ) ) print 'match<br>';
 
 		// Extract all the formatted tree rows in the page and if any, replace with dTree JavaScript
 		if( preg_match_all( "/\x7f1$u\x7f(.+?)\x7f([0-9]+)\x7f({$u}3(.+?){$u}4)?(.*?)(?=\x7f[12]$u)/", $text, $matches, PREG_SET_ORDER ) ) {
