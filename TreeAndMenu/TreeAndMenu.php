de56df9794a69403cf0e64e9cf1948143fb2d6cd
Aran Dunkley <aran@organicdesign.co.nz>
2012-09-26 21:22:02 +0000

    try no x7f
    git-svn-id: svn+ssh://organicdesign.co.nz/extensions@1381 2ad5491c-c162-49ae-9d3b-afa169216a3e

--- TreeAndMenu/TreeAndMenu.php
+++ TreeAndMenu/TreeAndMenu.php
@@ -193,7 +193,7 @@ class TreeAndMenu {
 		if( !preg_match_all( "/~x7f~x7f1$u~x7f(.+?)~x7f/", $text, $subs ) ) $subs = array( 1 => array() );
 
 		// Extract all the formatted tree rows in the page and if any, replace with dTree JavaScript
-		if( preg_match_all( "/~x7f1$u~x7f(.+?)~x7f([0-9]+)~x7f({$u}3(.+?){$u}4)?(.*?)(?=~x7f[12]$u)/", $text, $matches, PREG_SET_ORDER ) ) {
+		if( preg_match_all( "/~x7f1$u~x7f(.+?)~x7f([0-9]+)~x7f({$u}3(.+?){$u}4)?(.*?)(?=~x7f[12]$u)/s", $text, $matches, PREG_SET_ORDER ) ) {
 
 			// PASS-1: build $rows array containing depth, and tree start/end information
 			$rows   = array();
