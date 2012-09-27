fc72c7e18299516bd05f88d6aba1f9a8963c1d88
Aran Dunkley <aran@organicdesign.co.nz>
2012-09-27 21:24:15 +0000

    use jquery document ready
    git-svn-id: svn+ssh://organicdesign.co.nz/extensions@1506 2ad5491c-c162-49ae-9d3b-afa169216a3e

--- FormMailer/FormMailer.php
+++ FormMailer/FormMailer.php
@@ -89,7 +89,7 @@ function wfSetupFormMailer() {
 		$(document).ready(function() {
 			e = document.getElementsByTagName( 'input' );
 			for( i = 0; i < e.length; i++ ) {
-				if( e[i].name == 'formmailer' ) e[i].name += '$ap';
+				//if( e[i].name == 'formmailer' ) e[i].name += '$ap';
 			}
 		});
 		</script>" );
