599542c977815959c6a7ec6830907e32d00e07a7
Aran Dunkley <aran@organicdesign.co.nz>
2012-09-27 21:54:10 +0000

    test if comments allowed in php
    git-svn-id: svn+ssh://organicdesign.co.nz/extensions@1516 2ad5491c-c162-49ae-9d3b-afa169216a3e

--- AjaxComments/AjaxComments.php
+++ AjaxComments/AjaxComments.php
@@ -47,7 +47,7 @@ class AjaxComments {
 		// Create a hook to allow external condition for whether there should be comments
 		$ret = true;
 		$title = array_key_exists( 'title', $_GET ) ? Title::newFromText( $_GET['title'] ) : false;
-		if( !is_object( $title ))
+		if( !is_object( $title ) || $title->getArticleID() == 0 || $title->isRedirect() )
 			$ret = false;
 		if( $ret ) wfRunHooks( 'AjaxCommentsCheckTitle', array( $title, &$ret ) );
 		if( $ret ) {
