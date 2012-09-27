0525be7cdb709b11830b62450919309ebc10146c
Aran Dunkley <aran@organicdesign.co.nz>
2012-09-27 20:19:02 +0000

    can't use bool for hooks
    git-svn-id: svn+ssh://organicdesign.co.nz/extensions@1502 2ad5491c-c162-49ae-9d3b-afa169216a3e

--- AjaxComments/AjaxComments.php
+++ AjaxComments/AjaxComments.php
@@ -47,7 +47,8 @@ class AjaxComments {
 		// Create a hook to allow external condition for whether there should be comments
 		$ret = true;
 		$title = array_key_exists( 'title', $_GET ) ? Title::newFromText( $_GET['title'] ) : false;
-		wfRunHooks( 'AjaxCommentsCheckTitle', array( $title, &$ret ) );
+		if( !is_object( $title ) ) $ret = false;
+		if( $ret ) wfRunHooks( 'AjaxCommentsCheckTitle', array( $title, &$ret ) );
 		if( $ret ) {
 
 			$wgHooks['MediaWikiPerformAction'][] = $this;
