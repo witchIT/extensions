6fbb26cd8a968d1ed8792f459dd29aad6d7f0a3a
Aran Dunkley <aran@organicdesign.co.nz>
2012-09-27 19:18:09 +0000

    change hooks to bool
    git-svn-id: svn+ssh://organicdesign.co.nz/extensions@1460 2ad5491c-c162-49ae-9d3b-afa169216a3e

--- jQueryUpload/jQueryUpload_body.php
+++ jQueryUpload/jQueryUpload_body.php
@@ -11,8 +11,6 @@
  */
 class jQueryUpload extends SpecialPage {
 
-	var $title = false;
-
 	function __construct() {
 		global $wgHooks;
 
@@ -20,8 +18,8 @@ class jQueryUpload extends SpecialPage {
 		parent::__construct( 'jQueryUpload', 'upload' );
 
 		// Check if this page should be able to have files attached (by default allow attachments for all existing article)
-		$this->title = array_key_exists( 'title', $_GET ) ? Title::newFromText( $_GET['title'] ) : false;
-		$attach = is_object( $this->title ) && $this->title->getArticleID() && !array_key_exists( 'action', $_REQUEST );
+		$title = array_key_exists( 'title', $_GET ) ? Title::newFromText( $_GET['title'] ) : false;
+		$attach = is_object( $this ) && $this->getArticleID() && !array_key_exists( 'action', $_REQUEST );
 		if( !wfRunHooks( 'jQueryUploadAddAttachLink', array( $title ) ) ) $attach = false;
 
 		// If attachments allowed in this page, add the module into the page
