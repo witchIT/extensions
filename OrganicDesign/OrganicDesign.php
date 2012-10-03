d907bd6cfe286dc588224f2035077059869f9f76
Aran Dunkley <aran@organicdesign.co.nz>
2012-10-03 18:36:18 +0000

    for od domain for non http access
    git-svn-id: svn+ssh://organicdesign.co.nz/extensions@1559 2ad5491c-c162-49ae-9d3b-afa169216a3e

--- OrganicDesign/OrganicDesign.php
+++ OrganicDesign/OrganicDesign.php
@@ -12,18 +12,18 @@ if( !defined( 'MEDIAWIKI' ) ) die( "Not an entry point." );
 define( 'OD_VERSION', "0.0.0, 2012-09-27" );
 
 $wgExtensionCredits['other'][] = array(
-	'name'        => "OrganicDesign",
-	'author'      => "[http://www.organicdesign.co.nz/nad Aran Dunkley]",
+	'name'		=> "OrganicDesign",
+	'author'	  => "[http://www.organicdesign.co.nz/nad Aran Dunkley]",
 	'description' => "An extension to encapsulate all the functionality specific to the Organic Design wiki",
-	'url'         => "http://www.organicdesign.co.nz",
-	'version'     => OD_VERSION
+	'url'		 => "http://www.organicdesign.co.nz",
+	'version'	 => OD_VERSION
 );
 
 // Register the CSS file for the OrganicDesign skin
 $wgResourceModules['skins.organicdesign'] = array(
-        'styles' => array( 'organicdesign.css' => array( 'media' => 'screen' ) ),
-        'remoteBasePath' => "$wgStylePath/organicdesign",
-        'localBasePath' => "$IP/skins/organicdesign",
+		'styles' => array( 'organicdesign.css' => array( 'media' => 'screen' ) ),
+		'remoteBasePath' => "$wgStylePath/organicdesign",
+		'localBasePath' => "$IP/skins/organicdesign",
 );
 
 class OrganicDesign {
@@ -44,25 +44,25 @@ class OrganicDesign {
 		self::$title = array_key_exists( 'title', $_REQUEST ) ? Title::newFromText( $_REQUEST['title'] ) : false;
 
 		// Bounce requests to https for sysops and non-https for non-sysops, and force www prefix
-        $host = $_SERVER['HTTP_HOST'];
-        $uri = $_SERVER['REQUEST_URI'];
-        $ssl = isset( $_SERVER['HTTPS'] );
-        $www = preg_match( "|^www|", $host ) ? '' : 'www.';
-        if( in_array( 'sysop', $wgUser->getEffectiveGroups() ) ) {
-                if( $www || !$ssl ) {
-                        header( "Location: https://$www$host$uri" );
-                        exit;
-                }
-        } else {
-                if( $ssl && ( !array_key_exists( 'title', $_REQUEST ) || $_REQUEST['title'] != 'Special:UserLogin' ) ) {
-                        header( "Location: http://$www$host$uri" );
-                        exit;
-                }
-                if( $www && preg_match( '|organicdesign.+[^t]$|', $host ) ) {
-                        header( "Location: http://$www$host$uri" );
-                        exit;
-                }
-        }
+		$host = $_SERVER['HTTP_HOST'];
+		$uri = $_SERVER['REQUEST_URI'];
+		$ssl = isset( $_SERVER['HTTPS'] );
+		$www = preg_match( "|^www|", $host ) ? '' : 'www.';
+		if( in_array( 'sysop', $wgUser->getEffectiveGroups() ) ) {
+				if( $www || !$ssl ) {
+						header( "Location: https://$www$host$uri" );
+						exit;
+				}
+		} else {
+				if( $ssl && ( !array_key_exists( 'title', $_REQUEST ) || $_REQUEST['title'] != 'Special:UserLogin' ) ) {
+						header( "Location: http://www.organicdesign.co.nz$uri" );
+						exit;
+				}
+				if( $www && preg_match( '|organicdesign.+[^t]$|', $host ) ) {
+						header( "Location: http://$www$host$uri" );
+						exit;
+				}
+		}
 	}
 
 	/**
