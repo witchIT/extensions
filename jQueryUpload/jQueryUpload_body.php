84b59cb3be820f6796f254ef8522af4dd8f44f8d
Aran Dunkley <aran@organicdesign.co.nz>
2012-11-27 13:40:08 +0000

    jQueryUploadSetId hook
    git-svn-id: svn+ssh://organicdesign.co.nz/extensions@1567 2ad5491c-c162-49ae-9d3b-afa169216a3e

--- jQueryUpload/jQueryUpload_body.php
+++ jQueryUpload/jQueryUpload_body.php
@@ -23,7 +23,7 @@ class jQueryUpload extends SpecialPage {
 		// If attachments allowed in this page, add the module into the page
 		$title = array_key_exists( 'title', $_GET ) ? Title::newFromText( $_GET['title'] ) : false;
 		$this->id = $title->getArticleID();
-		wfRunHooks( 'jQueryUploadSetId', array( $title, &$this->id ) )
+		wfRunHooks( 'jQueryUploadSetId', array( $title, &$this->id ) );
 		$attach = is_object( $title ) && $this->id && !$title->isRedirect()
 			&& !array_key_exists( 'action', $_REQUEST ) && $title->getNamespace() != 6;
 		if( $attach ) wfRunHooks( 'jQueryUploadAddAttachLink', array( $title, &$attach ) );
