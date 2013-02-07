6c1df46ee2e33ed0c3c20e77b592e5f2b0637d01
Aran Dunkley <aran@organicdesign.co.nz>
2013-02-07 18:02:46 +0000

    change from old comment style
    git-svn-id: svn+ssh://organicdesign.co.nz/extensions@1600 2ad5491c-c162-49ae-9d3b-afa169216a3e

--- EmailPage/EmailPage.php
+++ EmailPage/EmailPage.php
@@ -13,15 +13,15 @@
  */
 if( !defined( 'MEDIAWIKI' ) ) die( "Not an entry point." );
 
-define( 'EMAILPAGE_VERSION', "2.1.2, 2010-11-01" );
+define( 'EMAILPAGE_VERSION', "2.1.3, 2013-02-07" );
 
-$wgEmailPageGroup           = "sysop";               # Users must belong to this group to send emails (empty string means anyone can send)
-$wgEmailPageCss             = false;                 # A minimal CSS page to embed in the email (eg. monobook/main.css without portlets, actions etc)
-$wgEmailPageAllowRemoteAddr = array( "127.0.0.1" );  # Allow anonymous sending from these addresses
-$wgEmailPageAllowAllUsers   = false;                 # Whether to allow sending to all users (the "user" group)
-$wgEmailPageToolboxLink     = true;                  # Add a link to the sidebar toolbox?
-$wgEmailPageActionLink      = true;                  # Add a link to the actions links?
-$wgEmailPageSepPattern      = "|[\\x00-\\x20,;*]+|"; # Regular expression for splitting emails
+$wgEmailPageGroup           = "sysop";               // Users must belong to this group to send emails (empty string means anyone can send)
+$wgEmailPageCss             = false;                 // A minimal CSS page to embed in the email (eg. monobook/main.css without portlets, actions etc)
+$wgEmailPageAllowRemoteAddr = array( "127.0.0.1" );  // Allow anonymous sending from these addresses
+$wgEmailPageAllowAllUsers   = false;                 // Whether to allow sending to all users (the "user" group)
+$wgEmailPageToolboxLink     = true;                  // Add a link to the sidebar toolbox?
+$wgEmailPageActionLink      = true;                  // Add a link to the actions links?
+$wgEmailPageSepPattern      = "|[\\x00-\\x20,;*]+|"; // Regular expression for splitting emails
 if( $wgEmailPageGroup ) $wgGroupPermissions['sysop'][$wgEmailPageGroup] = true;
 
 if( isset( $_SERVER['SERVER_ADDR'] ) ) $wgEmailPageAllowRemoteAddr[] = $_SERVER['SERVER_ADDR'];
@@ -41,13 +41,13 @@ $wgExtensionCredits['specialpage'][] = array(
 	'version'        => EMAILPAGE_VERSION
 );
 
-# If form has been posted, include the phpmailer class
+// If form has been posted, include the phpmailer class
 if( isset( $_REQUEST['ea-send'] ) ) {
 	if( $files = glob( "$dir/*/class.phpmailer.php" ) ) require_once( $files[0] );
 	else die( "PHPMailer class not found!" );
 }
 
-# Add toolbox and action links
+// Add toolbox and action links
 if( $wgEmailPageToolboxLink ) $wgHooks['SkinTemplateToolboxEnd'][] = 'wfEmailPageToolboxLink';
 if( $wgEmailPageActionLink )  {
 	$wgHooks['SkinTemplateTabs'][] = 'wfEmailPageActionLink';
