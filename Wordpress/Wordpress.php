b890cebde35a1a52199d3dfb30ad66af7f7cfd85
Aran Dunkley <aran@organicdesign.co.nz>
2013-05-31 20:22:35 +0000

    not current user anymore
    git-svn-id: svn+ssh://organicdesign.co.nz/extensions@1676 2ad5491c-c162-49ae-9d3b-afa169216a3e

--- Wordpress/Wordpress.php
+++ Wordpress/Wordpress.php
@@ -30,7 +30,6 @@ class Wordpress {
 		header( 'Content-Type: application/json' );
 		$data = array();
 		$user = User::newFromId( $id );
-		print $user->getToken();
 		if( $user->getToken() == $token ) {
 			$data['id'] = $user->getId();
 			$data['name'] = $user->getName();
