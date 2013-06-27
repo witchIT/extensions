684218eaaceb8c848a6b0f5aad8bacab79db75a1
Aran Dunkley <aran@organicdesign.co.nz>
2013-06-27 18:20:32 +0000

    error reporting - AND NOT srict
    git-svn-id: svn+ssh://organicdesign.co.nz/extensions@1722 2ad5491c-c162-49ae-9d3b-afa169216a3e

--- wikia.php
+++ wikia.php
@@ -23,7 +23,7 @@ ini_set( 'memory_limit', '128M' );
 
 // Need to turn of strict warnings as too many third-party extensions raise errors
 ini_set('display_errors', 'Off'); 
-error_reporting(E_ALL & ~E_STRICT);
+//error_reporting(E_ALL & ~E_STRICT);
 
 // Constants
 define( 'WIKIA_VERSION', '1.2.10, 2013-06-27' );
