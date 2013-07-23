f0c6fe9b211e3cc3ee0ec854242871cd286ad447
Aran Dunkley <aran@organicdesign.co.nz>
2013-07-23 14:20:51 +0000

    add zenovia wp theme
    git-svn-id: svn+ssh://organicdesign.co.nz/extensions@1756 2ad5491c-c162-49ae-9d3b-afa169216a3e

---
+++ skins/zenovia-wp/footer.php
@@ -0,0 +1,11 @@
+<div id="footer">
+<p><strong><?php bloginfo('name');?></strong> &copy; <?php echo date(__('Y','ml'));?> <?php _e('All Rights Reserved.','ml');?></p>
+<p class="right">
+	<span><a href="http://wpthemes.info/misty-look/" title="<?php _e('MistyLook WordPress Theme by Sadish Bala','ml');?>"><?php _e('Free WordPress Themes','ml');?></a> | <a href="http://www.freehostreview.com/" title="Free Web Space" target="_blank">Free Web Space</a></span>
+</p>
+<br class="clear" />
+</div><!-- end id:footer -->
+<?php wp_footer();?>
+
+</body>
+</html>
---
+++ skins/zenovia-wp/sidebar.php
@@ -0,0 +1,46 @@
+<div id="sidebar">
+<ul>
+<li class="sidebox">
+        <h3><?php _e('Search','ml');?></h3>
+        <?php get_search_form();?>
+</li>
+<?php mistylook_ShowRecentPosts();?>
+<li class="sidebox">
+        <h3><?php _e('Recent Comments','ml');?></h3>
+        <ul><?php get_comments('count=6');?></ul>
+</li>
+<li class="sidebox">
+	<h3><?php _e('Archives','ml'); ?></h3>
+	<ul><?php wp_get_archives('type=monthly&show_post_count=true'); ?></ul>
+</li>
+
+<li class="sidebox">
+	<h3><?php _e('Categories','ml'); ?></h3>
+	<ul>
+		<?php
+		if (function_exists('wp_list_categories'))
+		{
+			wp_list_categories('show_count=1&depth=1&&hierarchical=1&title_li=');
+		}
+		else
+		{
+			wp_list_cats('optioncount=1');
+		}
+		?>
+	</ul>
+</li>
+<li class="sidebox">
+	<h3><?php _e('Meta','ml'); ?></h3>
+	<ul>
+		<?php wp_register(); ?>
+		<li><?php wp_loginout(); ?></li>
+		<li><a href="http://validator.w3.org/check/referer" title="<?php _e('This page validates as XHTML 1.0 Transitional','ml');?>"><?php _e('Valid','ml');?> <abbr title="eXtensible HyperText Markup Language">XHTML</abbr></a></li>
+		<li><a href="http://gmpg.org/xfn/"><abbr title="<?php _e('XHTML Friends Network','ml');?>">XFN</abbr></a></li>
+		<li><a href="http://wordpress.org/" title="<?php _e('Powered by WordPress, state-of-the-art semantic personal publishing platform.','ml');?>">WordPress</a></li>
+		<?php wp_meta(); ?>
+	</ul>
+</li>
+</ul>
+</div><!-- end id:sidebar -->
+</div><!-- end id:content -->
+</div><!-- end id:container -->
