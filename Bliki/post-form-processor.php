6a3a08bc6dbf4a9a75b4ba3f858682bcdd071387
Aran Dunkley <aran@organicdesign.co.nz>
2013-07-28 01:26:56 +0000

    blog sig
    git-svn-id: svn+ssh://organicdesign.co.nz/extensions@1780 2ad5491c-c162-49ae-9d3b-afa169216a3e

--- Bliki/post-form-processor.php
+++ Bliki/post-form-processor.php
@@ -22,7 +22,7 @@ function wfBlogPost( $action, $article ) {
 			$summary = $wgRequest->getText( 'summary' );
 			$content = $wgRequest->getText( 'content' );
 			$user = $wgUser->getName();
-			$date = time();
+			$date = date('@');
 			$sig = "{{BlogSig|$user|$date}}";
 			$type = $wgRequest->getText( 'type' );
 			switch( $type ) {
