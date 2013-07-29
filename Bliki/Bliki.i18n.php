063a33dae57e0edb8958953bc374c166ef8f4a2e
Aran Dunkley <aran@organicdesign.co.nz>
2013-07-29 13:52:47 +0000

    i18n
    git-svn-id: svn+ssh://organicdesign.co.nz/extensions@1796 2ad5491c-c162-49ae-9d3b-afa169216a3e

---
+++ Bliki/Bliki.i18n.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Internationalisation for Bliki extension
+ *
+ * @author Aran Dunkley
+ * @file
+ * @ingroup Extensions
+ */
+
+$messages = array();
+
+/** English
+ * @author Dunkley
+ */
+$messages['en'] = array(
+	'blikifeed' => 'Bliki feed',
+);
