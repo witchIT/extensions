013d232557dbbf6544db9cef7addd3864976d750
Aran Dunkley <aran@organicdesign.co.nz>
2013-07-29 12:01:51 +0000

    move all into Bliki.php
    git-svn-id: svn+ssh://organicdesign.co.nz/extensions@1790 2ad5491c-c162-49ae-9d3b-afa169216a3e

--- Bliki/Bliki.php
+++ Bliki/Bliki.php
@@ -1,10 +1,19 @@
 <?php
-# Adds "Bliki" (blog in a wiki) functionality
-# See http://www.organicdesign.co.nz/bliki for details
-
+/**
+ * Bliki extension - Adds "Bliki" (blog in a wiki) functionality
+ * 
+ * See http://www.organicdesign.co.nz/bliki for more detail
+ *
+ * @package MediaWiki
+ * @subpackage Extensions
+ * @author [http://www.organicdesign.co.nz/nad Nad]
+ * @copyright © 2013 [http://www.organicdesign.co.nz/nad Nad]
+ * @licence GNU General Public Licence 2.0 or later
+ */
 if( !defined( 'MEDIAWIKI' ) ) die( 'Not an entry point.' );
+define( 'BLIKI_VERSION','2.0.1, 2013-07-29' );
 
-define( 'BLIKI_VERSION','2.0.0, 2013-07-27' );
+$wgBlikiAddBodyClass = false;
 
 $wgExtensionCredits['other'][] = array(
 	'path'        => __FILE__,
@@ -15,6 +24,91 @@ $wgExtensionCredits['other'][] = array(
 	'version'     => BLIKI_VERSION
 );
 
-$dir = dirname( __FILE__ );
-include( "$dir/post-form-processor.php" );
-include( "$dir/add-body-class.php" );
+class Bliki {
+	
+	function __construct() {
+		global $wgHooks, $wgBlikiAddBodyClass;
+
+		$wgHooks['UnknownAction'][] = $this;
+
+		if( $wgBlikiAddBodyClass ) $wgHooks['OutputPageBodyAttributes'][] = $this;
+
+	}
+
+	function onUnknownAction( $action, $article ) {
+		global $wgOut, $wgRequest, $wgUser, $wgParser;
+		if( $action == 'blog' ) {
+			$newtitle = $wgRequest->getText( 'newtitle' );
+			$title = Title::newFromText( $newtitle );
+			$error = false;
+			if( !is_object( $title ) ) {
+				$wgOut->addWikitext( '<div class="previewnote">Error: Bad title!</div>' );
+				$error = true;
+			}
+			elseif( $title->exists() ) {
+				$wgOut->addWikitext( '<div class="previewnote">Error: Title already exists!</div>' );
+				$error = true;
+			}
+			if( !$error ) {
+				$summary = $wgRequest->getText( 'summary' );
+				$content = $wgRequest->getText( 'content' );
+				$user = $wgUser->getName();
+				$date = date('U');
+				$sig = '<div class="blog-sig">{{BlogSig|' . "$user|@$date" . '}}</div>';
+				$type = $wgRequest->getText( 'type' );
+				switch( $type ) {
+
+					// Preview the item
+					case "Full preview":
+						$wikitext = "$sig\n$summary\n\n$content";
+						self::preview( $type, $title, $wikitext );
+						$article->view();
+					break;
+
+					// Preview the item in news/blog format
+					case "Summary preview":
+						$wikitext = "{|class=\"blog\"\n|\n== [[Post a blog item|$newtitle]] ==\n|-\n!$sig\n|-\n|$summary\n|}__NOEDITSECTION__";
+						$title = Title::newFromText( 'Blog' );
+						self::preview( $type, $title, $wikitext );
+						$article->view();
+					break;
+
+					// Create the item with tags as category links
+					case "Post":
+						$wikitext = '{{' . "Blog|1=$summary|2=$content" . '}}';
+						$wikitext .= "<noinclude>[[Category:Blog items]][[Category:Posts by $user]]";
+						foreach( array_keys( $_POST ) as $k ) {
+							if( preg_match( "|^tag(.+)$|", $k, $m ) ) {
+								$wikitext .= '[[Category:' . str_replace( '_', ' ', $m[1] ) . ']]';
+							}
+						}
+						$wikitext .= "</noinclude>";
+						$article = new Article( $title );
+						$article->doEdit( $wikitext, 'Blog item created via post form', EDIT_NEW );
+						$wgOut->redirect( $title->getFullURL() );
+					break;
+				}
+			} else $article->view();
+			return false;
+		}
+		return true;
+	}
+
+	function preview( $heading, $title, $wikitext ) {
+		global $wgOut, $wgParser;
+		$wgOut->addWikitext( '<div class="previewnote">' . wfMsg( 'previewnote' ) . '</div>' );
+		$wgOut->addWikitext( "== $heading ==" );
+		$wgOut->addHTML( $wgParser->parse( $wikitext, $title, new ParserOptions(), true, true )->getText() );
+		$wgOut->addHTML( "<br /><hr /><br />" );
+	}
+
+	function onOutputPageBodyAttributes( $out, $sk, &$bodyAttrs ) {
+		global $wgTitle;
+		$id  = $wgTitle->getArticleID();
+		$dbr = wfGetDB( DB_SLAVE );
+		if( $dbr->selectRow( 'categorylinks', '1', "cl_from = $id AND cl_to = 'Blog_items'" ) ) $bodyAttrs['class'] .= ' blog-item';
+		return true;
+	}
+}
+
+new Bliki();
