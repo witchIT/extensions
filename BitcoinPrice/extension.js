8ec274be5512c7abd7405e5834a5aab606c1a8c9
Aran Dunkley <aran@organicdesign.co.nz>
2013-11-24 13:02:49 +0000

    change structure to use menuButton instead of just label
    git-svn-id: svn+ssh://organicdesign.co.nz/extensions@1902 2ad5491c-c162-49ae-9d3b-afa169216a3e

--- BitcoinPrice/extension.js
+++ BitcoinPrice/extension.js
@@ -13,87 +13,90 @@ const Convenience = Local.imports.convenience;
 const Settings = Local.imports.settings;
 const Soup = imports.gi.Soup;
 
-let paneltext;
-let panelicon;
-let panelbox;
-let check_settings;
-let update_price;
-let update_price_regular;
-let settings;
-let currencies = ["USD","AUD","CHF","NOK","RUB","DKK","JPY","CAD","NZD","PLN","CNY","SEK","SGD","HKD","EUR"];
-let extPath;
-
+let extpath;
 function init(metadata) {
-	extPath = metadata.path;
+	extpath = metadata.path;
 }
 
-function enable() {
+let BitcoinPriceMenuButton = new Lang.Class({
+	Name: 'BitcoinPriceMenuButton',
+	Extends: PanelMenu.Button,
 
-	// Text
-	settings = Convenience.getSettings();
-	paneltext = new St.Bin({ style_class: 'bitcoinprice-text' });
-	let text = new St.Label({ text: '...' });
-	paneltext.set_child(text);
+	_init: function(extpath) {
+		this._settings = Convenience.getSettings();
+		this.parent(0.25);
 
-	// Icon
-	let icon = new St.Icon({ style_class: 'bitcoinprice-icon' });
-	icon.set_gicon(new Gio.FileIcon({ file: Gio.file_new_for_path(GLib.build_filenamev([extPath, 'Bitcoin-icon.png']))}));
-	panelicon = new St.Bin({ style_class: 'panel-button', reactive: true, can_focus: true, x_fill: true, y_fill: false, track_hover: true });
-	panelicon.set_child(icon);
+		// Text & icon
+		this.paneltext = new St.Label({ text: '...' });
+		let icon = new St.Icon({ style_class: 'bitcoinprice-icon' });
+		icon.set_gicon(new Gio.FileIcon({ file: Gio.file_new_for_path(GLib.build_filenamev([extpath, 'Bitcoin-icon.png']))}));
+		this.panelicon = new St.Bin({ style_class: 'panel-button', reactive: true, can_focus: true, x_fill: true, y_fill: false, track_hover: true });
+		this.panelicon.set_child(icon);
 
-	// Add them both to the panel box
-	panelbox = new St.BoxLayout();
-	panelbox.add_actor(panelicon);
-	panelbox.add_actor(paneltext);	
+		// Add text & icon to a topbox
+		let topBox = new St.BoxLayout();
+		topBox.add_actor(this.panelicon);
+		topBox.add_actor(this.paneltext);
+		this.actor.add_actor(topBox);
 
-	// Add the panel box to the panel
-    let children = Main.panel._rightBox.get_children();
-    Main.panel._rightBox.insert_child_at_index(panelbox, children.length - 2);
+		// Add the menu
+		let item = new PopupMenu.PopupSeparatorMenuItem();
+		this.menu.addMenuItem(item);
+		Main.panel._menus.addMenu(this.menu);
+		Main.panel._rightBox.insert_child_at_index(this.actor, 0);
 
-	// Check for changes to settings called every 2 seconds
-	check_settings = function() {
-		let settings_data = Settings.getSettings(settings);
-		if(settings_data.reload_now == true) {
-			settings_data.reload_now = false;
-			settings.set_string("settings-json", JSON.stringify(settings_data));
-			let text = new St.Label({ text: '...' });
-			paneltext.set_child(text);
-			update_price();
-		}
-		Mainloop.timeout_add_seconds(2, check_settings);
-	};
-	check_settings();
+		// Call the regular update and settings check
+		this.update_price();
+		this.check_settings();
+	},
 
-	// Function to retrieve the price and update the panel label
-	update_price = function() {
+	stop: function() {
+		if(this._timeoutSrc) Mainloop.source_remove(this._timeoutSrc);
+		if(this._checkSettingsSrc) Mainloop.source_remove(this._checkSettingsSrc);
+	},
+
+	update_price: function() {
 
 		// Get the currency setting and make the url
-		let settings_data = Settings.getSettings(settings);
-		let currency = currencies[settings_data.currency];
+		let settings = Settings.getSettings(this._settings);
+		let currencies = ["USD","AUD","CHF","NOK","RUB","DKK","JPY","CAD","NZD","PLN","CNY","SEK","SGD","HKD","EUR"];
+		let currency = currencies[settings.currency];
 		let url = 'http://mtgox.com/api/1/BTC' + currency + '/ticker';
+		let text = this.paneltext;
 
 		// Request the MtGox data
 		let session = new Soup.SessionAsync();
 		let message = Soup.Message.new('GET', url);
 		session.queue_message(message, function(session, message) {
 			let json = JSON.parse(message.response_body.data);
-			if(json.result == 'success') {
-				let text = new St.Label({ text: json['return']['last']['display'] });
-				paneltext.set_child(text);
-			}
+			if(json.result == 'success') text.set_text(json['return']['last']['display']);
 		});
-	};
 
-	// Call the update price function on a regular interval
-	update_price_regular = function() {
-		update_price();
-		let settings_data = Settings.getSettings(settings);
+		// Call regularly
 		let period = 120; //settings_data.refresh_period;
-		Mainloop.timeout_add_seconds(period, update_price_regular);
-	};
-	update_price_regular();
+		this._timeoutSrc = Mainloop.timeout_add_seconds(period, this.update_price);
+	},
+
+	check_settings: function() {
+		let settings = Settings.getSettings(this._settings);
+		if(settings.reload_now == true) {
+			settings.reload_now = false;
+			this._settings.set_string("settings-json", JSON.stringify(settings));
+			this.paneltext.set_text('...');
+			if(this._timeoutSrc) Mainloop.source_remove(this._timeoutSrc);
+			this.update_price();
+		}
+		this._checkSettingsSrc = Mainloop.timeout_add_seconds(2, this.check_settings);
+	}
+});
+
+let bitcoinpriceMenu;
+function enable() {
+    bitcoinpriceMenu = new BitcoinPriceMenuButton(extpath);
+    Main.panel.addToStatusArea('bitcoinpriceMenu', bitcoinpriceMenu);
 }
 
 function disable() {
-	Main.panel._rightBox.remove_child(panelbox);
+	bitcoinpriceMenu.stop();
+	bitcoinpriceMenu.destroy();
 }
