43f0ba71a6fb2846812c22fe191857e12c0448af
Aran Dunkley <aran@organicdesign.co.nz>
2014-07-03 18:23:35 +0000

    IN not working, use multiple OR
    git-svn-id: svn+ssh://organicdesign.co.nz/extensions@1970 2ad5491c-c162-49ae-9d3b-afa169216a3e

--- Bliki/BlikiFeed.php
+++ Bliki/BlikiFeed.php
@@ -42,10 +42,11 @@ class SpecialBlikiFeed extends SpecialRecentChanges {
 			$conds[] = 'rc_new=1';
 			$dbr = wfGetDB( DB_SLAVE );
 			if( is_array( $opts['bliki'] ) ) {
-				foreach( $opts['bliki'] as $i => $cat ) $opts['bliki'][$i] = Title::newFromText( $cat )->getDBkey();
-				$catCond = 'cl_to IN (' . $dbr->makeList( $opts['bliki'] ) . ')';
+				$tmp = array();
+				foreach( $opts['bliki'] as $cat ) $tmp[] = 'cl_to=' . $dbr->addQuotes( Title::newFromText( $cat )->getDBkey() );
+				$catCond = '(' . join( ' OR ', $tmp ) . ')';
 			} else $catCond = 'cl_to =' . $dbr->addQuotes( Title::newFromText( $opts['bliki'] )->getDBkey() );
-			$join_conds['categorylinks'] = array( 'RIGHT JOIN', "cl_from=page_id AND ($catCond)" );
+			$join_conds['categorylinks'] = array( 'RIGHT JOIN', "cl_from=page_id AND $catCond" );
 		}
 		return true;
 	}
