774ea2c0155333274ef824a85fb03e38b8634f6a
Aran Dunkley <aran@organicdesign.co.nz>
2014-07-03 18:32:13 +0000

    IN not working, use multiple OR
    git-svn-id: svn+ssh://organicdesign.co.nz/extensions@1973 2ad5491c-c162-49ae-9d3b-afa169216a3e

--- Bliki/BlikiFeed.php
+++ Bliki/BlikiFeed.php
@@ -46,7 +46,7 @@ class SpecialBlikiFeed extends SpecialRecentChanges {
 				foreach( $opts['bliki'] as $cat ) $tmp[] = 'cl_to=' . $dbr->addQuotes( Title::newFromText( $cat )->getDBkey() );
 				$catCond = '(' . join( ' OR ', $tmp ) . ')';
 			} else $catCond = 'cl_to =' . $dbr->addQuotes( Title::newFromText( $opts['bliki'] )->getDBkey() );
-			$join_conds['categorylinks'] = array( 'RIGHT JOIN', "cl_from=page_id AND $catCond" );
+			$join_conds['categorylinks'] = array( 'RIGHT JOIN', "cl_from=page_id AND $catCond AND foo" );
 		}
 		return true;
 	}
