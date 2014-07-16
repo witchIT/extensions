8f0240f46219e36ac149110f1c82ed50af84ffc3
Aran Dunkley <aran@organicdesign.co.nz>
2014-07-16 22:26:12 +0000

    just stick with what works for now
    git-svn-id: svn+ssh://organicdesign.co.nz/extensions@1988 2ad5491c-c162-49ae-9d3b-afa169216a3e

--- Bliki/BlikiFeed.php
+++ Bliki/BlikiFeed.php
@@ -40,8 +40,6 @@ class SpecialBlikiFeed extends SpecialRecentChanges {
  		$res = parent::doMainQuery( $conds, $opts );
 		$user->mRights = $rights;
 
-$dbr = wfGetDB( DB_SLAVE );
-print_r($dbr->lastQuery());
 		return $res;
 	}
 
