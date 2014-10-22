fb3e6993f535e9e83f3cca3a85090701fc9c1f97
Aran Dunkley <aran@organicdesign.co.nz>
2014-10-22 13:53:13 +0000

    still not working tho - add debugging to prev
    git-svn-id: svn+ssh://organicdesign.co.nz/extensions@2038 2ad5491c-c162-49ae-9d3b-afa169216a3e

--- ExtraMagic/ExtraMagic.php
+++ ExtraMagic/ExtraMagic.php
@@ -194,6 +194,7 @@ class ExtraMagic {
 		$r = '';
 		$l = preg_replace( '|\s*\[\[.+|', '', $l ); // ensure there's no "further results" link on the end
 		$l = explode( '#', $l );
+	print_r($l);
 		$i = array_search( $wgTitle->getPrefixedText(), $l );
 		if( $i !== false && array_key_exists( $i+$j, $l ) ) $r = $l[$i+$j];
 		return $r;
