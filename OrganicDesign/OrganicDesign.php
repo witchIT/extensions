144ceb7852b8efc947287d9960f8455a7b898e52
Aran Dunkley <aran@organicdesign.co.nz>
2014-11-01 15:54:45 +0000

    add all the content from the old skin
    git-svn-id: svn+ssh://organicdesign.co.nz/extensions@2050 2ad5491c-c162-49ae-9d3b-afa169216a3e

--- OrganicDesign/OrganicDesign.php
+++ OrganicDesign/OrganicDesign.php
@@ -160,8 +160,8 @@ class OrganicDesign {
 
 	public static function languages( $out ) {
 		$out->addHTML( '<div id="languages" style="display:none">
-			<a href="http://www.organicdesign.co.nz' . $uri . '" title="English"><img src="/wiki/skins/organicdesign/uk.png" /></a>
-			<a href="http://pt.organicdesign.co.nz' . $uri . '" title="Português brasileiro"><img src="/wiki/skins/organicdesign/br.png" /></a>
+			<a href="http://www.organicdesign.co.nz' . $_SERVER['REQUEST_URI'] . '" title="English"><img src="/wiki/skins/organicdesign/uk.png" /></a>
+			<a href="http://pt.organicdesign.co.nz' . $_SERVER['REQUEST_URI'] . '" title="Português brasileiro"><img src="/wiki/skins/organicdesign/br.png" /></a>
 		</div>' );
 	}
 
