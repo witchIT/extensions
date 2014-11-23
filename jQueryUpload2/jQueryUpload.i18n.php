3d14b8d91f2024b860720dd8773d33b8a209e0f2
Aran Dunkley <aran@organicdesign.co.nz>
2014-11-23 11:53:50 +0000

    start new version using native mediawiki files
    git-svn-id: svn+ssh://organicdesign.co.nz/extensions@2056 2ad5491c-c162-49ae-9d3b-afa169216a3e

---
+++ jQueryUpload2/jQueryUpload.i18n.php
@@ -0,0 +1,23 @@
+<?php
+/**
+* Internationalisation file for the jQueryUpload Extension.
+*
+* @file
+* @ingroup Extensions
+*/
+
+$messages = array();
+
+$messages['en'] = array(
+	'jqueryupload' => "jQuery File Upload",
+	'jqueryupload-desc' => "Allows files to be uploaded to the wiki or to specific pages using the jQueryFileUpload module",
+	'jqueryupload-attach' => "Attach file",
+	'jqueryupload-attachments' => "File attachments",
+	'jqueryupload-uploadinfo' => "Uploaded by $1 on $2",
+	'jqueryupload-enterdesc' => "Enter a brief description",
+	'jqueryupload-add' => "Add file(s)",
+	'jqueryupload-delsel' => "Delete file(s)",
+	'jqueryupload-del' => "Delete",
+	'jqueryupload-start' => "Start upload",
+	'jqueryupload-cancel' => "Cancel",
+);
