fbbcb0b86298bd2bebf060fc7923f0a1990e84e3
Aran Dunkley <aran@organicdesign.co.nz>
2014-11-28 19:12:01 +0000

    don't allow invalid email, send confirmation
    git-svn-id: svn+ssh://organicdesign.co.nz/extensions@2068 2ad5491c-c162-49ae-9d3b-afa169216a3e

--- FormMailer/formmailer.js
+++ FormMailer/formmailer.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
 	e = document.getElementsByTagName( 'input' );
 	for( i = 0; i < e.length; i++ ) {
-		if( e[i].name == 'formmailer' ) e[i].name += mw.config.get('wgFormMailerAP');
+		if( e[i].name == 'formmailer' ) e[i].name += '-' + mw.config.get('wgFormMailerAP');
 	}
 });
