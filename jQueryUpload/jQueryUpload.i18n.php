fd42edbe26145d6240d72f02a8740bacb19ecdc7
Aran Dunkley <aran@organicdesign.co.nz>
2014-12-08 10:56:54 +0000

    add initial pt version
    git-svn-id: svn+ssh://organicdesign.co.nz/extensions@2073 2ad5491c-c162-49ae-9d3b-afa169216a3e

--- jQueryUpload/jQueryUpload.i18n.php
+++ jQueryUpload/jQueryUpload.i18n.php
@@ -21,3 +21,17 @@ $messages['en'] = array(
 	'jqueryupload-start' => "Start upload",
 	'jqueryupload-cancel' => "Cancel",
 );
+
+$messages['pt'] = array(
+	'jqueryupload' => "jQuery File Upload",
+	'jqueryupload-desc' => "Permite que arquivos sejam carregados para o wiki ou para páginas específicas, utilizando o módulo jQueryFileUpload",
+	'jqueryupload-attach' => "Anexar arquivo",
+	'jqueryupload-attachments' => "Arquivo anexos",
+	'jqueryupload-uploadinfo' => "Carregado pela $1 às $2",
+	'jqueryupload-enterdesc' => "Digite uma breve descrição",
+	'jqueryupload-add' => "Adicionar arquivo(s)",
+	'jqueryupload-delsel' => "Apagar arquivo(s)",
+	'jqueryupload-del' => "Apagar",
+	'jqueryupload-start' => "Começar a carga",
+	'jqueryupload-cancel' => "Cancelar",
+);
