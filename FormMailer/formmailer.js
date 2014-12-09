a669e53a96b9d2a06f3089fb6df935d424ed28f6
Aran Dunkley <aran@organicdesign.co.nz>
2014-12-09 14:59:24 +0000

    use jquery selector and attr set
    git-svn-id: svn+ssh://organicdesign.co.nz/extensions@2091 2ad5491c-c162-49ae-9d3b-afa169216a3e

--- FormMailer/formmailer.js
+++ FormMailer/formmailer.js
@@ -1,6 +1,5 @@
 $(document).ready(function() {
-	e = document.getElementsByTagName( 'input' );
-	for( i = 0; i < e.length; i++ ) {
-		if( e[i].name == mw.config.get('wgFormMailerAP') ) e[i].name += '-' + mw.config.get('wgFormMailerAP');
-	}
+	$('input[name=' + mw.config.get('wgFormMailerVarName') + ']').each(function() {
+		$(this).attr('name', $(this).attr('name') + '-' + mw.config.get('wgFormMailerAP') );
+	});
 });
