5946b6b5bfcc3b05bd4ed635d19a47d0fecd11d9
Aran Dunkley <aran@organicdesign.co.nz>
2015-03-22 12:21:59 +0000

    incl ui
    git-svn-id: svn+ssh://organicdesign.co.nz/extensions@2172 2ad5491c-c162-49ae-9d3b-afa169216a3e

--- TreeAndMenu/TreeAndMenu_body.php
+++ TreeAndMenu/TreeAndMenu_body.php
@@ -20,7 +20,7 @@ class TreeAndMenu {
 		$path  = $wgExtensionAssetsPath . '/' . basename( __DIR__ ) . '/fancytree';
 		$wgResourceModules['ext.fancytree'] = array(
 			'scripts'        => array( 'jquery.fancytree.js', 'jquery.fancytree.persist.js', 'fancytree.js' ),
-			'dependencies'   => array( 'jquery.effects.blind', 'jquery.cookie' ),
+			'dependencies'   => array( 'jquery.ui.core', 'jquery.effects.blind', 'jquery.cookie' ),
 			'remoteBasePath' => $path,
 			'localBasePath'  => __DIR__ . '/fancytree',
 		);
