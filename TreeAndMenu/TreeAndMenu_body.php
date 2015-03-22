8594b43c1f43ee4cb88196ff279bf072bd36d4dd
Aran Dunkley <aran@organicdesign.co.nz>
2015-03-22 18:41:43 +0000

    forget parsing root
    git-svn-id: svn+ssh://organicdesign.co.nz/extensions@2188 2ad5491c-c162-49ae-9d3b-afa169216a3e

--- TreeAndMenu/TreeAndMenu_body.php
+++ TreeAndMenu/TreeAndMenu_body.php
@@ -83,9 +83,6 @@ class TreeAndMenu {
 			} else $opts[$opt] = true;
 		}
 
-		// If root arg sent, parse it
-		$root = array_key_exists( 'root', $atts ) ? $parser->parse( $atts['root'], $parser->getTitle(), $parser->getOptions(), true, false )->getText() : '';
-
 		// Sanitise the bullet structure (remove empty lines and empty bullets) and parse it to html
 		$bullets = preg_replace( '|^\*+\s*$|m', '', $bullets );
 		$bullets = preg_replace( '|\n+|', "\n", $bullets );
@@ -104,8 +101,8 @@ class TreeAndMenu {
 
 				// Mark the structure as tree data, wrap in an unclosable top level if root arg passed
 				$tree = '<ul id="treeData" style="display:none">';
-				if( $root ) {
-					$html = $tree . "<li>$root$html</li></ul>";
+				if( array_key_exists( 'root', $atts ) ) {
+					$html = $tree . '<li>' . $atts['root'] . $html . '</li></ul>';
 					$opts['minExpandLevel'] = 2;
 				} else $html = preg_replace( '|<ul>|', $tree, $html, 1 );
 
