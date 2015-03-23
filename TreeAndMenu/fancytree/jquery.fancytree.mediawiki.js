fa1c2f47c1a12b9aa3107a05a78b4c8b67b4bc2a
Aran Dunkley <aran@organicdesign.co.nz>
2015-03-23 18:54:57 +0000

    make links normal again
    git-svn-id: svn+ssh://organicdesign.co.nz/extensions@2213 2ad5491c-c162-49ae-9d3b-afa169216a3e

--- TreeAndMenu/fancytree/jquery.fancytree.mediawiki.js
+++ TreeAndMenu/fancytree/jquery.fancytree.mediawiki.js
@@ -55,7 +55,7 @@ $.ui.fancytree.registerExtension({
 		// Make nodes with hrefs back into normal links
 		opts.renderTitle = function(title) {
 			var s = '';
-			for(i in title) s+=i+'\n';
+			for(var i in title) s+=i+'\n';
 			console.log('info: '+s);
 		};
 
