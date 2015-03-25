b1288cd0ce376e2cdde576ce5c28810c8ac41d5a
Aran Dunkley <aran@organicdesign.co.nz>
2015-03-25 13:09:56 +0000

    ajax test - try treeCreate
    git-svn-id: svn+ssh://organicdesign.co.nz/extensions@2262 2ad5491c-c162-49ae-9d3b-afa169216a3e

--- TreeAndMenu/fancytree/jquery.fancytree.mediawiki.js
+++ TreeAndMenu/fancytree/jquery.fancytree.mediawiki.js
@@ -21,71 +21,69 @@
 var _assert = $.ui.fancytree.assert;
 
 
-/**
- * Open the tree to the node containing the passed title, or current page if none supplied
- */
-$.ui.fancytree._FancytreeClass.prototype.makeTitleVisible = function(title) {
-	var local = this.ext.mediawiki;
-	if(typeof(title) === 'undefined') title = mw.config.get('wgTitle');
-	this.visit(function(node) {
-		if(node.title == title) {
-			node.makeVisible({ noAnimation: true, noEvents: true, scrollIntoView: false });
-			node.setActive({ noEvents: true });
-			return false;
-		}
-	});
-};
-
-$.ui.fancytree.registerExtension({
-
-	name: "mediawiki",
-	version: "0.0.1",
-
-	// Default options for this extension.
-	options: {
-	},
-
-	// When a tree is initialised, do some modifications appropriate to mediawiki trees
-	treeInit: function(ctx) {
-		var tree = ctx.tree,
-			opts = ctx.options,
-			local = this._local,
-			instOpts = this.options.mediawiki;
-
-		// Make nodes with hrefs back into normal links
-		// - for samples of all events, see https://github.com/mar10/fancytree/blob/master/demo/sample-events.html
-		opts.renderNode = function(event, data) {
-			var node = data.node;
-			if(node.data.href) {
-				$('.fancytree-title',node.span).html('<a href="' + node.data.href + '" title="' + node.title + '">' + node.title + '</a>');
-			}
-		};
-		// Init the tree
-		return this._superApply(arguments);
-	},
-
-	// When a tree is initialised, do some modifications appropriate to mediawiki trees
-	treeCreate: function(ctx) {
-		var tree = ctx.tree,
-			opts = ctx.options,
-			local = this._local,
-			instOpts = this.options.mediawiki;
-
-		// Make nodes with titles starting with Ajax: into ajax loading nodes
-		// NOTE - move to render?
-		opts.lazyLoad = function(event, data) { alert('lazy');data.result = [{title: "node1"}, {title: "node2"}]; };
+	/**
+	 * Open the tree to the node containing the passed title, or current page if none supplied
+	 */
+	$.ui.fancytree._FancytreeClass.prototype.makeTitleVisible = function(title) {
+		var local = this.ext.mediawiki;
+		if(typeof(title) === 'undefined') title = mw.config.get('wgTitle');
 		this.visit(function(node) {
-			if(node.title == 'bar') alert(node.data.keys());
-			if('ajax' in node.data) {
-				alert(node.data.ajax);
-				node.lazy = true;
-				node.children = null;
+			if(node.title == title) {
+				node.makeVisible({ noAnimation: true, noEvents: true, scrollIntoView: false });
+				node.setActive({ noEvents: true });
+				return false;
 			}
 		});
+	};
 
-		// Init the tree
-		return this._superApply(arguments);
-	},
+	$.ui.fancytree.registerExtension({
 
-});
+		name: "mediawiki",
+		version: "0.0.1",
+
+		// Default options for this extension.
+		options: {
+		},
+
+		// When a tree is initialised, do some modifications appropriate to mediawiki trees
+		treeInit: function(ctx) {
+			var tree = ctx.tree,
+				opts = ctx.options,
+				local = this._local,
+				instOpts = this.options.mediawiki;
+
+			// Make nodes with hrefs back into normal links
+			// - for samples of all events, see https://github.com/mar10/fancytree/blob/master/demo/sample-events.html
+			opts.renderNode = function(event, data) {
+				var node = data.node;
+				if(node.data.href) {
+					$('.fancytree-title',node.span).html('<a href="' + node.data.href + '" title="' + node.title + '">' + node.title + '</a>');
+				}
+			};
+			// Init the tree
+			return this._superApply(arguments);
+		},
+
+		// When a tree is initialised, do some modifications appropriate to mediawiki trees
+		treeCreate: function(ctx) {
+			var tree = ctx.tree,
+				opts = ctx.options,
+				local = this._local,
+				instOpts = this.options.mediawiki;
+
+			// Make nodes with titles starting with Ajax: into ajax loading nodes
+			// NOTE - move to render?
+			opts.lazyLoad = function(event, data) { alert('lazy');data.result = [{title: "node1"}, {title: "node2"}]; };
+			this.visit(function(node) {
+				if(node.title == 'bar') alert(node.data.keys());
+				if('ajax' in node.data) {
+					alert(node.data.ajax);
+					node.lazy = true;
+					node.children = null;
+				}
+			});
+		},
+
+	});
+
 }(jQuery, window, document, mw));
