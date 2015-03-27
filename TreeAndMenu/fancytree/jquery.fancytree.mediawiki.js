990dbc3649eee76fd027c1328d216f1b58210ba1
Aran Dunkley <aran@organicdesign.co.nz>
2015-03-27 16:44:33 +0000

    try visit after superApply
    git-svn-id: svn+ssh://organicdesign.co.nz/extensions@2266 2ad5491c-c162-49ae-9d3b-afa169216a3e

--- TreeAndMenu/fancytree/jquery.fancytree.mediawiki.js
+++ TreeAndMenu/fancytree/jquery.fancytree.mediawiki.js
@@ -61,10 +61,12 @@ var _assert = $.ui.fancytree.assert;
 				}
 			};
 
+			var x = this._superApply(arguments);
+
 			// Make nodes with titles starting with Ajax: into ajax loading nodes
 			// NOTE - move to render?
 			opts.lazyLoad = function(event, data) { alert('lazy');data.result = [{title: "node1"}, {title: "node2"}]; };
-			ctx.visit(function(node) {
+			tree.visit(function(node) {
 				alert(node.title);
 				if(node.title == 'bar') alert(node.data.keys());
 				if('ajax' in node.data) {
@@ -75,7 +77,7 @@ var _assert = $.ui.fancytree.assert;
 			});
 
 			// Init the tree
-			return this._superApply(arguments);
+			return x;
 		},
 
 	});
