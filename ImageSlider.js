8b72f3647bf612c7ae75380e7272d333da5391da
Aran Dunkley <aran@organicdesign.co.nz>
2015-03-31 12:36:10 +0000

    no need to store w/h in element data as all are the same for each slider
    git-svn-id: svn+ssh://organicdesign.co.nz/extensions@2327 2ad5491c-c162-49ae-9d3b-afa169216a3e

--- ImageSlider.js
+++ ImageSlider.js
@@ -1,9 +1,11 @@
-$(document).ready( function() {
+$(document).ready(function() {
+
+	"use strict";
 
 	var delay = 5;
 
-	$('div.image-slider').each( function() {
-		var div = $(this);
+	$('div.image-slider').each(function() {
+		var div = $(this), w, h;
 
 		// Create default data for this slider and store it in the element
 		div.data('image', 0);
@@ -15,27 +17,27 @@ $(document).ready( function() {
 			var src = $(this).attr('src');
 			var image = $('<img />').attr('src', src);
 			div.data('images').push(src);
-			div.data('w', $(this).width());
-			div.data('h', $(this).height());
+			w = $(this).width();
+			h = $(this).height();
 		});
 
 		// Restructure the content of this sliders div into layered divs with prev/next buttons
 		var prev = '<a class="is-prev" href="javascript:">&lt; prev</a>';
 		var next = '<a class="is-next" href="javascript:">next &gt;</a>';
 		div.html( '<div class="is-img1"><div class="is-img2">' + prev + next + '</div></div>' );
-		$('.is-prev', div).click(function() { slide($('div.image-slider').has(this), -1 ); });
-		$('.is-next', div).click(function() { slide($('div.image-slider').has(this), 1 ); });
+		$('.is-prev', div).click(function() { slide($('div.image-slider').has(this), -1, w); });
+		$('.is-next', div).click(function() { slide($('div.image-slider').has(this), 1, w); });
 
 		// Set the cell size to the image size and other css styles
-		$('div',div).css({ padding: 0, width: div.data('w'), height: div.data('h') });
-		$('.is-prev',div).css({ float: 'left', 'margin-top': div.data('h')/2 });
-		$('.is-next',div).css({ float: 'right', 'margin-top': div.data('h')/2 });
+		$('div',div).css({ padding: 0, width: w, height: h });
+		$('.is-prev',div).css({ float: 'left', 'margin-top': h/2 });
+		$('.is-next',div).css({ float: 'right', 'margin-top': h/2 });
 
 		// Initialise the table's images to first image with zero offset
-		slide(div, 0);
+		slide(div, 0, w);
 	});
 
-	 function slide(div, dir) {
+	 function slide(div, dir, w) {
 
 		// Set the new image and animate to it (bail if already animating)
 		if(div.data('dir')) return;
@@ -44,7 +46,7 @@ $(document).ready( function() {
 
 		// Show next image on regular interval
 		if(div.data('timer')) clearTimeout(div.data('timer'));
-		div.data('timer', setTimeout(function() { slide(div,1); }, delay * 1000));
+		div.data('timer', setTimeout(function() { slide(div, 1, w); }, delay * 1000));
 
 		// Play an animation from the current image to the next
 		div.animate({ t: 1 }, {
@@ -58,11 +60,11 @@ $(document).ready( function() {
 				image += l * 1000000;
 				var next = ( image - dir ) % l;
 				image %= l;
-				var offset = -(div.data('dir') * fx.pos * div.data('w'));
+				var offset = -(div.data('dir') * fx.pos * w);
 
 				// Set the URL and position for the current image
 				$('.is-img1', div).css( 'background', 'transparent url("' + div.data('images')[image] 
-					+ '") no-repeat ' + (offset + div.data('w') * dir) + 'px center' );
+					+ '") no-repeat ' + (offset + w * dir) + 'px center' );
 
 				// Set the URL and position for the next image
 				$('.is-img2', div).css( 'background', 'transparent url("'
