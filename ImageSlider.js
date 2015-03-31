a56107133bb0749b1b6c7da6f5c244d94fa92d16
Aran Dunkley <aran@organicdesign.co.nz>
2015-03-31 18:11:09 +0000

    make thumb selections scroll upward instead
    git-svn-id: svn+ssh://organicdesign.co.nz/extensions@2329 2ad5491c-c162-49ae-9d3b-afa169216a3e

--- ImageSlider.js
+++ ImageSlider.js
@@ -2,17 +2,24 @@ $(document).ready(function() {
 
 	"use strict";
 
-	var delay = 5;
+	var delay = 2;
 	var thumbWidth = 100;
 
+	/**
+	 * Initialise all image-slider elements i the page and start them sliding
+	 */
 	$('div.image-slider').each(function() {
 		var div = $(this), w, h, src, thumb, img, prev, next;
 
-		// Create default data for this slider and store it in the element
-		div.data('image', 0);
-		div.data('last', 0);
-		div.data('dir', 0);
-		div.data('images', []);
+		// Initialise data structure in this slider element
+		div.data({
+			image: 1,
+			last: 0,
+			dir: 0,
+			images: [],
+			width: 0,
+			height: 0,
+		});
 
 		// If the slider has class "thumbs" then add another div that will have the thumbs in it
 		if(div.hasClass('thumbs')) thumb = $('<div />').addClass('thumbs');
@@ -22,15 +29,15 @@ $(document).ready(function() {
 			src = $(this).attr('src');
 			img = $('<img />').attr('src', src);
 			div.data('images').push(src);
-			w = $(this).width();
-			h = $(this).height();
+			div.data('width', w = $(this).width());
+			div.data('height', h = $(this).height());
 			if(thumb) {
 				img.width(thumbWidth);
 				img.height(h*thumbWidth/w);
 				img.css({float: 'left', cursor: 'pointer'});
 				img.data('index',div.data('images').length - 1);
 				img.click(function() {
-					slide($('div.image-slider').has(this), 1, w, $(this).data('index'));
+					slide($('div.image-slider').has(this), 1, $(this).data('index'));
 				});
 				thumb.append(img);
 			}
@@ -41,8 +48,8 @@ $(document).ready(function() {
 		next = '<a class="is-next" href="javascript:">next &gt;</a>';
 		div.html( '<div class="is-img1"><div class="is-img2">' + prev + next + '</div></div>' );
 		if(thumb) div.append(thumb);
-		$('.is-prev', div).click(function() { slide($('div.image-slider').has(this), -1, w); });
-		$('.is-next', div).click(function() { slide($('div.image-slider').has(this), 1, w); });
+		$('.is-prev', div).click(function() { slide($('div.image-slider').has(this), -1); });
+		$('.is-next', div).click(function() { slide($('div.image-slider').has(this), 1); });
 
 		// Set the container size to the image size and other css styles
 		$('div',div).css({ padding: 0, width: w, height: h });
@@ -50,11 +57,19 @@ $(document).ready(function() {
 		$('.is-next',div).css({ float: 'right', 'margin-top': h/2 });
 
 		// Start the sliding process
-		slide(div, 1, w);
+		slide(div, 1);
 	});
 
-	 function slide(div, dir, w, n) {
-		var l = div.data('images').length;
+	/**
+	 * Start animating the passed div
+	 * - dir is -1 or +1 for the direction to animate (left or right)
+	 * - n allows the new image to be specified rather than just next/prev (it will scroll upward)
+	 */
+	function slide(div, dir, n) {
+		var l = div.data('images').length,
+			w = div.data('width'),
+			h = div.data('height'),
+			img1, img2;
 
 		// Bail if already animating, else set animation to start
 		if(div.data('dir')) return; else div.data('dir', dir);
@@ -62,27 +77,31 @@ $(document).ready(function() {
 		// Set the new image either to the next according to the passed direction, or to n if passed
 		div.data('last', div.data('image'));
 		div.data('image', n === undefined ? (div.data('image') + dir + l) % l : n);
+		img1 = div.data('images')[div.data('image')];
+		img2 = div.data('images')[div.data('last')];
 
 		// Show next image on regular interval
 		if(div.data('timer')) clearTimeout(div.data('timer'));
-		div.data('timer', setTimeout(function() { slide(div, 1, w); }, delay * 1000));
+		div.data('timer', setTimeout(function() { slide(div, 1); }, delay * 1000));
 
 		// Play an animation from the current image to the next
 		div.animate({ t: 1 }, {
 			duration: 1000,
 			step: function(now, fx) {
-				var div = $(fx.elem), offset;
+				var div = $(fx.elem), offset, x1, y1, x2, y2;
 
 				// Set an offset in pixels for the transition between the current and last image
-				offset = -div.data('dir') * fx.pos * w;
+				offset = -div.data('dir') * fx.pos * (n === undefined ? w : h);
 
-				// Set the URL and position for the current image
-				$('.is-img1', div).css( 'background', 'transparent url("' + div.data('images')[div.data('image')] 
-					+ '") no-repeat ' + (offset + w * dir) + 'px center' );
+				// Calculate the positions of the current and last image (images specified with n scroll upward)
+				x1 = n === undefined ? offset + w * dir : 0;
+				x2 = n === undefined ? offset : 0;
+				y1 = n === undefined ? 0 : offset + h * dir;
+				y2 = n === undefined ? 0 : offset;
 
-				// Set the URL and position for the last image
-				$('.is-img2', div).css( 'background', 'transparent url("' + div.data('images')[div.data('last')]
-					+ '") no-repeat ' + offset + 'px center' );
+				// Set the positions of the images with CSS
+				$('.is-img1', div).css( 'background', 'url("' + img1 + '") no-repeat ' + x1 + 'px ' + y1 + 'px' );
+				$('.is-img2', div).css( 'background', 'url("' + img2 + '") no-repeat ' + x2 + 'px ' + y2 + 'px' );
 			},
 			complete: function(now, fx) {
 				$(this).data('dir', 0); // mark current slider as no longer animating
