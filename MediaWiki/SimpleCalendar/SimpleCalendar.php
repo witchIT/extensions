6978af8d5cbc049b19c67f2e6966aa0d6c834e2b
Aran <aran@organicdesign.co.nz>
2015-05-16 12:06:08 -0300

    use html not wikitext

--- MediaWiki/SimpleCalendar/SimpleCalendar.php
+++ MediaWiki/SimpleCalendar/SimpleCalendar.php
@@ -55,17 +55,18 @@ class SimpleCalendar {
 			$m = $argv['month'];
 			return self::renderMonth( strftime( '%m', strtotime( "$y-$m-01" ) ), $y, $p, $q, $f, $df );
 		} else $m = 1;
-		$table = "{| class=\"calendar\"\n";
-		for( $rows = 3; $rows--; $table .= "|-\n" )
+		$table = "<table class=\"calendar\"><tr>\n";
+		for( $rows = 3; $rows--; $table .= "</tr>\n<tr>" )
 			for( $cols = 0; $cols < 4; $cols++ )
-				$table .= '|' . self::renderMonth( $m++, $y, $p, $q, $f, $df ) . "\n";
-		return "$table\n|}\n";
+				$table .= '<td>' . self::renderMonth( $m++, $y, $p, $q, $f, $df ) . "</td>\n";
+		$table .= "\n</tr></table>\n";
+		return array( $table, 'isHTML' => true, 'noparse' => true );
 	}
 
 	/**
 	 * Return a calendar table of the passed month and year
 	 */
-	function renderMonth( $m, $y, $prefix = '', $query = '', $format = '%e %B %Y', $dayformat = false ) {
+	static function renderMonth( $m, $y, $prefix = '', $query = '', $format = '%e %B %Y', $dayformat = false ) {
 		$thisDay   = date( 'd' );
 		$thisMonth = date( 'n' );
 		$thisYear  = date( 'Y' );
@@ -74,24 +75,26 @@ class SimpleCalendar {
 		$days = array();
 		foreach( array( 'M', 'T', 'W', 'T', 'F', 'S', 'S' ) as $i => $day )
 			$days[] = $dayformat ? wfMessage( strftime( $dayformat, mktime( 0, 0, 0, 2, $i, 2000 ) ) )->text() : $day;
-		$table = "\n{| class=\"month\"\n|- class=\"heading\"\n|colspan=\"7\"|$month\n|- class=\"dow\"\n";
-		$table .= '|' . join( '||', $days ) . "\n";
-		if( $d > 1 ) $table .= "|-" . str_repeat( "\n|&nbsp;\n", $d - 1 );
+		$table = "\n<table border class=\"month\"\n<tr class=\"heading\"><th colspan=\"7\">$month</td></tr>\n";
+		$table .= '<tr class="dow"><th>' . implode( '</th><th>', $days ) . "</th></tr>";
+		$table .= "</tr>\n";
+		if( $d > 1 ) $table .= str_repeat( "<td>&nbsp;</td>", $d - 1 );
 		for( $i = $day = $d; $day < 32; $i++ ) {
 			$day = $i - $d + 1;
 			if( $day < 29 or checkdate( $m, $day, $y ) ) {
-				if( $i % 7 == 1 ) $table .= "\n|-\n";
-				$t = ( $day == $thisDay && $m == $thisMonth && $y == $thisYear ) ? ' class="today"' : '';
+				if( $i % 7 == 1 ) $table .= "\n</tr><tr>\n";
+				$t = ( $day == $thisDay && $m == $thisMonth && $y == $thisYear ) ? '  today' : '';
 				$ttext = $prefix . trim( strftime( $format, mktime( 0, 0, 0, $m, $day, $y ) ) );
 				$title = Title::newFromText( $ttext );
 				if( is_object( $title ) ) {
 					$class = $title->exists() ? 'day-active' : 'day-empty';
 					$url = $title->getLocalURL( $title->exists() ? '' : $query );
 				} else $url = $ttext;
-				$table .= "|$t|[$url <span class='$class'>$day</span>]\n";
+				$table .= "<td class='$class$t'><a href=\"$url\">$day</a></td>\n";
 			}
 		}
-		return "$table\n|}";
+		$table .= "\n</table>";
+		return $table;
 	}
 }
 
