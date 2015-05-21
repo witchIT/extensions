c996b51caa4d304d06d5b89e4abe4170e28c6155
Aran <aran@organicdesign.co.nz>
2015-05-21 16:47:32 -0300

    add lang file

---
+++ MediaWiki/CodeTidy/CodeTidy.i18n.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Internationalisation for CodeTidy extension
+ *
+ * @author Aran Dunkley
+ * @file
+ * @ingroup Extensions
+ */
+
+$messages = array();
+
+/** English
+ * @author Dunkley
+ */
+$messages['en'] = array(
+	'codetidy' => 'PHP Code Tidier',
+);
