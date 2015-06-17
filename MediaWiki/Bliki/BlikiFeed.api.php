2adde6f8ad7d31a638afd26b21a3aad9e6a3739e
Aran <aran@organicdesign.co.nz>
2015-06-17 18:56:41 -0300

    add 'days' option support - debug

--- MediaWiki/Bliki/BlikiFeed.api.php
+++ MediaWiki/Bliki/BlikiFeed.api.php
@@ -73,7 +73,7 @@ class ApiBlikiFeed extends ApiBase {
 		// Update the query condition and opts with the API params
 		if( $this->params['from'] ) $cond[] = 'rev_timestamp > ' . intval( $this->params['from'] );
 		elseif( $this->params['days'] ) $cond[] = 'rev_timestamp > ' . $dbr->timestamp( time() - intval( $this->param['days'] * 86400 ) );
-		print_r($conds);
+		print_r($cond);
 		if( $this->params['limit'] ) $opts['LIMIT'] = (integer)$this->params['limit'];
 
 		// Do the query
