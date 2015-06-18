76c640315fe227bd54cf72a9c3af48cad4a795e8
Aran <aran@organicdesign.co.nz>
2015-06-18 11:05:28 -0300

    try WikiPage

--- MediaWiki/OrganicDesign/OrganicDesign.php
+++ MediaWiki/OrganicDesign/OrganicDesign.php
@@ -120,13 +120,13 @@ class OrganicDesign {
 		// Add sidebar content
 		$title = Title::newFromText( 'Od-sidebar', NS_MEDIAWIKI );
 		$article = new WikiPage( $title );
-		$html = $out->parse( $article->getContent() );
+		$html = $out->parse( $article->getContent()->getNativeData() );
 		$out->addHTML( "<div id=\"wikitext-sidebar\" style=\"display:none\">$html</div>" );
 
 		// Add footer content
 		$title = Title::newFromText( 'Footer', NS_MEDIAWIKI );
 		$article = new WikiPage( $title );
-		$html = $out->parse( $article->getContent() );
+		$html = $out->parse( $article->getContent()->getNativeData() );
 		$out->addHTML( "<div id=\"wikitext-footer\" style=\"display:none\"><div id=\"od-footer\">$html</div></div>" );
 
 		// Add the other items
