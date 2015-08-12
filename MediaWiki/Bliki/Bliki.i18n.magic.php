36b41ced80612ef2493d8b7dd31990277192325a
Aran <aran@organicdesign.co.nz>
2015-08-12 13:08:46 -0300

    oops forgot to add magic i18n

---
+++ MediaWiki/Bliki/Bliki.i18n.magic.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Internationalization file for magic words.
+ */
+
+$magicWords = array();
+
+$magicWords['en'] = array(
+	'tags' => array( 0, 'tags' ),
+	'nextpost' => array( 0, 'nextpost' ),
+	'prevpost' => array( 0, 'prevpost' ),
+);
