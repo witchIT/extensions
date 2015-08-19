71fc66435a2d0b32c75d4e6a01043e876733b892
Aran <aran@organicdesign.co.nz>
2015-08-19 16:12:29 -0300

    add class

--- Joomla/BrazilWeight/brazilweight.php
+++ Joomla/BrazilWeight/brazilweight.php
@@ -12,17 +12,12 @@ defined('_JEXEC') or die;
  * @subpackage	System.mwsso
  * @since 2.5
  */
+
 class plgSystemBrazilWeight extends JPlugin {
 
-	/**
-	 * Install our extension into the shipping_ext table if its not there
-	 */
+	// Install our extension into the shipping_ext table if its not there
+	public function onAfterRoute() {
 /*
-	public function onAfterInitialise() {
-		$app = &JFactory::getApplication( 'site' );
-		$config = JFactory::getConfig();
-
-		// Insert our extension into the ext table if it's not there
 		$db = JFactory::getDbo();
 		$tbl = '#__jshopping_shipping_ext_calc';
 		$query = $db->getQuery( true );
@@ -37,23 +32,6 @@ class plgSystemBrazilWeight extends JPlugin {
 			$db->setQuery( $query );
 		}
 	}
-}
 */
-class sm_brazil_weight extends shippingextRoot {
-	var $version = 2;
-
-	function showShippingPriceForm($params, &$shipping_ext_row, &$template){        
-		echo 'prices calculated automatically';
-	}
-
-	function showConfigForm($config, &$shipping_ext, &$template){
-		echo 'no configuration required';
-	}
-
-	function getPrices( $cart, $params, $prices, &$shipping_ext_row, &$shipping_method_price ){
-		$weight = $cart->getWeightProducts();
-		$prices['shipping'] = $weight * 10;
-		$prices['package'] = 0;
-		return $prices;
-	}
+	require_once( __DIR__ . '/sm_brazil_weight.php' );
 }
