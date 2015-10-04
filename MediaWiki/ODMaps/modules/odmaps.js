5faff93b3d4cac44ea6a44dc2bdb80c6d4413b60
Aran <aran@organicdesign.co.nz>
2015-10-04 13:48:11 -0300

    make double newlines into br's

--- MediaWiki/ODMaps/modules/odmaps.js
+++ MediaWiki/ODMaps/modules/odmaps.js
@@ -60,7 +60,7 @@ $(document).ready( function() {
 			var html = '<div class="odmaps-info">';
 			html += '<h1>' + location.title + '</h1>';
 			if('image' in location) html += '<img src="' + location.image + '" />';
-			html += location.info;
+			html += location.info.replace('\n\n', '<br />');
 			html += '</div>';
 			return html;
 		};
