e3a7f4ee6258c36026f205fd7d5abb6a4f3afa6e
Aranad <aran@organicdesign.co.nz>
2016-03-30 17:19:04 +1300

    selected and blog css

--- MediaWiki/Bliki/Bliki.class.php
+++ MediaWiki/Bliki/Bliki.class.php
@@ -255,7 +255,7 @@ class Bliki {
 		$html = '<div class="bliki-pager">';
 		for( $n = 0; $n < $pages; $n++ ) {
 			$selected = ( $offset >= $n * $limit && $offset < ($n+1) * $limit ) ? ' class="selected"' : '';
-			$html .= "<a$selected href=\"/\">" . ($n + 1) . '</a>';
+			$html .= "<a$selected href=\"/\">" . ($n+1) . '</a>';
 		}
 		$html .= '</div>';
 		return $html;
