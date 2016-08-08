4ce6a5dc65deea3b45ef3fbca25317bcb8d05ff0
Aranad <aran@organicdesign.co.nz>
2016-08-08 15:44:18 -0300

    debug no bounce

--- MediaWiki/OrganicDesign/OrganicDesign.php
+++ MediaWiki/OrganicDesign/OrganicDesign.php
@@ -53,7 +53,7 @@ class OrganicDesign {
 			$ssl = array_key_exists( 'HTTPS', $_SERVER ) && $_SERVER['HTTPS'] == 'on';
 			$od = preg_match( "|^www\.organicdesign\.(.+)$|", $host, $m );
 			if( $wgUser->getName() == 'Nad' ) {
-				print $tld;
+				print "TLD $tld";
 				exit;
 			}
 			$tld = $m[1] ? $m[1] : 'co.nz';
